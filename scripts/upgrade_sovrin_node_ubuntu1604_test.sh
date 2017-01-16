7553916ece5c7351225dd51c4fe8361049db5396
Andrey Zheregelya <andrey.zheregelya@dsr-company.com>
2017-01-16 18:38:14 +0300

    Add draft for test version of updater shell script for ubuntu

---
+++ scripts/upgrade_sovrin_node_ubuntu1604_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+vers="$1"
+if [ -z "$vers" ] ; then
+  exit 1
+fi
+
+echo "Run sovrin upgrade to version $vers"
+apt-get update -y && apt-get install sovrin-node="$vers"
+ret=$?
+if [ $ret -ne 0 ] ; then
+  echo "Upgrade to version $vers failed"
+  exit 1
+fi
+
+echo "Restarting node and agent"
+systemctl restart sovrin-node
+
