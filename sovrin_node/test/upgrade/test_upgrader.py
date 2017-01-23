3646d8df61d60845365334374c1d29631e6cfd1d
Lovesh <lovesh.harchandani@evernym.com>
2017-01-24 00:38:48 +0530

    changing comparator in Upgrader

--- sovrin_node/test/upgrade/test_upgrader.py
+++ sovrin_node/test/upgrade/test_upgrader.py
@@ -3,11 +3,11 @@ from sovrin_node.server.upgrader import Upgrader
 
 
 def testVersions():
-    assert Upgrader.getNumericValueOfVersion('12.3') > \
-           Upgrader.getNumericValueOfVersion('1.23')
-    assert Upgrader.getNumericValueOfVersion('1.22.3') > \
-           Upgrader.getNumericValueOfVersion('1.2.23')
     assert Upgrader.isVersionHigher('0.0.5', '0.0.6')
     assert not Upgrader.isVersionHigher('0.0.9', '0.0.8')
     assert Upgrader.isVersionHigher('0.0.9', '0.1.0')
-    assert Upgrader.isVersionHigher('1.0.0', '0.20.30')
+    assert Upgrader.isVersionHigher('0.20.30', '1.0.0')
+    assert Upgrader.isVersionHigher('0.20.30', '1.0.0')
+    versions = ['0.0.1', '0.10.11', '0.0.2', '1.9.0', '9.10.0', '9.1.0']
+    assert Upgrader.versionsDescOrder(versions) == ['9.10.0', '9.1.0', '1.9.0',
+                                                    '0.10.11', '0.0.2', '0.0.1']
