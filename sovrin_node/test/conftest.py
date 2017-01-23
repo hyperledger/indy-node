761e821cd4cc8aa0c61aab1b13006abfb905f777
Rajesh Kalaria <rajesh.kalaria@gmail.com>
2017-01-23 08:44:21 +0530

    minor refactoring in conftests and in some other related files

--- sovrin_node/test/conftest.py
+++ sovrin_node/test/conftest.py
@@ -1,7 +1,3 @@
-import json
-import os
-
-from config.config import cmod
 from plenum.common.eventually import eventually
 from plenum.common.port_dispenser import genHa
 from plenum.common.raet import initLocalKeep
@@ -27,45 +23,22 @@ from ledger.serializers.compact_serializer import CompactSerializer
 from plenum.common.looper import Looper
 from plenum.common.signer_simple import SimpleSigner
 from plenum.common.txn import VERKEY, NODE_IP, NODE_PORT, CLIENT_IP, CLIENT_PORT, \
-    ALIAS, SERVICES, VALIDATOR
+    ALIAS, SERVICES, VALIDATOR, TYPE
 from plenum.test.plugin.helper import getPluginPath
-from plenum.test.conftest import patchPluginManager
 
 from sovrin_client.client.wallet.wallet import Wallet
 from sovrin_common.txn import STEWARD, NYM, SPONSOR
 from sovrin_common.txn import TXN_TYPE, TARGET_NYM, TXN_ID, ROLE, \
     getTxnOrderedFields
 from sovrin_common.config_util import getConfig
-from sovrin_client.test.cli.helper import newCLI
+
 from sovrin_node.test.helper import genTestClient, createNym, TestNode, \
     makePendingTxnsRequest, buildStewardClient, TestClient
+
 from sovrin_client.test.helper import addRole, getClientAddedWithRole
-
-primes = {
-    "prime1":
-        (cmod.integer(
-            157329491389375793912190594961134932804032426403110797476730107804356484516061051345332763141806005838436304922612495876180233509449197495032194146432047460167589034147716097417880503952139805241591622353828629383332869425029086898452227895418829799945650973848983901459733426212735979668835984691928193677469),
-         cmod.integer(
-             151323892648373196579515752826519683836764873607632072057591837216698622729557534035138587276594156320800768525825023728398410073692081011811496168877166664537052088207068061172594879398773872352920912390983199416927388688319207946493810449203702100559271439586753256728900713990097168484829574000438573295723))
-    , "prime2":
-        (cmod.integer(
-            150619677884468353208058156632953891431975271416620955614548039937246769610622017033385394658879484186852231469238992217246264205570458379437126692055331206248530723117202131739966737760399755490935589223401123762051823602343810554978803032803606907761937587101969193241921351011430750970746500680609001799529),
-         cmod.integer(
-             171590857568436644992359347719703764048501078398666061921719064395827496970696879481740311141148273607392657321103691543916274965279072000206208571551864201305434022165176563363954921183576230072812635744629337290242954699427160362586102068962285076213200828451838142959637006048439307273563604553818326766703))
-}
-
-
-@pytest.fixture(scope="module")
-def primes1():
-    P_PRIME1, Q_PRIME1 = primes.get("prime1")
-    return dict(p_prime=P_PRIME1, q_prime=Q_PRIME1)
-
-
-@pytest.fixture(scope="module")
-def primes2():
-    P_PRIME2, Q_PRIME2 = primes.get("prime2")
-    return dict(p_prime=P_PRIME2, q_prime=Q_PRIME2)
-
+from sovrin_client.test.cli.helper import newCLI
+from sovrin_client.test.conftest import updatedPoolTxnData, sponsorWallet, \
+    sponsor
 
 # noinspection PyUnresolvedReferences
 from plenum.test.conftest import tdir, counter, nodeReg, up, ready, \
@@ -73,7 +46,7 @@ from plenum.test.conftest import tdir, counter, nodeReg, up, ready, \
     startedNodes, tdirWithDomainTxns, txnPoolNodeSet, poolTxnData, dirName, \
     poolTxnNodeNames, allPluginsPath, tdirWithNodeKeepInited, tdirWithPoolTxns, \
     poolTxnStewardData, poolTxnStewardNames, getValueFromModule, \
-    txnPoolNodesLooper, nodeAndClientInfoFilePath, conf
+    txnPoolNodesLooper, nodeAndClientInfoFilePath, conf, patchPluginManager
 
 
 @pytest.fixture(scope="module")
@@ -81,21 +54,6 @@ def tconf(conf, tdir):
     conf.baseDir = tdir
     conf.MinSepBetweenNodeUpgrades = 5
     return conf
-
-
-@pytest.fixture(scope="module")
-def updatedPoolTxnData(poolTxnData):
-    data = poolTxnData
-    trusteeSeed = 'thisistrusteeseednotsteward12345'
-    signer = SimpleSigner(seed=trusteeSeed.encode())
-    t = {"dest": signer.verkey,
-         "role": "TRUSTEE",
-         "type": "NYM",
-         "alias": "Trustee1",
-         "txnId": "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4a"}
-    data["seeds"]["Trustee1"] = trusteeSeed
-    data["txns"].insert(0, t)
-    return data
 
 
 @pytest.fixture(scope="module")
@@ -196,58 +154,6 @@ def updatedDomainTxnFile(tdir, tdirWithDomainTxns, genesisTxns,
 @pytest.fixture(scope="module")
 def nodeSet(tconf, updatedPoolTxnData, updatedDomainTxnFile, txnPoolNodeSet):
     return txnPoolNodeSet
-
-
-@pytest.fixture(scope="module")
-def client1Signer():
-    seed = b'client1Signer secret key........'
-    signer = SimpleSigner(seed=seed)
-    assert signer.verkey == '6JvpZp2haQgisbXEXE9NE6n3Tuv77MZb5HdF9jS5qY8m'
-    return signer
-
-
-@pytest.fixture("module")
-def sponsorCli(looper, tdir):
-    return newCLI(looper, tdir)
-
-
-@pytest.fixture(scope="module")
-def clientAndWallet1(client1Signer, looper, nodeSet, tdir, up):
-    client, wallet = genTestClient(nodeSet, tmpdir=tdir, usePoolLedger=True)
-    wallet = Wallet(client.name)
-    wallet.addIdentifier(signer=client1Signer)
-    return client, wallet
-
-
-@pytest.fixture(scope="module")
-def client1(clientAndWallet1, looper):
-    client, wallet = clientAndWallet1
-    looper.add(client)
-    looper.run(client.ensureConnectedToNodes())
-    return client
-
-
-@pytest.fixture(scope="module")
-def wallet1(clientAndWallet1):
-    return clientAndWallet1[1]
-
-
-@pytest.fixture(scope="module")
-def sponsorWallet():
-    wallet = Wallet('sponsor')
-    seed = b'sponsors are people too.........'
-    wallet.addIdentifier(seed=seed)
-    return wallet
-
-
-@pytest.fixture(scope="module")
-def sponsor(nodeSet, addedSponsor, sponsorWallet, looper, tdir):
-    s, _ = genTestClient(nodeSet, tmpdir=tdir, usePoolLedger=True)
-    s.registerObserver(sponsorWallet.handleIncomingReply)
-    looper.add(s)
-    looper.run(s.ensureConnectedToNodes())
-    makePendingTxnsRequest(s, sponsorWallet)
-    return s
 
 
 @pytest.fixture(scope="module")
