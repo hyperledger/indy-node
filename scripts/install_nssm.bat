29af3de2fce6f27174761d449f289b97efb2cdb1
Andrei Goncharov <frantic1993@gmail.com>
2017-02-07 08:55:02 +0300

    Feature/update scripts (#27)
    * Feature Added: Update scripts pool_transactions backup
    
    * Feature Added: Update scripts pool_transactions backup
    
    * Feature Added: Update scripts pool_transactions backup
    
    * Feature Added: Update scripts pool_transactions backup
    
    * Feature Added: Update scripts nssm backup
    
    Feature Added: System dependent post-install
    
    * Feature Added: Update scripts nssm backup
    
    * Feature Added: Update scripts nssm backup

--- scripts/install_nssm.bat
+++ scripts/install_nssm.bat
@@ -1,6 +1,7 @@
 @echo off
 
 echo "Installing nssm"
+copy /y C:\Users\sovrin\.sovrin\nssm_original.exe C:\Users\sovrin\.sovrin\nssm.exe
 SET Key="HKCU\Environment"
 FOR /f "skip=2 tokens=3*" %%a in ('reg query HKCU\Environment /v PATH') DO (
     IF [%%b]==[] (
