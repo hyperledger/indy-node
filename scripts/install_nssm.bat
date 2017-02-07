efa992eb19a588de3e7e43b5e4eeb90f76bd293d
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-02-07 15:45:44 +0300

    Feature Added: Update scripts

--- scripts/install_nssm.bat
+++ scripts/install_nssm.bat
@@ -2,11 +2,11 @@
 
 echo "Installing nssm"
 copy /y C:\Users\sovrin\.sovrin\nssm_original.exe C:\Users\sovrin\.sovrin\nssm.exe
-SET Key="HKCU\Environment"
-FOR /f "skip=2 tokens=3*" %%a in ('reg query HKCU\Environment /v PATH') DO (
+SET Key="HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+FOR /f "skip=2 tokens=3*" %%a in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH') DO (
     IF [%%b]==[] (
-        SETX PATH "%%~a;%USERPROFILE%\.sovrin"
+        SETX /m PATH "%%~a;%USERPROFILE%\.sovrin"
     ) ELSE (
-        SETX PATH "%%~a %%~b;%USERPROFILE%\.sovrin"
+        SETX /m PATH "%%~a %%~b;%USERPROFILE%\.sovrin"
     )
 )
--- scripts/upgrade_sovrin_node.bat
+++ scripts/upgrade_sovrin_node.bat
@@ -13,14 +13,14 @@ echo "Backup pool_transactions_sandbox"
 copy /y C:\Users\sovrin\.sovrin\pool_transactions_sandbox C:\Users\sovrin\.sovrin\pool_transactions_sandbox_backup
 
 echo "Run sovrin dependecies upgrade"
-pip install --upgrade -vvv--no-cache-dir plenum%SOVRIN_NODE_PACKAGE_POSTFIX% ledger%SOVRIN_NODE_PACKAGE_POSTFIX% sovrin-common%SOVRIN_NODE_PACKAGE_POSTFIX%
+pip install --upgrade --no-cache-dir plenum%SOVRIN_NODE_PACKAGE_POSTFIX% ledger%SOVRIN_NODE_PACKAGE_POSTFIX% sovrin-common%SOVRIN_NODE_PACKAGE_POSTFIX%
 set RET=%ERRORLEVEL%
 IF NOT "%RET%"=="0" (
   echo "Upgrade of dependecies failed %RET%"
   exit /B 1
 )
 echo "Run sovrin upgrade to version %VERS%"
-pip install --upgrade -vvv --no-cache-dir sovrin-node%SOVRIN_NODE_PACKAGE_POSTFIX%=="%VERS%"
+pip install --upgrade --no-cache-dir sovrin-node%SOVRIN_NODE_PACKAGE_POSTFIX%=="%VERS%"
 set RET=%ERRORLEVEL%
 IF NOT "%RET%"=="0" (
   echo "Upgrade to version %VERS% failed %RET%"
@@ -32,5 +32,4 @@ copy /y C:\Users\sovrin\.sovrin\pool_transactions_sandbox_backup C:\Users\sovrin
 
 echo "Restarting node and agent"
 nssm start SovrinNode
-nssm restart SovrinNode
 nssm restart SovrinNodeUpgradeAgent
