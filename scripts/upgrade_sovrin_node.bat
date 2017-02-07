38297688fd39b95969917e9999f02816c89d9fd5
Andrei Goncharov <frantic1993@gmail.com>
2017-02-07 15:55:03 +0300

    Feature Added: Update scripts for Ubuntu and Windows fixed
    * Feature Added: Update scripts pool_transactions backup
    
    * Feature Added: Update scripts pool_transactions backup
    
    * Feature Added: Update scripts pool_transactions backup
    
    * Feature Added: Update scripts pool_transactions backup
    
    * Feature Added: Update scripts nssm backup
    
    Feature Added: System dependent post-install
    
    * Feature Added: Update scripts nssm backup
    
    * Feature Added: Update scripts nssm backup
    
    * Feature Added: Update scripts restart order
    
    * Feature Added: Update scripts
    
    * Feature Added: Update scripts

--- scripts/upgrade_sovrin_node.bat
+++ scripts/upgrade_sovrin_node.bat
@@ -12,12 +12,18 @@ nssm stop SovrinNode
 echo "Backup pool_transactions_sandbox"
 copy /y C:\Users\sovrin\.sovrin\pool_transactions_sandbox C:\Users\sovrin\.sovrin\pool_transactions_sandbox_backup
 
-echo "Run sovrin upgrade to version %VERS%"
+echo "Run sovrin dependecies upgrade"
 pip install --upgrade --no-cache-dir plenum%SOVRIN_NODE_PACKAGE_POSTFIX% ledger%SOVRIN_NODE_PACKAGE_POSTFIX% sovrin-common%SOVRIN_NODE_PACKAGE_POSTFIX%
+set RET=%ERRORLEVEL%
+IF NOT "%RET%"=="0" (
+  echo "Upgrade of dependecies failed %RET%"
+  exit /B 1
+)
+echo "Run sovrin upgrade to version %VERS%"
 pip install --upgrade --no-cache-dir sovrin-node%SOVRIN_NODE_PACKAGE_POSTFIX%=="%VERS%"
 set RET=%ERRORLEVEL%
 IF NOT "%RET%"=="0" (
-  echo "Upgrade to version %VERS% failed"
+  echo "Upgrade to version %VERS% failed %RET%"
   exit /B 1
 )
 
