30a08dd8945a8f3f02174c4ce420cbcee1a94017
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-02-07 15:00:03 +0300

    Feature Added: Update scripts

--- scripts/upgrade_sovrin_node.bat
+++ scripts/upgrade_sovrin_node.bat
@@ -12,12 +12,18 @@ nssm stop SovrinNode
 echo "Backup pool_transactions_sandbox"
 copy /y C:\Users\sovrin\.sovrin\pool_transactions_sandbox C:\Users\sovrin\.sovrin\pool_transactions_sandbox_backup
 
-echo "Run sovrin upgrade to version %VERS%"
-pip install --upgrade --no-cache-dir plenum%SOVRIN_NODE_PACKAGE_POSTFIX% ledger%SOVRIN_NODE_PACKAGE_POSTFIX% sovrin-common%SOVRIN_NODE_PACKAGE_POSTFIX%
-pip install --upgrade --no-cache-dir sovrin-node%SOVRIN_NODE_PACKAGE_POSTFIX%=="%VERS%"
+echo "Run sovrin dependecies upgrade"
+pip install --upgrade -vvv--no-cache-dir plenum%SOVRIN_NODE_PACKAGE_POSTFIX% ledger%SOVRIN_NODE_PACKAGE_POSTFIX% sovrin-common%SOVRIN_NODE_PACKAGE_POSTFIX%
 set RET=%ERRORLEVEL%
 IF NOT "%RET%"=="0" (
-  echo "Upgrade to version %VERS% failed"
+  echo "Upgrade of dependecies failed %RET%"
+  exit /B 1
+)
+echo "Run sovrin upgrade to version %VERS%"
+pip install --upgrade -vvv --no-cache-dir sovrin-node%SOVRIN_NODE_PACKAGE_POSTFIX%=="%VERS%"
+set RET=%ERRORLEVEL%
+IF NOT "%RET%"=="0" (
+  echo "Upgrade to version %VERS% failed %RET%"
   exit /B 1
 )
 
@@ -26,4 +32,5 @@ copy /y C:\Users\sovrin\.sovrin\pool_transactions_sandbox_backup C:\Users\sovrin
 
 echo "Restarting node and agent"
 nssm start SovrinNode
+nssm restart SovrinNode
 nssm restart SovrinNodeUpgradeAgent
