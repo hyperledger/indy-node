b9c03d044a6212eb6dd7ca8aeaccd1228af887b1
Muzychenko Victor <mzkvct@gmail.com>
2017-02-09 15:03:34 +0400

    replace claim def by schema in node (#32)

--- sovrin_node/server/node.py
+++ sovrin_node/server/node.py
@@ -26,7 +26,7 @@ from sovrin_common.txn import TXN_TYPE, \
     TARGET_NYM, allOpKeys, validTxnTypes, ATTRIB, SPONSOR, NYM,\
     ROLE, STEWARD, GET_ATTR, DISCLO, DATA, GET_NYM, \
     TXN_ID, TXN_TIME, reqOpKeys, GET_TXNS, LAST_TXN, TXNS, \
-    getTxnOrderedFields, CLAIM_DEF, GET_CLAIM_DEF, openTxns, \
+    getTxnOrderedFields, SCHEMA, GET_SCHEMA, openTxns, \
     ISSUER_KEY, GET_ISSUER_KEY, REF, TRUSTEE, TGB, IDENTITY_TXN_TYPES, \
     CONFIG_TXN_TYPES, POOL_UPGRADE, ACTION, START, CANCEL, SCHEDULE, \
     NODE_UPGRADE, COMPLETE, FAIL
@@ -369,7 +369,7 @@ class Node(PlenumNode, HasPoolManager):
                         "Only user's sponsor can add attribute for that user")
 
         # TODO: Just for now. Later do something meaningful here
-        elif typ in [DISCLO, GET_ATTR, CLAIM_DEF, GET_CLAIM_DEF, ISSUER_KEY,
+        elif typ in [DISCLO, GET_ATTR, SCHEMA, GET_SCHEMA, ISSUER_KEY,
                      GET_ISSUER_KEY]:
             pass
         elif request.operation.get(TXN_TYPE) in POOL_TXN_TYPES:
@@ -474,17 +474,17 @@ class Node(PlenumNode, HasPoolManager):
             })
             self.transmitToClient(Reply(result), frm)
 
-    def processGetClaimDefReq(self, request: Request, frm: str):
+    def processGetSchemaReq(self, request: Request, frm: str):
         issuerNym = request.operation[TARGET_NYM]
         name = request.operation[DATA][NAME]
         version = request.operation[DATA][VERSION]
-        claimDef = self.graphStore.getClaimDef(issuerNym, name, version)
+        schema = self.graphStore.getSchema(issuerNym, name, version)
         result = {
             TXN_ID: self.genTxnId(
                 request.identifier, request.reqId)
         }
         result.update(request.operation)
-        result[DATA] = json.dumps(claimDef, sort_keys=True)
+        result[DATA] = json.dumps(schema, sort_keys=True)
         result.update({
             f.IDENTIFIER.nm: request.identifier,
             f.REQ_ID.nm: request.reqId,
@@ -532,8 +532,8 @@ class Node(PlenumNode, HasPoolManager):
             self.processGetNymReq(request, frm)
         elif request.operation[TXN_TYPE] == GET_TXNS:
             self.processGetTxnReq(request, frm)
-        elif request.operation[TXN_TYPE] == GET_CLAIM_DEF:
-            self.processGetClaimDefReq(request, frm)
+        elif request.operation[TXN_TYPE] == GET_SCHEMA:
+            self.processGetSchemaReq(request, frm)
         elif request.operation[TXN_TYPE] == GET_ATTR:
             self.processGetAttrsReq(request, frm)
         elif request.operation[TXN_TYPE] == GET_ISSUER_KEY:
@@ -605,8 +605,8 @@ class Node(PlenumNode, HasPoolManager):
             self.graphStore.addNymTxnToGraph(result)
         elif result[TXN_TYPE] == ATTRIB:
             self.graphStore.addAttribTxnToGraph(result)
-        elif result[TXN_TYPE] == CLAIM_DEF:
-            self.graphStore.addClaimDefTxnToGraph(result)
+        elif result[TXN_TYPE] == SCHEMA:
+            self.graphStore.addSchemaTxnToGraph(result)
         elif result[TXN_TYPE] == ISSUER_KEY:
             self.graphStore.addIssuerKeyTxnToGraph(result)
         else:
