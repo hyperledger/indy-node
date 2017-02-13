dd9b465c618f0fcb876ebc459a60fb88080dfc7e
Victor Muzychenko <mzkvct@gmail.com>
2017-02-13 18:33:19 +0300

    add getters for schema and issuer key

--- sovrin_node/persistence/StateTreeStore.py
+++ sovrin_node/persistence/StateTreeStore.py
@@ -77,6 +77,12 @@ class StateTreeStore:
         assert txn[TXN_TYPE] == ISSUER_KEY
         logger.warn("Not implemented")
 
+    def getSchema(self, did):
+        pass
+
+    def getIssuerKey(self, did):
+        pass
+
     def getAttr(self, key: str, did):
         assert key is not None
         assert did is not None
