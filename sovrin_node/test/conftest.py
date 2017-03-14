f29c6aca970827d3aa0eedc9e25d8576661bd6ee
Alexander Shcherbakov <alexander.sherbakov@dsr-company.com>
2017-03-14 15:53:36 +0400

    rename SPONSOR to TRUST_ANCHOR (#52)
    * Renamed SPONSOR to TRUST_ANCHOR
    
    * fix repo name
    
    * use encoded values for Roles

--- sovrin_node/test/conftest.py
+++ sovrin_node/test/conftest.py
@@ -23,11 +23,11 @@ from ledger.serializers.compact_serializer import CompactSerializer
 from plenum.common.looper import Looper
 from plenum.common.signer_simple import SimpleSigner
 from plenum.common.txn import VERKEY, NODE_IP, NODE_PORT, CLIENT_IP, CLIENT_PORT, \
-    ALIAS, SERVICES, VALIDATOR, TYPE
+    ALIAS, SERVICES, VALIDATOR, TYPE, STEWARD, TRUST_ANCHOR
 from plenum.test.plugin.helper import getPluginPath
 
 from sovrin_client.client.wallet.wallet import Wallet
-from sovrin_common.txn import STEWARD, NYM, SPONSOR
+from sovrin_common.txn import NYM
 from sovrin_common.txn import TXN_TYPE, TARGET_NYM, TXN_ID, ROLE, \
     getTxnOrderedFields
 from sovrin_common.config_util import getConfig
@@ -38,8 +38,8 @@ from sovrin_node.test.helper import TestNode, \
 from sovrin_client.test.helper import addRole, getClientAddedWithRole, \
     genTestClient, TestClient, createNym
 from sovrin_client.test.cli.helper import newCLI
-from sovrin_client.test.conftest import updatedPoolTxnData, sponsorWallet, \
-    sponsor
+from sovrin_client.test.conftest import updatedPoolTxnData, trustAnchorWallet, \
+    trustAnchor
 
 # noinspection PyUnresolvedReferences
 from plenum.test.conftest import tdir, counter, nodeReg, up, ready, \
@@ -158,20 +158,20 @@ def nodeSet(tconf, updatedPoolTxnData, updatedDomainTxnFile, txnPoolNodeSet):
 
 
 @pytest.fixture(scope="module")
-def addedSponsor(nodeSet, steward, stewardWallet, looper,
-                 sponsorWallet):
+def addedTrustAnchor(nodeSet, steward, stewardWallet, looper,
+                 trustAnchorWallet):
     createNym(looper,
-              sponsorWallet.defaultId,
+              trustAnchorWallet.defaultId,
               steward,
               stewardWallet,
-              role=SPONSOR,
-              verkey=sponsorWallet.getVerkey())
-    return sponsorWallet
+              role=TRUST_ANCHOR,
+              verkey=trustAnchorWallet.getVerkey())
+    return trustAnchorWallet
 
 
 @pytest.fixture(scope="module")
-def userWalletB(nodeSet, addedSponsor, sponsorWallet, looper, sponsor):
-    return addRole(looper, sponsor, sponsorWallet, 'userB', useDid=False)
+def userWalletB(nodeSet, addedTrustAnchor, trustAnchorWallet, looper, trustAnchor):
+    return addRole(looper, trustAnchor, trustAnchorWallet, 'userB', useDid=False)
 
 
 @pytest.fixture(scope="module")
