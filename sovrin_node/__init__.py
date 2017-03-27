3e7b964fb19b47d6bd1504fd8dd0a2e6a77bd41e
Andrei Goncharov <frantic1993@gmail.com>
2017-03-28 01:52:55 +0300

    Feature Added: Manual dependency check deprecated (#67)

--- sovrin_node/__init__.py
+++
@@ -1,4 +0,0 @@
-import sovrin_node
-from plenum.common.pkg_util import check_deps
-
-check_deps(sovrin_node)
--- sovrin_node/__metadata__.py
+++ sovrin_node/__metadata__.py
@@ -7,8 +7,3 @@ __author__ = "Sovrin Foundation."
 __license__ = "Apache 2.0"
 
 __all__ = ['__version_info__', '__version__', '__author__', '__license__']
-
-# TODO: Shouldn't we update these dependencies?
-__dependencies__ = {
-    "sovrin_common": ">=0.0.4",
-}
