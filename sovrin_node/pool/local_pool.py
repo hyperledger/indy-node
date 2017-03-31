faac4d3d05d894c978f9ff3fa6a561f2984f3a32
Devin Fisher <devin.fisher@evernym.com>
2017-03-31 16:48:02 -0600

    Added code that will create a local pool
    This code will take server permeters and do all the work
    to get a pool up and running, including all the stewers and
    their keys

--- sovrin_node/pool/local_pool.py
+++ sovrin_node/pool/local_pool.py
@@ -1,32 +1,97 @@
 from collections import deque
 
+from plenum.common.member.steward import Steward
 from plenum.common.raet import initLocalKeep
 from plenum.common.test_network_setup import TestNetworkSetup
 from plenum.common.constants import TYPE, NODE, NYM
 from plenum.common.looper import Looper
 from plenum.common.util import adict
+from sovrin_client.agent.agent import WalletedAgent
+from sovrin_client.client.client import Client
+from sovrin_node.server.node import Node
+from sovrin_client.client.wallet.wallet import Wallet
 
 from sovrin_common.config_util import getConfig
+from sovrin_common.init_util import initialize_node_environment
 from sovrin_common.pool.pool import Pool
 from sovrin_common.txn_util import getTxnOrderedFields
 
 
+def create_local_pool(base_dir, node_size=4, looper=None):
+    conf = getConfig(base_dir)
+    stewards = []
+    node_conf = []
+    nodes = []
+    genesis_txns = []
+    for i in range(node_size):
+        w = Wallet()
+        s = Steward(wallet=w)
+        s.wallet.addIdentifier()
+
+        stewards.append(s)
+
+        n_config = adict(name='Node'+str(i+1),
+                         basedirpath=base_dir,
+                         ha=('127.0.0.1', 9700+(i * 2)),
+                         cliha=('127.0.0.1', 9700+(i * 2)+1))
+
+        n_verkey = initialize_node_environment(name=n_config.name,
+                                               base_dir=n_config.basedirpath,
+                                               override_keep=True,
+                                               config=conf)
+
+        s.set_node(n_config, verkey=n_verkey)
+
+        node_conf.append(n_config)
+
+        genesis_txns += s.generate_genesis_txns()
+
+    pool = LocalPool(genesis_txns, base_dir, steward=stewards[0])
+
+    for c in node_conf:
+        n = Node(**c)
+        pool.add(n)
+        nodes.append(n)
+
+    pool.run_till_quiet(1)
+
+    return pool
+
+
 class LocalPool(Pool, Looper):
+    def __init__(self, genesis_txns, base_dir, config=None, loop=None, steward: Steward=None):
+        super().__init__(loop=loop)
+        self.base_dir = base_dir
+        self.genesis_txns = genesis_txns
+        self.config = config or getConfig(self.base_dir)
+        self._generate_genesis_files()
+        self._steward = steward
+
+        if steward is not None:
+            self._steward_agent = WalletedAgent(name="steward1",
+                                                basedirpath=self.base_dir,
+                                                client=self.create_client(5005),
+                                                wallet=steward.wallet,
+                                                port=8781)
+            self.add(self._steward_agent)
 
     @property
     def genesis_transactions(self):
         return self.genesis_txns
 
-    def __init__(self, genesis_txns, base_dir, config=None, loop=None):
-        super().__init__(loop=loop)
-        self.base_dir = base_dir
-        self.genesis_txns = genesis_txns
-        self.config = config or getConfig(self.base_dir)
+    def create_client(self, port: int):
+        return Client(basedirpath=self.base_dir,
+                      ha=('127.0.0.1', port))
+
+    def steward_agent(self):
+        return self._steward_agent
+
+
 
     def setup_local_node(self, name, sigseed, override=True):
         _, verkey = initLocalKeep(name, self.base_dir, sigseed, override)
 
-    def generate_genesis_files(self):
+    def _generate_genesis_files(self):
 
         pool_txns, domain_txns = self._split_pool_and_domain()
 
