998a371b281d4070e9bb0d15365b86cf4ae6b5e7
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-04-20 22:37:36 +0300

    increment sovrin-client version

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
     )],
     install_requires=['sovrin-common-3pc-batch==0.2.9', 'python-dateutil', 'leveldb'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'sovrin-client-3pc-batch==0.3.31'],
+    tests_require=['pytest', 'sovrin-client-3pc-batch==0.3.45'],
     scripts=['scripts/start_sovrin_node',
              'scripts/node_control_tool.py',
              'scripts/clear_node.py',
