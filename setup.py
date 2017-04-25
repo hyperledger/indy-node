482ed711c04d3f8cb83fadf353fc85b10ef87948
spivachuk <nikita.spivachuk@dsr-company.com>
2017-04-25 13:41:12 +0100

    Common test code extract (#129)
    * Factored out common code related to tests from sovrin-node and sovrin-client to sovrin-common.
    
    * - Removed sovrin-node fixtures that duplicate sovrin-client fixtures related to client functionality.
    - Removed nodeThetaAdded fixture from sovrin-client since it is unused.
    
    * Updated sovrin-common-dev and sovrin-client-dev dependencies.
    
    * Removed testNodeClass and nodeSet fixtures (now the corresponding fixtures from sovrin-client are used instead).
    
    * Removed updatedPoolTxnData, trusteeData, trusteeWallet and trustee fixtures (now the corresponding fixtures from sovrin-client are used instead).
    
    * Removed unused imports.
    
    * - Removed userWalletB fixture (now the corresponding fixture from sovrin-client is used instead).
    - Corrected imports and removed unused imports.
    
    * Updated sovrin-client-dev dependency.

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
     )],
     install_requires=['sovrin-common-dev==0.2.48', 'python-dateutil'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'sovrin-client-dev==0.3.67'],
+    tests_require=['pytest', 'sovrin-client-dev==0.3.69'],
     scripts=['scripts/start_sovrin_node',
              'scripts/node_control_tool.py',
              'scripts/clear_node.py',
