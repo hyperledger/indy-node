7f7d45bdbf62874532ebb1d771732b289c5f2d5a
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-04-26 19:51:12 +0300

    increment sovrin-common

--- setup.py
+++ setup.py
@@ -89,9 +89,9 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['sovrin-common-3pc-batch==0.2.18', 'python-dateutil'],
+    install_requires=['sovrin-common-3pc-batch==0.2.20', 'python-dateutil'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'sovrin-client-3pc-batch==0.3.65'],
+    tests_require=['pytest', 'sovrin-client-3pc-batch==0.3.68'],
     scripts=['scripts/start_sovrin_node',
              'scripts/node_control_tool.py',
              'scripts/clear_node.py',
