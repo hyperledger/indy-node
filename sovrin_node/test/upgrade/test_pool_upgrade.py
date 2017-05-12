40a2eaba8d83bb0d86f02161eec21ccee0fede95
Lovesh <lovesh.harchandani@evernym.com>
2017-05-12 14:04:56 +0530

    remove duplicate test

--- sovrin_node/test/upgrade/test_pool_upgrade.py
+++ sovrin_node/test/upgrade/test_pool_upgrade.py
@@ -1,13 +1,7 @@
-from copy import deepcopy
-
 from plenum.common.constants import VERSION
-from plenum.test import waits as plenumWaits
 from plenum.test.test_node import checkNodesConnected, ensureElectionsDone
-from sovrin_common.constants import CANCEL, \
-    ACTION, SCHEDULE, JUSTIFICATION
 from sovrin_node.test import waits
-from sovrin_node.test.upgrade.helper import sendUpgrade, \
-    checkUpgradeScheduled, checkNoUpgradeScheduled
+from sovrin_node.test.upgrade.helper import checkUpgradeScheduled
 from stp_core.loop.eventually import eventually
 
 whitelist = ['Failed to upgrade node']
@@ -41,26 +35,3 @@ def testNodeSchedulesUpgradeAfterRestart(upgradeScheduled, looper, nodeSet,
     looper.run(eventually(checkUpgradeScheduled, nodeSet, validUpgrade[VERSION],
                           retryWait=1, timeout=waits.expectedUpgradeScheduled()))
 
-
-
-def testTrustyCancelsUpgrade(validUpgradeSent, looper, nodeSet, trustee,
-                             trusteeWallet, validUpgrade):
-    # here there is a dependency from 'testNodeSchedulesUpgradeAfterRestart'
-    # we have to reconnect client after the nodes is restarted.
-    looper.run(trustee.ensureConnectedToNodes())
-
-    validUpgradeCopy = deepcopy(validUpgrade)
-    validUpgradeCopy[ACTION] = CANCEL
-    validUpgradeCopy[JUSTIFICATION] = '"not gonna give you one"'
-
-    validUpgradeCopy.pop(SCHEDULE, None)
-    upgrade, req = sendUpgrade(trustee, trusteeWallet, validUpgradeCopy)
-
-    def check():
-        assert trusteeWallet.getPoolUpgrade(upgrade.key).seqNo
-
-    timeout = plenumWaits.expectedTransactionExecutionTime(len(nodeSet))
-    looper.run(eventually(check, retryWait=1, timeout=timeout))
-
-    looper.run(eventually(checkNoUpgradeScheduled, nodeSet, retryWait=1,
-                          timeout=waits.expectedNoUpgradeScheduled()))
