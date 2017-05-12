dde69e54548d38fae70a8899e72ebe45ede07361
Muzychenko Victor <mzkvct@gmail.com>
2017-05-12 22:14:40 +0400

    remove cleanup of orientdb (#145)

--- scripts/clear_node.py
+++ scripts/clear_node.py
@@ -6,29 +6,6 @@ import argparse
 import re
 
 
-try:
-    import pyorient
-    ORIENTDB_HOST = "localhost"
-    ORIENTDB_PORT = 2424
-    ORIENTDB_USER = "root"
-    ORIENTDB_PASSWORD = "password"
-
-
-    def clean_orientdb():
-        client = pyorient.OrientDB(ORIENTDB_HOST, ORIENTDB_PORT)
-        client.connect(ORIENTDB_USER, ORIENTDB_PASSWORD)
-        names = [n for n in client.db_list().oRecordData['databases'].keys()]
-        for nm in names:
-            try:
-                client.db_drop(nm)
-            except:
-                continue
-
-except ImportError:
-    pyorient = None
-    clean_orientdb = lambda: None
-
-
 def pathList(*paths):
     return {os.path.expanduser(path) for path in paths}
 
@@ -86,7 +63,3 @@ if __name__ == "__main__":
                         help="remove configs and logs too")
     args = parser.parse_args()
     clean_files(args.full)
-    try:
-        clean_orientdb()
-    except Exception as ex:
-        print("Cleaning of data from orientdb failed:", ex)
