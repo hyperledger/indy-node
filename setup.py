09aff069b5f17046308f5919a627f5637f137069
Muzychenko Victor <mzkvct@gmail.com>
2017-05-19 18:52:42 +0400

    Fix for connection loss and refactoring (#148)
    * add exception handling for case when no answers received yet
    
    * up common to 0.2.64
    
    * up client to 0.3.100

--- setup.py
+++ setup.py
@@ -73,9 +73,9 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['sovrin-common-dev==0.2.63', 'python-dateutil'],
+    install_requires=['sovrin-common-dev==0.2.64', 'python-dateutil'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'sovrin-client-dev==0.3.92'],
+    tests_require=['pytest', 'sovrin-client-dev==0.3.100'],
     scripts=['scripts/start_sovrin_node',
              'scripts/node_control_tool.py',
              'scripts/clear_node.py',
