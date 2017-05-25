040e165f9d905a8f3a50d5b170087a5dc7b6fdae
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-05-25 17:10:45 +0300

    Completed migration_tool

--- data/migrations/README.md
+++ data/migrations/README.md
@@ -2,5 +2,15 @@
 
 All migrations are python scripts with names satisfying the following rule:
 ```
-TODO: Add rule
+<version-migration-should-be-applied-to>_<migration-name>
+```
+Example:
+```
+0.3.100_replace_db
+```
+If there are several migrations that should be applied to the same version they will be applied alphabetically. If you want to ensure migrations' execution order prefix their name with digits.    
+Example:
+```
+0.3.100_0_first
+0.3.100_1_second
 ```
--- scripts/migration_tool.py
+++ scripts/migration_tool.py
@@ -2,17 +2,25 @@
 
 import select
 import time
+import pkgutil
+import importlib
+
 from stp_core.common.log import getlogger
 
+# Data folder is published as a separate 'data' python package
+from data import migrations
+
 logger = getlogger()
+
+SCRIPT_PREFIX = 'data.migrations.'
 
 
 # Returns number of performed migrations
 def migrate(current_version):
     logger.debug('Looking for migrations')
-    migration_scripts = get_migration_scripts()
+    migration_scripts = _get_migration_scripts()
     logger.debug('Found migration scripts: {}'.format(migration_scripts))
-    migration_scripts = get_relevalnt_migrations(migration_scripts, current_version)
+    migration_scripts = _get_relevalnt_migrations(migration_scripts, current_version)
     if not len(migration_scripts):
         logger.info('No migrations can be applied to the current code.')
         return 0
@@ -20,21 +28,25 @@ def migrate(current_version):
     for migration in migration_scripts:
         logger.info('Applying migration {}'.format(migration))
         start_time = time.time()
-        call_migration_script(migration)
+        _call_migration_script(migration)
         logger.info('Migration {} applied in {} seconds'.format(migration, time.time() - start_time))
     return len(migration_scripts)
 
 
-def get_migration_scripts():
-    # TODO: Implement me
-    pass
+def _get_migration_scripts():
+    migrations = [name for module_finder, name, ispkg in pkgutil.iter_modules(migrations.__path__)]
+    migrations.sort()
+    return migrations
 
 
-def get_relevalnt_migrations(migration_scripts, current_version):
-    # TODO: Implement me
-    pass
+def _get_relevalnt_migrations(migration_scripts, current_version):
+    relevant_migrations = []
+    for migration in migration_scripts:
+        migration_version = migration.split('_')[0]
+        if migration_version >= current_version:
+            relevant_migrations.append(migration)
+    return relevant_migrations
 
 
-def call_migration_script(migration_script):
-    # TODO: Implement me
-    pass
+def _call_migration_script(migration_script):
+    importlib.import_module(SCRIPT_PREFIX + migration_script)
