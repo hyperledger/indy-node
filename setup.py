50846683426eff5442b36ed20047602e18ef0ee7
Andrei Goncharov <frantic1993@gmail.com>
2017-05-30 21:04:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['sovrin-common-dev==0.2.72', 'python-dateutil'],
+    install_requires=['sovrin-common-new-elec==0.2.2', 'python-dateutil'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'sovrin-client-dev==0.3.104'],
     scripts=['scripts/start_sovrin_node',
