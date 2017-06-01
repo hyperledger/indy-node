84307a06d52b11af20e79452e3c6af9825b1f5b7
Alexander Shekhovtsov <alexander.shekhovcov@dsr-company.com>
2017-06-01 15:46:51 +0300

    Remove ATTRIB checks
    The checks were moved to the validation logic

--- sovrin_node/server/domain_req_handler.py
+++ sovrin_node/server/domain_req_handler.py
@@ -112,19 +112,6 @@ class DomainReqHandler(PHandler):
                                        '{} should have one and only one of '
                                        '{}, {}, {}'
                                        .format(ATTRIB, RAW, ENC, HASH))
-        if RAW in operation:
-            try:
-                data = json.loads(operation[RAW])
-                # QUESTION: This does not fit here, this should be the
-                # client's responsibility to check.
-                endpoint = data.get(ENDPOINT)
-                if endpoint is not None:
-                    check_endpoint_valid(data[ENDPOINT])
-
-            except EndpointException as exc:
-                raise InvalidClientRequest(identifier, reqId, str(exc))
-            except BaseException as exc:
-                raise InvalidClientRequest(identifier, reqId, str(exc))
 
         # TODO: This is not static validation as it involves state
         if not (not operation.get(TARGET_NYM) or
