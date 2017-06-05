77c8ee2795be1a9897d321d2811f1f511a54425f
Victor Muzychenko <mzkvct@gmail.com>
2017-06-05 13:13:53 +0300

    up client version to 0.3.125

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     )],
     install_requires=['sovrin-common-dev==0.2.81', 'python-dateutil'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'sovrin-client-dev==0.3.123'],
+    tests_require=['pytest', 'sovrin-client-dev==0.3.125'],
     scripts=['scripts/start_sovrin_node',
              'scripts/node_control_tool.py',
              'scripts/migration_tool.py',
