c9a5abef2ce73bcd2c5f64667e3f6eb52d927524
Victor Muzychenko <mzkvct@gmail.com>
2017-06-05 23:39:44 +0300

    up common to 0.2.11
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: behalf
    
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['sovrin-common==0.2.10', 'python-dateutil'],
+    install_requires=['sovrin-common==0.2.11', 'python-dateutil'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'sovrin-client==0.3.19'],
     scripts=['scripts/start_sovrin_node',
