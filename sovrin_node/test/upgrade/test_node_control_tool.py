5b5ac57468940b91bf679ede846b8a4d28f9a814
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-06-20 18:09:43 +0300

    Added tests for node_control_tool

--- sovrin_node/test/upgrade/test_node_control_tool.py
+++ sovrin_node/test/upgrade/test_node_control_tool.py
@@ -57,14 +57,14 @@ def testNodeControlPerformsMigrations(monkeypatch, tdir, looper):
 def testNodeControlCreatesBackups(monkeypatch, tdir, looper):
     msg = 'test'
     stdout = 'teststdout'
-    current_version = Upgrader.getVersion()
+    currentVersion = Upgrader.getVersion()
 
     def transform(tool):
         nodeControlGeneralMonkeypatching(tool, monkeypatch, tdir, stdout)
         monkeypatch.setattr(tool, '_remove_backup', lambda *x: None)
 
     def checkBackup(tool):
-        assert os.path.isfile('{}.{}'.format(tool._backup_name(current_version), tool.backup_format))
+        assert os.path.isfile('{}.{}'.format(tool._backup_name(currentVersion), tool.backup_format))
     nct = NCT(transform = transform)
     try:
         sendUpgradeMessage(msg)
@@ -76,11 +76,11 @@ def testNodeControlCreatesBackups(monkeypatch, tdir, looper):
 def testNodeControlRemovesBackups(monkeypatch, tdir, looper):
     msg = 'test'
     stdout = 'teststdout'
-    current_version = Upgrader.getVersion()
-    backup_was_removed = m.Value('b', False)
+    currentVersion = Upgrader.getVersion()
+    backupWasRemoved = m.Value('b', False)
 
     def testRemoveBackup(tool, version):
-        backup_was_removed.value = True
+        backupWasRemoved.value = True
         tool._remove_backup_test(version)
 
     def transform(tool):
@@ -89,13 +89,47 @@ def testNodeControlRemovesBackups(monkeypatch, tdir, looper):
         monkeypatch.setattr(tool, '_remove_backup', functools.partial(testRemoveBackup, tool))
 
     def checkBackupRemoved(tool):
-        assert backup_was_removed.value
+        assert backupWasRemoved.value
 
     nct = NCT(transform = transform)
     try:
         sendUpgradeMessage(msg)
         looper.run(eventually(checkBackupRemoved, nct.tool))
-        assert not os.path.exists('{}.{}'.format(nct.tool._backup_name(current_version), nct.tool.backup_format))
+        assert not os.path.exists('{}.{}'.format(nct.tool._backup_name(currentVersion), nct.tool.backup_format))
+    finally:
+        nct.stop()
+
+
+def testNodeControlRestoresFromBackups(monkeypatch, tdir, looper):
+    msg = 'test'
+    stdout = 'teststdout'
+    currentVersion = Upgrader.getVersion()
+    backupWasRestored = m.Value('b', False)
+    testFile = 'testFile'
+
+    def testRestoreBackup(tool, version):
+        backupWasRestored.value = True
+        tool._restore_from_backup_test(version)
+
+    def mockMigrate(tool):
+        with open(os.path.join(tool.sovrin_dir, testFile), 'w') as f:
+            f.write('random')
+        raise Exception('test')
+
+    def transform(tool):
+        nodeControlGeneralMonkeypatching(tool, monkeypatch, tdir, stdout)
+        tool._restore_from_backup_test = tool._restore_from_backup
+        monkeypatch.setattr(tool, '_migrate', functools.partial(mockMigrate, tool))
+        monkeypatch.setattr(tool, '_restore_from_backup', functools.partial(testRestoreBackup, tool))
+
+    def checkBackupRestored(tool):
+        assert backupWasRestored.value
+
+    nct = NCT(transform = transform)
+    try:
+        sendUpgradeMessage(msg)
+        looper.run(eventually(checkBackupRestored, nct.tool))
+        assert not os.path.exists(os.path.join(nct.tool.sovrin_dir, testFile))
     finally:
         nct.stop()
 
