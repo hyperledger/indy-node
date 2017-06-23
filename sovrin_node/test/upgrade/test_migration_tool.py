e9a6c9f652a6d2f79e09d2ce30e226b2eb26bfe0
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-06-23 14:57:21 +0300

    Fixed migration_tool tests

--- sovrin_node/test/upgrade/test_migration_tool.py
+++ sovrin_node/test/upgrade/test_migration_tool.py
@@ -1,14 +1,15 @@
 import sovrin_node.utils.migration_tool as migration_tool
 
 
-TEST_MIGRATION_SCRIPTS = ['0_3_100_0_test', '10_2_1_test1', '0_9_2_test3', '0_3_100_1_test4', '0_1_10_2_test5']
+TEST_MIGRATION_SCRIPTS = ['0_3_100_to_0_3_104', '10_2_1_to_10_0_2', '0_9_2_to_0_9_10', '0_3_100_to_0_3_101', '0_1_10_to_0_9_10']
 TEST_VERSION = '0.3.100'
+TEST_NEW_VERSION = '0.10.105'
 
 
 def testGetRelevantMigrations():
-    relevantMigrations = migration_tool._get_relevant_migrations(TEST_MIGRATION_SCRIPTS, TEST_VERSION)
-    assert len(relevantMigrations) == 4
-    assert relevantMigrations == ['0_3_100_0_test', '0_3_100_1_test4', '0_9_2_test3', '10_2_1_test1']
+    relevantMigrations = migration_tool._get_relevant_migrations(TEST_MIGRATION_SCRIPTS, TEST_VERSION, TEST_NEW_VERSION)
+    assert len(relevantMigrations) == 3
+    assert relevantMigrations == ['0_3_100_to_0_3_104', '0_9_2_to_0_9_10', '0_3_100_to_0_3_101']
 
 
 def testMigrate(monkeypatch):
@@ -17,7 +18,7 @@ def testMigrate(monkeypatch):
     monkeypatch.setattr(migration_tool, '_call_migration_script', testList.append)
     monkeypatch.setattr(migration_tool, '_get_migration_scripts', lambda: TEST_MIGRATION_SCRIPTS)
 
-    assert migration_tool.migrate(TEST_VERSION) == 4
-    assert len(testList) == 4
+    assert migration_tool.migrate(TEST_VERSION, TEST_NEW_VERSION) == 3
+    assert len(testList) == 3
 
 
