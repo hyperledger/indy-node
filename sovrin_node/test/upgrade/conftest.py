a146207d89a178cdc7ef6cb1a983823c5fab8359
Dmitry Surnin <dmitry.surnin@dsr-company.com>
2017-06-23 14:38:52 +0300

    Add tests and change sovrin-common version

--- sovrin_node/test/upgrade/conftest.py
+++ sovrin_node/test/upgrade/conftest.py
@@ -5,9 +5,9 @@ import pytest
 from plenum.common.constants import VERSION, STEWARD
 from sovrin_client.test.helper import getClientAddedWithRole
 
-from sovrin_common.constants import START
+from sovrin_common.constants import START, FORCE
 from sovrin_node.test import waits
-from sovrin_node.test.upgrade.helper import bumpedVersion, ensureUpgradeSent, checkUpgradeScheduled
+from sovrin_node.test.upgrade.helper import bumpedVersion, ensureUpgradeSent, checkUpgradeScheduled, bumpVersion
 from stp_core.loop.eventually import eventually
 
 
@@ -29,6 +29,22 @@ def validUpgrade(nodeIds, tconf):
                 schedule=schedule, sha256='aad1242', timeout=1)
 
 
+@pytest.fixture(scope='module')
+def validUpgradeExpForceFalse(validUpgrade):
+    nup = validUpgrade.copy()
+    nup.update({FORCE: False})
+    nup.update({VERSION: bumpVersion(validUpgrade[VERSION])})
+    return nup
+
+
+@pytest.fixture(scope='module')
+def validUpgradeExpForceTrue(validUpgradeExpForceFalse):
+    nup = validUpgradeExpForceFalse.copy()
+    nup.update({FORCE: True})
+    nup.update({VERSION: bumpVersion(validUpgradeExpForceFalse[VERSION])})
+    return nup
+
+
 @pytest.fixture(scope="module")
 def validUpgradeSent(looper, nodeSet, tdir, trustee, trusteeWallet,
                      validUpgrade):
@@ -36,9 +52,34 @@ def validUpgradeSent(looper, nodeSet, tdir, trustee, trusteeWallet,
 
 
 @pytest.fixture(scope="module")
+def validUpgradeSentExpForceFalse(looper, nodeSet, tdir, trustee, trusteeWallet,
+                                  validUpgradeExpForceFalse):
+    ensureUpgradeSent(looper, trustee, trusteeWallet, validUpgradeExpForceFalse)
+
+
+@pytest.fixture(scope="module")
+def validUpgradeSentExpForceTrue(looper, nodeSet, tdir, trustee, trusteeWallet,
+                                 validUpgradeExpForceTrue):
+    ensureUpgradeSent(looper, trustee, trusteeWallet, validUpgradeExpForceTrue)
+
+
+@pytest.fixture(scope="module")
 def upgradeScheduled(validUpgradeSent, looper, nodeSet, validUpgrade):
     looper.run(eventually(checkUpgradeScheduled, nodeSet, validUpgrade[VERSION],
                           retryWait=1, timeout=waits.expectedUpgradeScheduled()))
+
+
+@pytest.fixture(scope="module")
+def upgradeScheduledExpForceFalse(validUpgradeSentExpForceFalse, looper, nodeSet, validUpgradeExpForceFalse):
+    looper.run(eventually(checkUpgradeScheduled, nodeSet, validUpgradeExpForceFalse[VERSION],
+                          retryWait=1, timeout=waits.expectedUpgradeScheduled()))
+
+
+@pytest.fixture(scope="module")
+def upgradeScheduledExpForceTrue(validUpgradeSentExpForceTrue, looper, nodeSet, validUpgradeExpForceTrue):
+    looper.run(eventually(checkUpgradeScheduled, nodeSet, validUpgradeExpForceTrue[VERSION],
+                          retryWait=1, timeout=waits.expectedUpgradeScheduled()))
+
 
 @pytest.fixture(scope='module')
 def invalidUpgrade(nodeIds, tconf):
