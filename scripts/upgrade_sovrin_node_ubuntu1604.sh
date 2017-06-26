507d2a6e729bd258ccceb5afc2648c1f4765ff79
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-06-26 12:23:06 +0300

    Fixed node_control_tool bug

--- scripts/upgrade_sovrin_node_ubuntu1604.sh
+++ scripts/upgrade_sovrin_node_ubuntu1604.sh
@@ -6,7 +6,7 @@ if [ -z "$deps" ] ; then
 fi
 
 echo "Try to donwload sovrin version $deps"
-apt-get -y update && apt-get --download-only -y install $deps
+apt-get -y update && apt-get --download-only -y --allow-downgrades install $deps
 ret=$?
 if [ $ret -ne 0 ] ; then
   echo "Failed to obtain $deps"
@@ -17,7 +17,7 @@ echo "Stop sovrin-node"
 systemctl stop sovrin-node
 
 echo "Run sovrin upgrade to $deps"
-apt-get -y install $deps
+apt-get -y --allow-downgrades install $deps
 ret=$?
 if [ $ret -ne 0 ] ; then
   echo "Upgrade to $deps failed"
