8bd8c50e41a52bfdbc3343b0afb195319b125a54
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-07-09 16:46:05 +0300

    [WIP] Repo merge
    Merged in sovrin-client, sovrin-common

--- sovrin_client/__metadata__.py
+++
@@ -1,9 +0,0 @@
-"""
-sovrin-client package metadata
-"""
-__version_info__ = (0, 4)
-__version__ = '.'.join(map(str, __version_info__))
-__author__ = "Sovrin Foundation."
-__license__ = "Apache 2.0"
-
-__all__ = ['__version_info__', '__version__', '__author__', '__license__']
