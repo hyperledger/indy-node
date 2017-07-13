8d3d221a1c033a86decd12dbfdb690ee87310866
Artem Ivanov <Artemka1528@yandex.ru>
2017-07-13 10:43:18 +0300

    Updated anoncreds version

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
     install_requires=['plenum-dev==0.4.39',
-                      'anoncreds-dev==0.4.10',
+                      'anoncreds-dev==0.4.11',
                       'python-dateutil',
                       'timeout-decorator'],
     setup_requires=['pytest-runner'],
