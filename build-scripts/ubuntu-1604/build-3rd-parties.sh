d5c73aa36ffe480d5e429d69b4cf2a48471e899a
Alexander Shekhovtsov <alexander.shekhovcov@dsr-company.com>
2017-07-17 16:47:46 +0300

    Add build scripts

---
+++ build-scripts/ubuntu-1604/build-3rd-parties.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+OUTPUT_PATH=${1:-.}
+
+function build_from_pypi {
+    PACKAGE_NAME=$1
+
+    if [ -z $2 ]; then
+        PACKAGE_VERSION=""
+    else
+        PACKAGE_VERSION="==$2"
+    fi
+    POSTINST_TMP=postinst-${PACKAGE_NAME}
+    PREREM_TMP=prerm-${PACKAGE_NAME}
+    cp postinst ${POSTINST_TMP}
+    cp prerm ${PREREM_TMP}
+    sed -i 's/{package_name}/python3-'${PACKAGE_NAME}'/' ${POSTINST_TMP}
+    sed -i 's/{package_name}/python3-'${PACKAGE_NAME}'/' ${PREREM_TMP}
+
+    fpm --input-type "python" \
+        --output-type "deb" \
+        --architecture "amd64" \
+        --verbose \
+        --python-package-name-prefix "python3"\
+        --python-bin "/usr/bin/python3" \
+        --exclude "*.pyc" \
+        --exclude "*.pyo" \
+        --after-install ${POSTINST_TMP} \
+        --before-remove ${PREREM_TMP} \
+        --maintainer "Evernym <dev@evernym.com>" \
+        --package ${OUTPUT_PATH} \
+        ${PACKAGE_NAME}${PACKAGE_VERSION}
+
+    rm ${POSTINST_TMP}
+    rm ${PREREM_TMP}
+}
+
+build_from_pypi timeout-decorator
+
+
