591d2df1ac6d5f1035d15426c15b2d6d3cbf4817
Alexander Shekhovtsov <alexander.shekhovcov@dsr-company.com>
2017-07-18 13:27:08 +0300

    Update author_email

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     description='Sovrin node',
     url='https://github.com/hyperledger/indy-node',
     author=__author__,
-    author_email='dev@evernym.us',
+    author_email='repo@sovrin.org',
     license=__license__,
     keywords='Sovrin Node',
     packages=find_packages(exclude=['docs', 'docs*']) + [
