7dacd9420a53150189ebdd70a4efc9b00f0f841d
Lovesh Harchandani <lovesh@users.noreply.github.com>
2017-07-21 17:32:43 +0530

    test to reproduce bug and changes in load test script (#252)
    * test to reproduce bug and changes in load test script
    
    * Update setup.py

--- scripts/load_multi.py
+++ scripts/load_multi.py
@@ -1,18 +1,17 @@
+from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import Pool, Process
 
 from scripts.load import put_load
 
-num_processes = 10
+num_tasks = 100
+use_processes = False
 
 
 if __name__ == '__main__':
-    # with Pool(num_processes) as p:
-    #     p.map(put_load, [])
-    processes = []
-    for _ in range(num_processes):
-        p = Process(target=put_load, )
-        p.start()
-        processes.append(p)
 
-    for p in processes:
-        p.join()
+    executor = ProcessPoolExecutor if use_processes else ThreadPoolExecutor
+
+    with executor(max_workers=10) as e:
+        for _ in range(num_tasks):
+            e.submit(put_load)
