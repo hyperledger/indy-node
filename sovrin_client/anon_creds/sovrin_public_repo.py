3403a193577d739107f9feed0d4e7f95a41974db
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-08-08 18:15:43 +0300

    support backward-compatibility

--- sovrin_client/anon_creds/sovrin_public_repo.py
+++ sovrin_client/anon_creds/sovrin_public_repo.py
@@ -34,6 +34,10 @@ def _ensureReqCompleted(reqKey, client, clbk):
 
 def _getData(result, error):
     data = result.get(DATA) if result.get(DATA) else {}
+    # TODO: we have an old txn in the live pool where DATA is stored a json string.
+    # We can get rid of the code above once we create a versioning support in txns
+    if isinstance(data, str):
+        data = json.loads(data)
     seqNo = result.get(F.seqNo.name)
     return data, seqNo
 
