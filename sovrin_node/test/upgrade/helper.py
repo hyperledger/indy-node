1692570b9fa4e8d8cb207286574326833c27fd39
Andrei Goncharov <frantic1993@gmail.com>
2017-08-08 16:59:01 +0300

    Indy 316 (#284)
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Initial draft
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Added storing of 10 last backups
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Test fix, refactoring
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Test fix
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Test fix
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Test fix
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Process ledger bug fix
    Refactoring
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Ledger processing refactored
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Bug fix
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Test fix
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Added upgrade_id
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Added tests for loop
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Fixed timeout test
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Made NAME required for POOL_UPGRADE
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    Fixed failing tests
    
    * [WIP] Feature Added: Sending a POOL_UPGRADE to a version that does not exist puts the pool into a continuous upgrade loop
    
    DRY refactoring

--- sovrin_node/test/upgrade/helper.py
+++ sovrin_node/test/upgrade/helper.py
@@ -2,15 +2,24 @@ from plenum.common.util import randomString
 from stp_core.loop.eventually import eventually
 from plenum.test.helper import waitForSufficientRepliesForRequests
 from plenum.test import waits as plenumWaits
+from plenum.common.types import f
+from plenum.common.constants import TXN_TYPE, DATA
+from sovrin_common.constants import NODE_UPGRADE, ACTION
 from sovrin_client.client.wallet.upgrade import Upgrade
 from sovrin_node.server.upgrader import Upgrader
+from sovrin_node.server.upgrade_log import UpgradeLog
 from sovrin_node.utils.node_control_tool import NodeControlTool
 from sovrin_common.config_util import getConfig
+from datetime import datetime
+from typing import List, Tuple
+import dateutil.tz
 import subprocess
 import os
 import multiprocessing
 import socket
 import json
+import functools
+
 
 
 config = getConfig()
@@ -102,3 +111,66 @@ def nodeControlGeneralMonkeypatching(tool, monkeypatch, tdir, stdout):
 
 def get_valid_code_hash():
     return randomString(64)
+
+
+def populate_log_with_upgrade_events(tdir_with_pool_txns, pool_txn_node_names, tconf,
+                                      version: Tuple[str, str, str]):
+    for nm in pool_txn_node_names:
+        path = os.path.join(tdir_with_pool_txns, tconf.nodeDataDir, nm)
+        os.makedirs(path)
+        log = UpgradeLog(os.path.join(path, tconf.upgradeLogFile))
+        when = datetime.utcnow().replace(tzinfo=dateutil.tz.tzutc())
+        log.appendScheduled(when, version, randomString(10))
+        log.appendStarted(when, version, randomString(10))
+
+
+def check_node_set_acknowledges_upgrade(looper, node_set, node_ids, allowed_actions: List,
+                                        version: Tuple[str, str, str]):
+    check = functools.partial(check_ledger_after_upgrade, node_set, allowed_actions, node_ids=node_ids)
+
+    for node in node_set:
+        node.upgrader.scheduledUpgrade = (version, 0, randomString(10))
+        node.notify_upgrade_start()
+        node.upgrader.scheduledUpgrade = None
+
+    timeout = plenumWaits.expectedTransactionExecutionTime(len(node_set))
+    looper.run(eventually(functools.partial(check, ledger_size=len(node_set)), retryWait=1, timeout=timeout))
+
+    for node in node_set:
+        node.acknowledge_upgrade()
+
+    looper.run(eventually(functools.partial(check, ledger_size=2 * len(node_set)), retryWait=1, timeout=timeout))
+
+
+def check_ledger_after_upgrade(node_set, allowed_actions, ledger_size, node_ids=None, allowed_txn_types=[NODE_UPGRADE]):
+    for node in node_set:
+        print(len(node.configLedger))
+        assert len(node.configLedger) == ledger_size
+        ids = set()
+        for _, txn in node.configLedger.getAllTxn():
+            type = txn[TXN_TYPE]
+            assert type in allowed_txn_types
+            data = txn
+            if type == NODE_UPGRADE:
+                data = txn[DATA]
+            assert data[ACTION] in allowed_actions
+            ids.add(txn[f.IDENTIFIER.nm])
+        ids.add(node.id)
+
+        if node_ids:
+            assert ids == set(node_ids)
+
+
+def check_no_loop(nodeSet, event):
+    for node in nodeSet:
+        # mimicking upgrade start
+        node.upgrader._upgradeLog.appendStarted(0, node.upgrader.scheduledUpgrade[0], node.upgrader.scheduledUpgrade[2])
+        node.notify_upgrade_start()
+        # mimicking upgrader's initialization after restart
+        node.upgrader.check_upgrade_succeeded()
+        node.upgrader.scheduledUpgrade = None
+        assert node.upgrader._upgradeLog.lastEvent[1] == event
+        # mimicking node's catchup after restart
+        node.postConfigLedgerCaughtUp()
+        assert node.upgrader.scheduledUpgrade is None
+        assert node.upgrader._upgradeLog.lastEvent[1] == event
