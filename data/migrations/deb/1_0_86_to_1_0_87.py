2303715d0cb64de20b4833800e21e681709431c3
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-08-09 21:02:50 +0300

    add migration script

--- data/migrations/deb/1_0_86_to_1_0_87.py
+++ data/migrations/deb/1_0_86_to_1_0_87.py
@@ -8,6 +8,7 @@ from common.serializers.mapping_serializer import MappingSerializer
 from ledger.compact_merkle_tree import CompactMerkleTree
 from ledger.ledger import Ledger
 from plenum.persistence.leveldb_hash_store import LevelDbHashStore
+from storage.chunked_file_store import ChunkedFileStore
 
 from sovrin_common.config_util import getConfig
 from sovrin_common.txn_util import getTxnOrderedFields
@@ -26,11 +27,17 @@ def migrate_ledger(data_directory,
         return
 
     # open the current ledger using the specified serializer
+    old_txn_log_store = ChunkedFileStore(data_directory,
+                                         old_ledger_file,
+                                         isLineNoKey=True,
+                                         storeContentHash=False)
     old_ledger = Ledger(CompactMerkleTree(),
                         dataDir=data_directory,
                         txn_serializer=serializer,
                         hash_serializer=serializer,
-                        fileName=old_ledger_file)
+                        fileName=old_ledger_file,
+                        transactionLogStore=old_txn_log_store)
+    print("old size for {}: {}".format(old_ledger_file, str(old_ledger.size)))
 
     # open the new ledger with new serialization
     new_ledger_file_backup = new_ledger_file + "_new"
@@ -41,18 +48,27 @@ def migrate_ledger(data_directory,
     # add all txns into the new ledger
     for _, txn in old_ledger.getAllTxn():
         new_ledger.add(txn)
+    print("new size for {}: {}".format(new_ledger_file, str(new_ledger.size)))
 
     old_ledger.stop()
     new_ledger.stop()
 
-    # now that everything succeeded, backup the old files and move the new files into place
-    old_ledger_file_backup = old_ledger_file + "_old"
-    os.rename(
-        os.path.join(data_directory, old_ledger_file),
-        os.path.join(data_directory, old_ledger_file_backup))
+    # now that everything succeeded, remove the old files and move the new files into place
+    shutil.rmtree(
+        os.path.join(data_directory, old_ledger_file))
     os.rename(
         os.path.join(data_directory, new_ledger_file_backup),
         os.path.join(data_directory, new_ledger_file))
+
+
+def open_new_ledger(data_directory, new_ledger_file, hash_store_name):
+    # open new Ledger with leveldb hast store (to re-init it)
+    new_ledger = Ledger(CompactMerkleTree(
+        hashStore=LevelDbHashStore(
+            dataDir=data_directory, fileNamePrefix=hash_store_name)),
+        dataDir=data_directory,
+        fileName=new_ledger_file)
+    new_ledger.stop()
 
 
 def migrate_all_hash_stores(data_directory):
@@ -66,6 +82,15 @@ def migrate_all_hash_stores(data_directory):
         os.path.join(data_directory, '_merkleNodes.bin'))
     os.remove(
         os.path.join(data_directory, '_merkleLeaves.bin'))
+    os.remove(
+        os.path.join(data_directory, 'config_merkleNodes.bin'))
+    os.remove(
+        os.path.join(data_directory, 'config_merkleLeaves.bin'))
+
+    # open new Ledgers
+    open_new_ledger(data_directory, config.poolTransactionsFile, 'pool')
+    open_new_ledger(data_directory, config.domainTransactionsFile, 'domain')
+    open_new_ledger(data_directory, config.configTransactionsFile, 'config')
 
 
 def migrate_all_ledgers_for_node(node_data_directory):
@@ -88,9 +113,7 @@ if __name__ == "__main__":
     config = getConfig()
     base_dir = config.baseDir
     nodes_data_dir = os.path.join(base_dir, config.nodeDataDir)
-    for (_, dirnames, _) in os.walk(nodes_data_dir):
-        for node_dir in dirnames:
-            node_data_dir = os.path.join(nodes_data_dir, node_dir)
-            migrate_all_ledgers_for_node(node_data_dir)
-            migrate_all_hash_stores(node_data_dir)
-
+    for node_dir in os.listdir(nodes_data_dir):
+        node_data_dir = os.path.join(nodes_data_dir, node_dir)
+        migrate_all_ledgers_for_node(node_data_dir)
+        migrate_all_hash_stores(node_data_dir)
