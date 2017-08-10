92e3d02c30f17e53cbcae9747482a902bcec812e
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-08-10 18:11:48 +0300

    add a script to read txns from the ledger

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum-dev==1.0.84',
+    install_requires=['indy-plenum-dev',
                       'indy-anoncreds-dev==1.0.25',
                       'python-dateutil',
                       'timeout-decorator'],
@@ -110,7 +110,8 @@ setup(
              'scripts/install_sovrin_node.bat',
              'scripts/delete_sovrin_node.bat',
              'scripts/restart_upgrade_agent.bat',
-             'scripts/install_nssm.bat'],
+             'scripts/install_nssm.bat',
+             'scripts/read_ledger'],
     cmdclass={
         'install': EnhancedInstall,
         'develop': EnhancedInstallDev
