7f6558c9938c1dc214134fec7abe6f023889ccc7
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-08-11 13:10:10 +0300

    Code review fixes

--- sovrin_node/test/upgrade/test_upgrader.py
+++ sovrin_node/test/upgrade/test_upgrader.py
@@ -2,11 +2,17 @@
 from sovrin_node.server.upgrader import Upgrader
 
 
+def comparator_test(lower_versrion, higher_version):
+    assert Upgrader.compareVersions(lower_versrion, higher_version) == 1
+    assert Upgrader.compareVersions(higher_version, lower_versrion) == -1
+    assert Upgrader.compareVersions(higher_version, higher_version) == 0
+    assert not Upgrader.is_version_upgradable(higher_version, higher_version)
+    assert Upgrader.is_version_upgradable(higher_version, higher_version, reinstall=True)
+    assert Upgrader.is_version_upgradable(lower_versrion, higher_version)
+    assert Upgrader.is_version_upgradable(higher_version, lower_versrion)
+
+
 def testVersions():
-    assert Upgrader.compareVersions('0.0.5', '0.0.6') == 1
-    assert Upgrader.compareVersions('0.0.6', '0.0.5') == -1
-    assert Upgrader.compareVersions('0.0.6', '0.0.6') == 0
-    assert not Upgrader.is_version_upgradable('0.0.6', '0.0.6')
-    assert Upgrader.is_version_upgradable('0.0.6', '0.0.6', reinstall=True)
-    assert Upgrader.is_version_upgradable('0.0.5', '0.0.6')
-    assert Upgrader.is_version_upgradable('0.0.6', '0.0.5')
+    comparator_test('0.0.5', '0.0.6')
+    comparator_test('0.1.2', '0.2.6')
+    comparator_test('1.10.2', '2.0.6')
