de2803b78700ee8ec9678cbc8983ba2328e907b8
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-08-11 14:17:37 +0300

    increment plenum

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum-dev==1.0.84',
+    install_requires=['indy-plenum-dev==1.0.90',
                       'indy-anoncreds-dev==1.0.25',
                       'python-dateutil',
                       'timeout-decorator'],
