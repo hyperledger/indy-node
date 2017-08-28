9a79bada6046032d05705b0c6646ee6f313bc8b0
Alexander Shekhovtsov <alexander.shekhovcov@dsr-company.com>
2017-08-28 13:47:26 +0300

    Make node status tool safe

--- sovrin_node/test/validator_info/test_validator_info.py
+++ sovrin_node/test/validator_info/test_validator_info.py
@@ -1,6 +1,6 @@
 # noinspection PyUnresolvedReferences
 from plenum.test.validator_info.test_validator_info import patched_dump_info_period, \
-    info_path, info  # qa
+    info_path, info, load_latest_info, node  # qa
 
 
 TEST_NODE_NAME = 'Alpha'
@@ -14,3 +14,12 @@ def test_validator_info_file_schema_is_valid(info):
 
 def test_validator_info_file_metrics_count_ledger_field_valid(info):
     assert info['metrics']['transaction-count']['config'] == 0
+
+
+def test_validator_info_file_handle_fails(info,
+                                          node,
+                                          load_latest_info):
+    node._info_tool._node = None
+    latest_info = load_latest_info()
+
+    assert latest_info['metrics']['transaction-count']['config'] is None
