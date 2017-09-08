dbdcd2654a8b7108d5c6fb2abc0cd38c5954ceee
dsurnin <dmitry.surnin@dsr-corporation.com>
2017-09-08 15:08:29 +0300

    Add additional txn length check based on txn field size (#341)
    * Add additional txn length check based on txn field size
    
    * Remove duplicated import

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum-dev==1.1.121',
+    install_requires=['indy-plenum-dev==1.1.123',
                       'indy-anoncreds-dev==1.0.25',
                       'python-dateutil',
                       'timeout-decorator'],
