c093512921b8fb8e1af119fc7ccecba8d753d1ce
Nikita Spivachuk <nikita.spivachuk@dsr-company.com>
2017-09-13 21:11:53 +0300

    Test Automation: - Added a test for Acceptance Test Scenario 9 (Remove / Add Roles). - Removed example system tests. - Corrected setup.py file. - Updated python3-indy dependency.

--- acceptance/setup.py
+++ acceptance/setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, __version__
+from setuptools import setup
 
 setup(
     name='indy-node-acceptance',
-    version=__version__,
+    version='0.0.1',
     description='Acceptance tests for Hyperledger Indy-Node',
     url='https://github.com/hyperledger/indy-node/tree/master/acceptance',
     author='Nikita Spivachuk',
-    author_email='hyperledger-indy@lists.hyperledger.org',
+    author_email='nikita.spivachuk@dsr-company.com',
     license='MIT/Apache-2.0',
-    install_requires=['python3-indy==1.0.0-dev-171', 'pytest', 'pytest-asyncio',
+    install_requires=['python3-indy==1.0.0-dev-177', 'pytest', 'pytest-asyncio',
                       'pytest-runner'],
     setup_requires=['pytest-runner']
 )
