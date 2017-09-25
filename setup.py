6a08959c1e4fcdd07de592db65017951d7935e63
Alexander Shcherbakov <alexander.sherbakov@dsr-company.com>
2017-09-25 17:50:56 +0300

    INDY-670, INDY-790: Multi-signatures and state proofs support (#358)
    * add test for state proof in get_attr request
    
    * add constants for state proofs
    
    * add test for state proof in get_claim_def
    
    * add tet for state proofs in get_schema
    
    * add state proof to replies for get_attr, get_schema and get_claim_def transactions
    
    * make get_nym return state proof
    
    * base58 proof nodes and root_hash in state proof
    
    * conert root_hash to bytes before encoding to base58
    
    * do not add ORIGIN to data for get_schema reply
    
    * pass bls hash store to DomainReqHandler in state proof test
    
    * add multisignature for root for get requests
    
    * add test that checks that state proof is returned for get_attr request
    
    * return multisignature in state_proof
    
    * use state proof constants from plenum
    
    * add seqNo to the data passed to NYM request handling function
    
    * Add seqNo to reply for get_nym request
    
    On the same level as identifier, data and state proof to make it look like replies to other get_* requests
    
    * use nymData instead of data to get seqno in get_nym
    
    * do not take issuer from schemas data
    
    * add generation of bls keys to init_sovrin_keys
    
    * make IdrCache return None instead of empty string
    
    * fix formatting
    
    * add test for state proof in get_nym
    
    * add divider to schema path to avoid collisions
    
    * make IdrCache store seqno of nym
    
    * remove setVerkey and setRole methods
    
    it is better to always use set instead
    
    * domain_req_handler should work with seqno in nym
    
    * update tests of state proof
    
    * use base64 for proof nodes instead of base58
    
    because it works much quicker
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * blskey support, fix tests
    
    * fix tests
    
    * fix getting seq_no from None
    
    * fixed tests
    
    * add txn_time to state for all txn except nym
    
    * send nym request in test_state_proof_returned_for_get_nym
    
    * switch off checking of TXN_TIME
    
    * save txn_time in idr cache
    
    * fix state proof test
    
    * add test for blskey in send NODE CLI command
    
    * use initNodeKeysForBothStacks in init_sovrin_keys
    
    * fix API
    
    * add migration script
    
    * increment plenum version
    
    * fix BLS keys in tests
    
    * fix static code validation errors
    
    * support indy-crypto in CI
    
    * Add indy-crypto dependency to documentation
    
    * fix tests
    
    * improve tests
    
    * increment plenum version

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum-dev==1.1.128',
+    install_requires=['indy-plenum-dev==1.1.134',
                       'indy-anoncreds-dev==1.0.25',
                       'python-dateutil',
                       'timeout-decorator'],
