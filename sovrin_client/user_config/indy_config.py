1cb701bea7a626c7fe44e1f9132c0011275eaf58
Sergey Shilov <sergey.shilov@dsr-company.com>
2017-10-03 19:13:46 +0300

    Add CLI support.

---
+++ sovrin_client/user_config/indy_config.py
@@ -0,0 +1,17 @@
+# Current network
+CURRENT_NETWORK = 'none'
+
+# Directory to store ledger.
+LEDGER_DIR = '~/.indy-cli'
+
+# Directory to store logs.
+LOG_DIR = '~/.indy-cli'
+
+# Directory to store keys.
+KEYS_DIR = '~/.indy-cli'
+
+# Directory to store genesis transactions files.
+GENESIS_DIR = '~/.indy-cli/networks'
+
+# Directory to store wallets.
+WALLETS_DIR = '~/.indy-cli/wallets'
