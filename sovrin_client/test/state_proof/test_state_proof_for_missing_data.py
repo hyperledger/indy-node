394ea0f46f3209c93799880c875fb87c0003bbad
Victor Muzychenko <mzkvct@gmail.com>
2017-10-04 01:22:31 +0300

    update test for missing data
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>

--- sovrin_client/test/state_proof/test_state_proof_for_missing_data.py
+++ sovrin_client/test/state_proof/test_state_proof_for_missing_data.py
@@ -1,15 +1,16 @@
-from plenum.common.constants import TARGET_NYM, TXN_TYPE, RAW
+from plenum.common.constants import TARGET_NYM, TXN_TYPE, RAW, DATA, STATE_PROOF, ROOT_HASH, MULTI_SIGNATURE, \
+    PROOF_NODES
 from plenum.test.helper import waitForSufficientRepliesForRequests, getRepliesFromClientInbox
+from sovrin_common.serialization import attrib_raw_data_serializer
 
 from sovrin_common.constants import GET_ATTR
 
 # fixtures, do not remove
-from sovrin_client.test.test_nym_attrib import attributeName, attributeData, attributeValue
+from sovrin_client.test.test_nym_attrib import attributeName
 
 
 def test_state_proof_returned_for_missing_attr(looper,
                                                attributeName,
-                                               attributeData,
                                                trustAnchor,
                                                trustAnchorWallet):
     """
@@ -29,22 +30,18 @@ def test_state_proof_returned_for_missing_attr(looper,
     replies = getRepliesFromClientInbox(client.inBox, get_attr_request.reqId)
     for reply in replies:
         print(reply)
+    for reply in replies:
+        result = reply['result']
+        assert DATA not in result or result[DATA] is None
+        assert STATE_PROOF in result
+        state_proof = result[STATE_PROOF]
+        assert ROOT_HASH in state_proof
+        assert state_proof[ROOT_HASH]
+        assert MULTI_SIGNATURE in state_proof
+        assert state_proof[MULTI_SIGNATURE]
+        assert state_proof[MULTI_SIGNATURE]["participants"]
+        assert state_proof[MULTI_SIGNATURE]["pool_state_root"]
+        assert state_proof[MULTI_SIGNATURE]["signature"]
+        assert PROOF_NODES in state_proof
+        assert state_proof[PROOF_NODES]
 
-    # expected_data = attrib_raw_data_serializer.deserialize(attributeData)
-    # for reply in replies:
-    #     result = reply['result']
-    #     assert DATA in result
-    #     data = attrib_raw_data_serializer.deserialize(result[DATA])
-    #     assert data == expected_data
-    #     assert result[TXN_TIME]
-    #     assert STATE_PROOF in result
-    #     state_proof = result[STATE_PROOF]
-    #     assert ROOT_HASH in state_proof
-    #     assert state_proof[ROOT_HASH]
-    #     assert MULTI_SIGNATURE in state_proof
-    #     assert state_proof[MULTI_SIGNATURE]
-    #     assert state_proof[MULTI_SIGNATURE]["participants"]
-    #     assert state_proof[MULTI_SIGNATURE]["pool_state_root"]
-    #     assert state_proof[MULTI_SIGNATURE]["signature"]
-    #     assert PROOF_NODES in state_proof
-    #     assert state_proof[PROOF_NODES]
