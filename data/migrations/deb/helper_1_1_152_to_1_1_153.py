7ebb7121da99c6fbc68bb9d4639f91e15153dfd1
Victor Muzychenko <mzkvct@gmail.com>
2017-10-06 13:27:30 +0300

    make migration script be runned by indy user
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>

---
+++ data/migrations/deb/helper_1_1_152_to_1_1_153.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+
+from indy_common.config_util import getConfig
+from stp_core.common.log import getlogger
+
+config = getConfig()
+logger = getlogger()
+
+
+def _get_nodes_data_dir():
+    base_dir = config.baseDir
+    nodes_data_dir = os.path.join(base_dir, config.nodeDataDir)
+    if not os.path.exists(nodes_data_dir):
+        msg = \
+            'Can not find the directory with the ledger: {}'\
+            .format(nodes_data_dir)
+        logger.error(msg)
+        raise Exception(msg)
+    return nodes_data_dir
+
+
+def migrate_all_states(node_data_directory):
+    # the states will be recovered from the ledger during the start-up.
+    # just delete the current ones
+    shutil.rmtree(os.path.join(node_data_directory, 'pool_state'))
+    shutil.rmtree(os.path.join(node_data_directory, 'domain_state'))
+    shutil.rmtree(os.path.join(node_data_directory, 'config_state'))
+    shutil.rmtree(os.path.join(node_data_directory, 'idr_cache_db'))
+
+
+def migrate_all():
+    nodes_data_dir = _get_nodes_data_dir()
+    for node_dir in os.listdir(nodes_data_dir):
+        node_data_dir = os.path.join(nodes_data_dir, node_dir)
+        logger.info("Applying migration to {}".format(node_data_dir))
+        migrate_all_states(node_data_dir)
+
+
+migrate_all()
