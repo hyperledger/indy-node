24fd144323ca8c24966819792bf6fd53234e1eb6
Nikita Spivachuk <nikita.spivachuk@dsr-company.com>
2017-10-24 20:41:30 +0300

    INDY-917: Added a test for the fix
    - Added a test verifying that a canceled upgrade is not rescheduled after the node has been restarted.
    
    Signed-off-by: Nikita Spivachuk <nikita.spivachuk@dsr-company.com>

---
+++ indy_node/test/upgrade/test_node_does_not_reschedule_canceled_upgrade.py
@@ -0,0 +1,56 @@
+from copy import deepcopy
+
+from indy_common.constants import ACTION, CANCEL, SCHEDULE
+from indy_node.test import waits
+from indy_node.test.upgrade.helper import checkNoUpgradeScheduled, \
+    ensureUpgradeSent
+from plenum.test.test_node import checkNodesConnected, ensureElectionsDone
+from stp_core.loop.eventually import eventually
+
+
+def test_node_does_not_reschedule_cancelled_upgrade_after_restart(
+        upgradeScheduled, looper, nodeSet, validUpgrade,
+        testNodeClass, tdirWithPoolTxns, tconf, allPluginsPath,
+        trustee, trusteeWallet):
+
+    # Cancel the scheduled upgrade
+    valid_upgrade_cancel = deepcopy(validUpgrade)
+    valid_upgrade_cancel[ACTION] = CANCEL
+    del valid_upgrade_cancel[SCHEDULE]
+
+    ensureUpgradeSent(looper, trustee, trusteeWallet, valid_upgrade_cancel)
+
+    # Verify that no upgrade is scheduled now
+    looper.run(
+        eventually(
+            checkNoUpgradeScheduled,
+            nodeSet,
+            retryWait=1,
+            timeout=waits.expectedNoUpgradeScheduled()))
+
+    # Restart all the nodes
+    names = []
+    while nodeSet:
+        node = nodeSet.pop()
+        names.append(node.name)
+        node.cleanupOnStopping = False
+        looper.removeProdable(node)
+        node.stop()
+        del node
+
+    for nm in names:
+        node = testNodeClass(nm, basedirpath=tdirWithPoolTxns,
+                             config=tconf, pluginPaths=allPluginsPath)
+        looper.add(node)
+        nodeSet.append(node)
+
+    looper.run(checkNodesConnected(nodeSet))
+    ensureElectionsDone(looper=looper, nodes=nodeSet, retryWait=1)
+
+    # Verify that still no upgrade is scheduled
+    looper.run(
+        eventually(
+            checkNoUpgradeScheduled,
+            nodeSet,
+            retryWait=1,
+            timeout=waits.expectedNoUpgradeScheduled()))
