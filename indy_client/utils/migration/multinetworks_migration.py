f1707ecaf6194c07a696177aeb5eba0a37b55776
Nikita Spivachuk <nikita.spivachuk@dsr-company.com>
2017-11-03 19:11:18 +0300

    INDY-925: Client migration for multi-network
    - Commented out unsafe superfluous actions.
    
    Signed-off-by: Nikita Spivachuk <nikita.spivachuk@dsr-company.com>

--- indy_client/utils/migration/multinetworks_migration.py
+++ indy_client/utils/migration/multinetworks_migration.py
@@ -14,24 +14,24 @@ _CLIENTS = 'clients'
 _LEGACY_NETWORKS = ['live', 'local', 'sandbox']
 
 
-def _get_used_network_name():
-    old_config_path = os.path.join(_OLD_BASE_DIR, _CONFIG)
-    spec = spec_from_file_location('old_user_overrides', old_config_path)
-    old_user_overrides = module_from_spec(spec)
-    spec.loader.exec_module(old_user_overrides)
-
-    if hasattr(old_user_overrides, 'poolTransactionsFile'):
-        network_name = old_user_overrides.poolTransactionsFile.split('_')[-1]
-        if network_name in _LEGACY_NETWORKS:
-            return network_name
-
-    if hasattr(old_user_overrides, 'current_env') \
-            and old_user_overrides.current_env != 'test':
-        network_name = old_user_overrides.current_env
-        if network_name in _LEGACY_NETWORKS:
-            return network_name
-
-    return 'sandbox'
+# def _get_used_network_name():
+#     old_config_path = os.path.join(_OLD_BASE_DIR, _CONFIG)
+#     spec = spec_from_file_location('old_user_overrides', old_config_path)
+#     old_user_overrides = module_from_spec(spec)
+#     spec.loader.exec_module(old_user_overrides)
+#
+#     if hasattr(old_user_overrides, 'poolTransactionsFile'):
+#         network_name = old_user_overrides.poolTransactionsFile.split('_')[-1]
+#         if network_name in _LEGACY_NETWORKS:
+#             return network_name
+#
+#     if hasattr(old_user_overrides, 'current_env') \
+#             and old_user_overrides.current_env != 'test':
+#         network_name = old_user_overrides.current_env
+#         if network_name in _LEGACY_NETWORKS:
+#             return network_name
+#
+#     return 'sandbox'
 
 
 def _migrate_config():
@@ -74,47 +74,49 @@ def _migrate_wallets():
                       os.path.join(new_wallets_dir, 'sandbox'))
 
 
-def _migrate_keys(network):
-    old_data_dir = os.path.join(_OLD_BASE_DIR, _DATA, _CLIENTS)
-
-    for client in os.listdir(old_data_dir):
-        old_client_keys_dir = os.path.join(_OLD_BASE_DIR, client)
-        if os.path.isdir(old_client_keys_dir):
-            new_client_keys_dir = os.path.join(
-                _CLI_BASE_DIR, _NETWORKS, network, _KEYS, client)
-            shutil.copytree(old_client_keys_dir, new_client_keys_dir)
-
-
-def _migrate_data(network):
-    old_data_dir = os.path.join(_OLD_BASE_DIR, _DATA, _CLIENTS)
-
-    for client in os.listdir(old_data_dir):
-        old_client_data_dir = os.path.join(old_data_dir, client)
-        if os.path.isdir(old_client_data_dir):
-            new_client_data_dir = os.path.join(
-                _CLI_BASE_DIR, _NETWORKS, network, _DATA, _CLIENTS, client)
-            shutil.copytree(old_client_data_dir, new_client_data_dir)
-
-            sole_pool_txn_dir = os.path.join(new_client_data_dir,
-                                             'pool_transactions')
-            for specific_network in _LEGACY_NETWORKS:
-                specific_pool_txn_dir = \
-                    os.path.join(new_client_data_dir,
-                                 'pool_transactions_{}'.format(specific_network))
-                if os.path.isdir(specific_pool_txn_dir):
-                    if specific_network == network:
-                        os.rename(specific_pool_txn_dir, sole_pool_txn_dir)
-                    else:
-                        shutil.rmtree(specific_pool_txn_dir)
+# def _migrate_keys(network):
+#     old_data_dir = os.path.join(_OLD_BASE_DIR, _DATA, _CLIENTS)
+#
+#     for client in os.listdir(old_data_dir):
+#         old_client_keys_dir = os.path.join(_OLD_BASE_DIR, client)
+#         if os.path.isdir(old_client_keys_dir):
+#             new_client_keys_dir = os.path.join(
+#                 _CLI_BASE_DIR, _NETWORKS, network, _KEYS, client)
+#             shutil.copytree(old_client_keys_dir, new_client_keys_dir)
+#
+#
+# def _migrate_data(network):
+#     old_data_dir = os.path.join(_OLD_BASE_DIR, _DATA, _CLIENTS)
+#
+#     for client in os.listdir(old_data_dir):
+#         old_client_data_dir = os.path.join(old_data_dir, client)
+#         if os.path.isdir(old_client_data_dir):
+#             new_client_data_dir = os.path.join(
+#                 _CLI_BASE_DIR, _NETWORKS, network, _DATA, _CLIENTS, client)
+#             shutil.copytree(old_client_data_dir, new_client_data_dir)
+#
+#             sole_pool_txn_dir = os.path.join(new_client_data_dir,
+#                                              'pool_transactions')
+#             for specific_network in _LEGACY_NETWORKS:
+#                 specific_pool_txn_dir = \
+#                     os.path.join(new_client_data_dir,
+#                                  'pool_transactions_{}'.format(specific_network))
+#                 if os.path.isdir(specific_pool_txn_dir):
+#                     if specific_network == network:
+#                         os.rename(specific_pool_txn_dir, sole_pool_txn_dir)
+#                     else:
+#                         shutil.rmtree(specific_pool_txn_dir)
 
 
 def migrate():
     os.makedirs(_CLI_BASE_DIR)
 
-    network = _get_used_network_name()
+    # Used network cannot be determined for client in this way
+    # network = _get_used_network_name()
 
     _migrate_config()
     _migrate_genesis_txn_files()
     _migrate_wallets()
-    _migrate_keys(network)
-    _migrate_data(network)
+    # Migration of keys and data is superfluous for client
+    # _migrate_keys(network)
+    # _migrate_data(network)
