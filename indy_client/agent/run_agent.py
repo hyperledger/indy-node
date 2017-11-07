7649764c1c68bacb22979e1b098430d22e9518ec
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-11-07 15:44:31 +0300

    fix tests
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-company.com>

--- indy_client/agent/run_agent.py
+++ indy_client/agent/run_agent.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+from indy_common.exceptions import NotConnectedToNetwork
 from plenum.common.exceptions import NoConsensusYet
 from stp_core.common.log import getlogger
 from indy_client.agent.agent_cli import AgentCli
@@ -55,19 +56,22 @@ def runAgentCli(agent, config, looper=None, bootstrap=None):
             run(looper)
 
 
+CONNECTION_TIMEOUT = 120
+
+
 def runAgent(agent, looper=None, bootstrap=None):
     assert agent
 
     def is_connected(agent):
         client = agent.client
         if not client.can_send_write_requests():
-            raise Exception("Client hasn't finished catch-up with Pool Ledger yet or "
-                            "doesn't have sufficient number of connections")
+            raise NotConnectedToNetwork("Client hasn't finished catch-up with Pool Ledger yet or "
+                                        "doesn't have sufficient number of connections")
 
     async def wait_until_connected(agent):
         from stp_core.loop.eventually import eventually
         await eventually(is_connected, agent,
-                         timeout=120, retryWait=2)
+                         timeout=CONNECTION_TIMEOUT, retryWait=2)
 
     def do_run(looper):
         agent.loop = looper.loop
