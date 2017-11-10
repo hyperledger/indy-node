59909e0959f081daa951e899c7e36e7037f7cacb
Dmitry Surnin <dmitry.surnin@dsr-company.com>
2017-11-10 11:31:27 +0300

    Fix paths in migration file 152-153
    Signed-off-by: Dmitry Surnin <dmitry.surnin@dsr-company.com>

--- data/migrations/deb/helper_1_1_152_to_1_1_153.py
+++ data/migrations/deb/helper_1_1_152_to_1_1_153.py
@@ -9,24 +9,26 @@ logger = getlogger()
 
 
 def _get_nodes_data_dir():
-    base_dir = config.baseDir
-    nodes_data_dir = os.path.join(base_dir, config.nodeDataDir)
+    base_dir = os.path.expanduser(config.baseDir)
+    nodes_data_dir = os.path.join(base_dir, config.NETWORK_NAME, config.nodeDataDir)
     if not os.path.exists(nodes_data_dir):
-        msg = \
-            'Can not find the directory with the ledger: {}'\
-            .format(nodes_data_dir)
+        msg = 'Can not find the directory with the ledger: {}'.format(nodes_data_dir)
         logger.error(msg)
         raise Exception(msg)
     return nodes_data_dir
 
 
+def rm_if_exist(base_dir, sub_dirs):
+    for sd in sub_dirs:
+        to_del = os.path.join(base_dir, sd)
+        if os.path.isdir(to_del):
+            shutil.rmtree(to_del)
+
+
 def migrate_all_states(node_data_directory):
     # the states will be recovered from the ledger during the start-up.
     # just delete the current ones
-    shutil.rmtree(os.path.join(node_data_directory, 'pool_state'))
-    shutil.rmtree(os.path.join(node_data_directory, 'domain_state'))
-    shutil.rmtree(os.path.join(node_data_directory, 'config_state'))
-    shutil.rmtree(os.path.join(node_data_directory, 'idr_cache_db'))
+    rm_if_exist(node_data_directory, ['pool_state', 'domain_state', 'config_state', 'idr_cache_db'])
 
 
 def migrate_all():
