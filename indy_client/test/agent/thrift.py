5f72778d00a3d1689549415eb0d85ecb686398aa
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-11-10 18:07:53 +0300

    fixed agents for Getting Started
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-company.com>

--- indy_client/test/agent/thrift.py
+++ indy_client/test/agent/thrift.py
@@ -1,3 +1,6 @@
+import os
+
+from indy_common.config_util import getConfig
 from plenum.common.signer_did import DidSigner
 from indy_client.agent.constants import EVENT_NOTIFY_MSG
 from indy_client.client.wallet.wallet import Wallet
@@ -18,7 +21,6 @@ THRIFT_VERKEY = THRIFT_SIGNER.verkey
 
 
 class ThriftAgent(WalletedAgent):
-
     async def postProofVerif(self, claimName, link, frm):
         if claimName == "Loan-Application-Basic":
             self.notifyToRemoteCaller(
@@ -32,7 +34,6 @@ class ThriftAgent(WalletedAgent):
 
 def create_thrift(name=None, wallet=None, base_dir_path=None,
                   port=7777, client=None):
-
     endpoint_args = {'onlyListener': True}
     if wallet:
         endpoint_args['seed'] = wallet._signerById(wallet.defaultId).seed
@@ -42,7 +43,7 @@ def create_thrift(name=None, wallet=None, base_dir_path=None,
         endpoint_args['seed'] = THRIFT_SEED
 
     if client is None:
-        client = create_client(base_dir_path=None, client_class=TestClient)
+        client = create_client(base_dir_path=base_dir_path, client_class=TestClient)
 
     agent = ThriftAgent(name=name or 'Thrift Bank',
                         basedirpath=base_dir_path,
@@ -62,14 +63,23 @@ def create_thrift(name=None, wallet=None, base_dir_path=None,
 async def bootstrap_thrift(agent):
     pass
 
+
 if __name__ == "__main__":
     args = RunnableAgent.parser_cmd_args()
     name = 'Thrift Bank'
     port = args.port
     if port is None:
         port = 7777
+    network = args.network or 'sandbox'
     with_cli = args.withcli
+
+    config = getConfig()
+    base_dir_path = os.path.expanduser(
+        os.path.join(
+            config.CLI_NETWORK_DIR, network
+        ))
+
     agent = create_thrift(name=name, wallet=buildThriftWallet(),
-                          base_dir_path=None, port=port)
+                          base_dir_path=base_dir_path, port=port)
     RunnableAgent.run_agent(agent, bootstrap=bootstrap_thrift(agent),
                             with_cli=with_cli)
