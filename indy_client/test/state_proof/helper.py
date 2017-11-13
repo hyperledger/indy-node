d357c118497bc0b76ef347867a8f538152cf516c
anikitinDSR <andrew.nikitin@dsr-corporation.com>
2017-11-13 15:00:05 +0300

    Add license Apache-2.0 for source file and Creative Commons Attribution 4.0 International License (#938)
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- indy_client/test/state_proof/helper.py
+++ indy_client/test/state_proof/helper.py
@@ -1,3 +1,18 @@
+#   Copyright 2017 Sovrin Foundation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
 from plenum.common.constants import STATE_PROOF, ROOT_HASH, MULTI_SIGNATURE, PROOF_NODES, MULTI_SIGNATURE_SIGNATURE, \
     MULTI_SIGNATURE_PARTICIPANTS, MULTI_SIGNATURE_VALUE, MULTI_SIGNATURE_VALUE_LEDGER_ID, \
     MULTI_SIGNATURE_VALUE_STATE_ROOT, MULTI_SIGNATURE_VALUE_TXN_ROOT, MULTI_SIGNATURE_VALUE_POOL_STATE_ROOT, \
