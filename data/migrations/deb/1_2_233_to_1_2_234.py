baeee0b0914094b1865e5c59f1cee9fbe6ec9c27
Sergey Shilov <sergey.shilov@dsr-company.com>
2017-12-11 16:44:46 +0300

    Add a little re-factoring for readability.
    Signed-off-by: Sergey Shilov <sergey.shilov@dsr-company.com>

--- data/migrations/deb/1_2_233_to_1_2_234.py
+++ data/migrations/deb/1_2_233_to_1_2_234.py
@@ -13,28 +13,30 @@ def migrate():
 
     general_config_path = os.path.join(config.GENERAL_CONFIG_DIR, config.GENERAL_CONFIG_FILE)
 
-    if os.path.exists(general_config_path):
-        if not os.path.isfile(general_config_path):
-            logger.error("General config '{}' exists, but it is not a regular file, abort migration."
-                         .format(general_config_path))
-        else:
-            logger.info("Open '{}' for appending missing paths configuration."
-                        .format(general_config_path))
-            general_config_file = open(general_config_path, "a")
-            logger.info("Open '{}' to get missing paths configuration."
-                        .format(indy_config.__file__))
-            indy_config_file = open(indy_config.__file__, "r")
+    if not os.path.exists(general_config_path):
+        return
 
-            general_config_file.write("\n")
+    if not os.path.isfile(general_config_path):
+        logger.error("General config '{}' exists, but it is not a regular file, abort migration."
+                     .format(general_config_path))
+    else:
+        logger.info("Open '{}' for appending missing paths configuration."
+                    .format(general_config_path))
+        general_config_file = open(general_config_path, "a")
+        logger.info("Open '{}' to get missing paths configuration."
+                    .format(indy_config.__file__))
+        indy_config_file = open(indy_config.__file__, "r")
 
-            for line in indy_config_file:
-                if not line.startswith("NETWORK_NAME") and not line == "# Current network\n":
-                    logger.info("Append '{}' line to '{}'."
-                                .format(line, general_config_path))
-                    general_config_file.write(line)
+        general_config_file.write("\n")
 
-            general_config_file.close()
-            indy_config_file.close()
+        for line in indy_config_file:
+            if not line.startswith("NETWORK_NAME") and not line == "# Current network\n":
+                logger.info("Append '{}' line to '{}'."
+                            .format(line, general_config_path))
+                general_config_file.write(line)
+
+        general_config_file.close()
+        indy_config_file.close()
 
 
 migrate()
