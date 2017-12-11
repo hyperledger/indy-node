70d325b9c353cc8716df0765695f631cdc0433bd
Sergey Shilov <sergey.shilov@dsr-company.com>
2017-12-11 16:30:05 +0300

    Increase indy-plenum version.
    Signed-off-by: Sergey Shilov <sergey.shilov@dsr-company.com>

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum-dev==1.2.194',
+    install_requires=['indy-plenum-dev==1.2.196',
                       'indy-anoncreds-dev==1.0.32',
                       'python-dateutil',
                       'timeout-decorator'],
