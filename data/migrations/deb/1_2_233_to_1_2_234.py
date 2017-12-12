a9772e09ac894e001b78dee031ba229d8ad12c9d
Sergey Shilov <sergey.shilov@dsr-company.com>
2017-12-12 17:01:52 +0300

    Implement migration of nodes data.
    Signed-off-by: Sergey Shilov <sergey.shilov@dsr-company.com>

--- data/migrations/deb/1_2_233_to_1_2_234.py
+++ data/migrations/deb/1_2_233_to_1_2_234.py
@@ -1,16 +1,64 @@
 #!/usr/bin/python3.5
 import os
+import shutil
+import pwd
+import grp
+import stat
 
 from stp_core.common.log import getlogger
-from indy_common.config_util import getConfig
+from indy_common.config_util import getConfigOnce
+from indy_common.config_helper import ConfigHelper
 
 import indy_node.general_config.indy_config as indy_config
 
 
-def migrate():
-    config = getConfig()
-    logger = getlogger()
+logger = getlogger()
 
+
+def ext_copytree(src, dst):
+    if not os.path.isdir(dst):
+        shutil.copytree(src, dst)
+
+
+def set_own_perm(usr, dir_list):
+    uid = pwd.getpwnam(usr).pw_uid
+    gid = grp.getgrnam(usr).gr_gid
+    perm_mask_rw = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP
+    perm_mask_rwx = stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP
+
+    for cdir in dir_list:
+        os.chown(cdir, uid, gid)
+        os.chmod(cdir, perm_mask_rwx)
+        for croot, sub_dirs, cfiles in os.walk(cdir):
+            for fs_name in sub_dirs:
+                os.chown(os.path.join(croot, fs_name), uid, gid)
+                os.chmod(os.path.join(croot, fs_name), perm_mask_rwx)
+            for fs_name in cfiles:
+                os.chown(os.path.join(croot, fs_name), uid, gid)
+                os.chmod(os.path.join(croot, fs_name), perm_mask_rw)
+
+
+def migrate_nodes_data():
+    config = getConfigOnce()
+    config_helper = ConfigHelper(config)
+
+    # Move data
+    old_nodes_data_dir = os.path.join(config_helper.ledger_data_dir, 'nodes')
+    new_node_data_dir = config_helper.ledger_data_dir
+    try:
+        visit_dirs = os.listdir(old_nodes_data_dir)
+    except FileNotFoundError:
+        visit_dirs = []
+    for node_name in visit_dirs:
+            move_path = os.path.join(old_nodes_data_dir, node_name)
+            to_path = os.path.join(new_node_data_dir, node_name)
+            ext_copytree(move_path, to_path)
+    shutil.rmtree(old_nodes_data_dir)
+    set_own_perm("indy", [new_node_data_dir])
+
+
+def migrate_config():
+    config = getConfigOnce()
     general_config_path = os.path.join(config.GENERAL_CONFIG_DIR, config.GENERAL_CONFIG_FILE)
 
     if not os.path.exists(general_config_path):
@@ -39,4 +87,10 @@ def migrate():
         indy_config_file.close()
 
 
-migrate()
+def migrate_all():
+    # Migrate config first!
+    migrate_config()
+    migrate_nodes_data()
+
+
+migrate_all()
