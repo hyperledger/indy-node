aeef96fc531bd22d87e8eb95a6912aba4b9055f3
Sergey Shilov <sergey.shilov@dsr-company.com>
2018-01-15 15:51:20 +0300

    Fix script name.
    Signed-off-by: Sergey Shilov <sergey.shilov@dsr-company.com>

--- setup.py
+++ setup.py
@@ -95,5 +95,5 @@ setup(
              'scripts/enable_bls',
              'scripts/create_dirs.sh',
              'scripts/indy_old_cli_export_dids',
-             'scripts/setup_indy_node_iptable']
+             'scripts/setup_indy_node_iptables']
 )
