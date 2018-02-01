d0a376dd71d1e901319c7e37c4c9c91236e1ae78
Andrey Kononykhin <andkononykhin@gmail.com>
2018-02-01 12:35:59 +0300

    refactored local lib, adjusted jenkinsfiles
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,36 +1,33 @@
 #!/usr/bin/env groovy
-
 import groovy.transform.Field
 
-def init() {
-    return [
-        dockers: [
-            ubuntu: [
-                imgName: "hyperledger/indy-node-ci",
-                dockerfile: "ci/ubuntu.dockerfile",
-                contextDir: "ci"
-            ]
+@Field Map config = [
+    dockers: [
+        ubuntu: [
+            imgName: "hyperledger/indy-node-ci",
+            dockerfile: "ci/ubuntu.dockerfile",
+            contextDir: "ci"
+        ]
+    ],
+    tests: [
+        common: [
+            resFile: { "test-result-common.${NODE_NAME}.xml" },
+            testDir: 'indy_common',
+            docker: 'ubuntu'
         ],
-        tests: [
-            common: [
-                resFile: { "test-result-common.${NODE_NAME}.xml" },
-                testDir: 'indy_common',
-                docker: 'ubuntu'
-            ],
-            client: [
-                resFile: { "test-result-client.${NODE_NAME}.txt" },
-                testDir: 'indy_client',
-                useRunner: true,
-                docker: 'ubuntu'
-            ],
-            node: [
-                resFile: { "test-result-node.${NODE_NAME}.txt" },
-                testDir: 'indy_node',
-                useRunner: true,
-                docker: 'ubuntu'
-            ]
+        client: [
+            resFile: { "test-result-client.${NODE_NAME}.txt" },
+            testDir: 'indy_client',
+            useRunner: true,
+            docker: 'ubuntu'
+        ],
+        node: [
+            resFile: { "test-result-node.${NODE_NAME}.txt" },
+            testDir: 'indy_node',
+            useRunner: true,
+            docker: 'ubuntu'
         ]
     ]
-}
+]
 
 return this;
