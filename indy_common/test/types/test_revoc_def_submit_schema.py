e3a190b87189d9b9ea09c1104cf15906dd98b426
anikitinDSR <andrew.nikitin@dsr-corporation.com>
2018-02-21 17:07:13 +0300

    [INDY-1134] Added support for REVOC_REG_DEF (#567)
    * [INDY-1134] Added support for REVOC_REG_DEF
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Removed constants to indy-node
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Changed schema for REVOC_DEF request
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Code static validation fixes
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Added test for schema
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Static code validation
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Fix import
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Increment plenum version
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Added support for REVOC_REG_DEF
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Removed constants to indy-node
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Changed schema for REVOC_DEF request
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Code static validation fixes
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Added test for schema
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Static code validation
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Fix import
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Skip tests
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Move test to sdk
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Remove unused import
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Increment plenum version
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Add plenum's looper
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
    
    * [INDY-1134] Skip tests
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

---
+++ indy_common/test/types/test_revoc_def_submit_schema.py
@@ -0,0 +1,46 @@
+from collections import OrderedDict
+from indy_common.types import RevocDefValueField, ClientRevocDefSubmitField
+from plenum.common.messages.fields import IntegerField, AnyMapField, NonEmptyStringField
+from indy_common.constants import ISSUANCE_TYPE, MAX_CRED_NUM, PUBLIC_KEYS, \
+    TAILS_LOCATION, TAILS_HASH, ID, TYPE, TAG, CRED_DEF_ID, VALUE
+
+
+EXPECTED_REVOC_DEF_VALUE_FIELDS = OrderedDict([
+    (ISSUANCE_TYPE, NonEmptyStringField),
+    (MAX_CRED_NUM, IntegerField),
+    (PUBLIC_KEYS, AnyMapField),
+    (TAILS_HASH, NonEmptyStringField),
+    (TAILS_LOCATION, NonEmptyStringField),
+])
+
+EXPECTED_REVOC_DEF_SUBMIT_FIELDS = OrderedDict([
+    (ID, NonEmptyStringField),
+    (TYPE, NonEmptyStringField),
+    (TAG, NonEmptyStringField),
+    (CRED_DEF_ID, NonEmptyStringField),
+    (VALUE, RevocDefValueField)
+])
+
+
+def test_revoc_value_has_expected_fields():
+    actual_field_names = OrderedDict(
+        RevocDefValueField.schema).keys()
+    assert actual_field_names == EXPECTED_REVOC_DEF_VALUE_FIELDS.keys()
+
+
+def test_revoc_value_has_expected_validators():
+    schema = dict(RevocDefValueField.schema)
+    for field, validator in EXPECTED_REVOC_DEF_VALUE_FIELDS.items():
+        assert isinstance(schema[field], validator)
+
+
+def test_client_submit_has_expected_fields():
+    actual_field_names = OrderedDict(
+        ClientRevocDefSubmitField.schema).keys()
+    assert actual_field_names == EXPECTED_REVOC_DEF_SUBMIT_FIELDS.keys()
+
+
+def test_client_submit_has_expected_validators():
+    schema = dict(ClientRevocDefSubmitField.schema)
+    for field, validator in EXPECTED_REVOC_DEF_SUBMIT_FIELDS.items():
+        assert isinstance(schema[field], validator)
