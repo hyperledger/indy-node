8848c168526e2892e94a5c095f172407c3498c3f
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2018-03-14 18:05:31 +0300

    [INDY-1138] Use get_equal_or_prev from KVstore
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- indy_node/persistence/state_ts_store.py
+++ indy_node/persistence/state_ts_store.py
@@ -6,7 +6,7 @@ logger = getlogger()
 
 class StateTsDbStorage():
     def __init__(self, name, storage):
-        logger.debug("Initializing timestamp-root_hash storage")
+        logger.debug("Initializing timestamp-rootHash storage")
         self._storage = storage
         self._name = name
 
@@ -24,18 +24,4 @@ class StateTsDbStorage():
         self._storage.close()
 
     def get_equal_or_prev(self, timestamp):
-        # return value can be:
-        #    None, if required key less then minimal key from DB
-        #    Equal by key if key exist in DB
-        #    Previous if key does not exist in Db, but there is key less than required
-
-        try:
-            value = self.get(timestamp)
-        except KeyError:
-            prev_value = None
-            for k, v in self._storage.iterator():
-                if int(k) > timestamp:
-                    break
-                prev_value = v
-            value = prev_value
-        return value
+        return self._storage.get_equal_or_prev(str(timestamp))
