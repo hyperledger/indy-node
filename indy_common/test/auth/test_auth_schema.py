3673b4bf63682a47174ef653559773b70179bacd
toktar <renata.toktar@dsr-corporation.com>
2018-03-16 14:17:15 +0300

    INDY-1111: test debug
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- indy_common/test/auth/test_auth_schema.py
+++ indy_common/test/auth/test_auth_schema.py
@@ -7,14 +7,14 @@ from indy_common.constants import NAME, TGB, TRUST_ANCHOR, SCHEMA
 def test_schema_adding():
     roles = {TRUSTEE, STEWARD, TRUST_ANCHOR}
     for role in roles:
-        r, msg = _authorised_for_schemas(role, True)
+        r, msg = _authorised_for_schemas(role)
         assert r and not msg
 
 
 def test_schema_adding_without_permission():
     roles = {TGB, None}
     for role in roles:
-        r, msg = _authorised_for_schemas(role, True)
+        r, msg = _authorised_for_schemas(role)
         assert not r and msg
 
 
