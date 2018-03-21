2253e1a7aa03e476800f52da0a86dad55bf3eb4e
Michael Boyd <michaeldavisboyd@gmail.com>
2018-03-21 06:51:35 -0600

    patch to vagrant files for libsodium18 missing dependancy
    Signed-off-by: Michael Boyd <michaeldavisboyd@gmail.com>

--- environment/vagrant/training/vb-multi-vm/scripts/validator.sh
+++ environment/vagrant/training/vb-multi-vm/scripts/validator.sh
@@ -31,14 +31,14 @@ cp $TIMEZONE /etc/localtime
 #--------------------------------------------------------
 echo "Installing Required Packages"
 apt-get update
-apt-get install -y software-properties-common python-software-properties
+apt-get install -y software-properties-common python-software-properties libsodium18
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 68DB5E88
 add-apt-repository "deb https://repo.sovrin.org/deb xenial master"
 apt-get update
 #DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 DEBIAN_FRONTEND=noninteractive apt-get install -y unzip make screen indy-node tmux vim wget
 
-awk '{if (index($1, "NETWORK_NAME") != 0) {print("NETWORK_NAME = \"sandbox\"")} else print($0)}' /etc/indy/indy_config.py> /tmp/indy_config.py
+awk '{if (index($1, "NETWORK_NAME") != 0) {print("NETWORK_NAME =\"sandbox\"")} else print($0)}' /etc/indy/indy_config.py> /tmp/indy_config.py
 mv /tmp/indy_config.py /etc/indy/indy_config.py
 
 #--------------------------------------------------------
