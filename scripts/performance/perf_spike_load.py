5a407d282920712aba438ea650c10b0537bde401
NataliaDracheva <natalia.dracheva@dsr-corporation.com>
2018-08-20 16:21:41 +0100

    Renamed the script, added clients variable
    Signed-off-by: NataliaDracheva <natalia.dracheva@dsr-corporation.com>

--- scripts/performance/perf_spike_load.py
+++ scripts/performance/perf_spike_load.py
@@ -6,6 +6,11 @@ import subprocess
 
 parser = argparse.ArgumentParser(description='The script uses another load script with different parameters')
 
+parser.add_argument('-c', '--clients', default=0, type=int, required=False, dest='clients',
+                    help='Number of client you want to create. '
+                         '0 or less means equal to number of available CPUs. '
+                         'Default value is 0')
+
 parser.add_argument('--read_mode', default='permanent', type=str, required=False, dest='read_mode',
                     help='Load profile for reading transactions. '
                          'Permanent - stable background load, '
@@ -61,23 +66,26 @@ def start_profile(args):
     write_arg_kind = "-k=%s" % args.write_kind
     rps_arg = "-l=%s" % args.rps
     wps_arg = "-l=%s" % args.wps
+    clients_arg = "-c=%s" %args.clients
     spike_time_in_seconds = int(args.spike_time_in_minutes) * 60
     spike_time_arg = "--load_time=%s" % str(spike_time_in_seconds)
     rest_time_in_seconds = int(args.rest_time_in_minutes) * 60
 
-
     if args.read_mode == 'permanent':
         # start profile with reading transactions for the whole time
-        subprocess_args = create_subprocess_args([genesis_arg, output_arg, rps_arg, read_kind_arg, overall_time_arg])
+        subprocess_args = create_subprocess_args([genesis_arg, output_arg, rps_arg, read_kind_arg, overall_time_arg,
+                                                  clients_arg])
         subprocess.Popen(subprocess_args, stdin=None, stdout=None, stderr=None, close_fds=True)
     end_time = datetime.now() + timedelta(minutes=int(args.overall_time_in_minutes))
     while datetime.now() < end_time:
         if args.read_mode != 'permanent':
             # start profile with reading transactions for x minutes
-            subprocess_args = create_subprocess_args([genesis_arg, output_arg, rps_arg, read_kind_arg, spike_time_arg])
+            subprocess_args = create_subprocess_args([genesis_arg, output_arg, rps_arg, read_kind_arg, spike_time_arg,
+                                                      clients_arg])
             subprocess.Popen(subprocess_args, stdin=None, stdout=None, stderr=None, close_fds=True)
         # start profile with writing transactions for x minutes
-        subprocess_args = create_subprocess_args([genesis_arg, output_arg, wps_arg, write_arg_kind, spike_time_arg])
+        subprocess_args = create_subprocess_args([genesis_arg, output_arg, wps_arg, write_arg_kind, spike_time_arg,
+                                                  clients_arg])
         subprocess.Popen(subprocess_args, stdin=None, stdout=None, stderr=None, close_fds=True)
         time.sleep(rest_time_in_seconds + spike_time_in_seconds)
 
