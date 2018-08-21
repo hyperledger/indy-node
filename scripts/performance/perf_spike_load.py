f7a71fd1219d6d4acabd5a1af38e084bdadbf673
NataliaDracheva <natalia.dracheva@dsr-corporation.com>
2018-08-21 15:47:21 +0100

    [INDY-1567] Configured log folders structure.
    Signed-off-by: NataliaDracheva <natalia.dracheva@dsr-corporation.com>

--- scripts/performance/perf_spike_load.py
+++ scripts/performance/perf_spike_load.py
@@ -3,49 +3,55 @@
 """This script uses another load script (perf_processes.py) running it with different parameters"""
 
 
-import time
 from datetime import timedelta, datetime
 import subprocess
 import yaml
+import time
+import os
 
 
-def create_subprocess_args(config, sub_process_type):
+def create_subprocess_args(config, sub_process_type, folder_count, log_folder_name):
     args = ["python3", "perf_processes.py"]
-    common_args = config["common"]
+    common_args = config["common"].copy()
+    if "read" in sub_process_type:
+        common_args.update(config["read_txns"])
+    elif "write" in sub_process_type:
+        common_args.update(config["write_txns"])
     for dict_key in common_args:
-        args.append("--" + dict_key + "=" + str(common_args[dict_key]))
-    if sub_process_type == "read_background":
+        if dict_key == "directory":
+            output_folder = str(common_args[dict_key]) + log_folder_name + "/" + \
+                            sub_process_type + "_" + str(folder_count)
+            if not os.path.isdir(output_folder):
+                os.makedirs(output_folder)
+            args.append("--" + dict_key + "=" + output_folder)
+        else:
+            args.append("--" + dict_key + "=" + str(common_args[dict_key]))
+    if "background" in sub_process_type:
         args.append("--load_time=" + str(config["perf_spike"]["overall_time_in_seconds"]))
-        args.append("--load_rate=" + str(config["read_txns"]["load_rate"]))
-        args.append("--kind=" + str(config["read_txns"]["kind"]))
-    elif sub_process_type == "read_spike":
+    elif "spike" in sub_process_type:
         args.append("--load_time=" + str(config["perf_spike"]["spike_time_in_seconds"]))
-        args.append("--load_rate=" + str(config["read_txns"]["load_rate"]))
-        args.append("--kind=" + str(config["read_txns"]["kind"]))
-    elif sub_process_type == "write_spike":
-        args.append("--load_time=" + str(config["perf_spike"]["spike_time_in_seconds"]))
-        args.append("--load_rate=" + str(config["write_txns"]["load_rate"]))
-        args.append("--kind=" + str(config["write_txns"]["kind"]))
     return args
 
 
 def start_profile():
+    folder_count = 0  # ordering number of the spike which goes to logs folder name
+    root_log_folder_name = "Spike_log %s" % time.strftime("%m-%d-%y %H-%M-%S")
     with open("config_perf_spike_load.yml") as file:
         config = yaml.load(file)
     if config["perf_spike"]["read_mode"] == 'permanent':
-        subprocess_args = create_subprocess_args(config, "read_background")
-        print(subprocess_args)
-        subprocess.Popen(subprocess_args, stdin=None, stdout=None, stderr=None, close_fds=True)
+        subprocess_args = create_subprocess_args(config, "read_background", folder_count, root_log_folder_name)
+        subprocess.Popen(subprocess_args, close_fds=True)
     end_time = datetime.now() + timedelta(seconds=int(config["perf_spike"]["overall_time_in_seconds"]))
     while datetime.now() < end_time:
+        folder_count += 1
         if config["perf_spike"]["read_mode"] == 'spike':
             # start profile with reading transactions for x minutes
-            subprocess_args = create_subprocess_args(config, "read_spike")
-            subprocess.Popen(subprocess_args, stdin=None, stdout=None, stderr=None, close_fds=True)
-            time.sleep(2)  # log folders cannot be created for 2 scripts launched at the same time
+            subprocess_args = create_subprocess_args(config, "read_spike", folder_count, root_log_folder_name)
+            subprocess.Popen(subprocess_args, close_fds=True)
+            folder_count += 1
         # start profile with writing transactions for x minutes
-        subprocess_args = create_subprocess_args(config, "write_spike")
-        subprocess.Popen(subprocess_args, stdin=None, stdout=None, stderr=None, close_fds=True)
+        subprocess_args = create_subprocess_args(config, "write_spike", folder_count, root_log_folder_name)
+        subprocess.Popen(subprocess_args, close_fds=True)
         time.sleep(int(config["perf_spike"]["spike_time_in_seconds"]) +
                    int(config["perf_spike"]["rest_time_in_seconds"]))
 
