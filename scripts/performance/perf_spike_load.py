76e1723c97be4a097ad8fc2e8e1fc21b17b4cca7
NataliaDracheva <natalia.dracheva@dsr-corporation.com>
2018-08-31 14:26:46 +0100

    [INDY-1666] Stress scenario works in stepwise and stable modes
    Signed-off-by: NataliaDracheva <natalia.dracheva@dsr-corporation.com>

--- scripts/performance/perf_spike_load.py
+++ scripts/performance/perf_spike_load.py
@@ -11,74 +11,128 @@ import time
 import os
 
 
-def create_subprocess_args(config, sub_process_type, folder_count, log_folder_name):
+def create_output_directory(folder_path):
+    output_folder = ""
+    for folder in folder_path:
+        output_folder = os.path.join(output_folder, folder)
+    try:
+        output_folder = os.path.expanduser(output_folder)
+    except OSError:
+        raise ValueError("Bad output log folder pathname!")
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+    directory = "--directory={}".format(output_folder)
+    return directory
+
+
+def create_subprocess(config, sub_process_type, directory_arg, load_time, load_rate=None):
     args = ["python3", "perf_processes.py"]
     common_args = config["common"].copy()
-    if "read" in sub_process_type:
-        common_args.update(config["read_txns"])
-    elif "write" in sub_process_type:
-        common_args.update(config["write_txns"])
+    common_args.update(config[sub_process_type])
     for dict_key in common_args:
         if dict_key == "directory":
-            output_folder = os.path.join(str(common_args[dict_key]), log_folder_name,
-                                         "{}_{}".format(sub_process_type, folder_count))
-            try:
-                output_folder = os.path.expanduser(output_folder)
-            except OSError:
-                raise ValueError("Bad output log folder pathname!")
-            if not os.path.isdir(output_folder):
-                os.makedirs(output_folder)
-            args.append("--{}={}".format(dict_key, output_folder))
+            args.append(directory_arg)
+        elif "stepwise" in dict_key:
+            continue
+        elif load_rate is not None and dict_key == "load_rate":
+            args.append("--{}={}".format(dict_key, load_rate))
         else:
             args.append("--{}={}".format(dict_key, common_args[dict_key]))
-    if "background" in sub_process_type:
-        args.append("--load_time={}".format(config["perf_spike"]["overall_time_in_seconds"]))
-    elif "spike" in sub_process_type:
-        args.append("--load_time={}".format(config["perf_spike"]["spike_time_in_seconds"]))
-    return args
+    args.append("--load_time={}".format(load_time))
+    subprocess.Popen(args, close_fds=True)
+    return
 
 
 def start_profile():
-    folder_count = 0  # ordering number of the spike which goes to logs folder name
-    root_log_folder_name = "Spike_log {}".format(time.strftime("%m-%d-%y %H-%M-%S"))
     with open("config_perf_spike_load.yml") as file:
         config = yaml.load(file)
-    if config["perf_spike"]["read_mode"] == 'permanent':
-        print("""
-              ==========================================================================================
-              The script creates writing transaction spikes, during intervals there is a background load
-              of reading transactions
-              ==========================================================================================""")
-    elif config["perf_spike"]["read_mode"] == 'spike':
-        print("""
-              ============================================================================================
-              The script creates reading and writing transaction spikes, during intervals there is no load
-              ============================================================================================""")
-    print("Reading transactions mode:          ", config["perf_spike"]["read_mode"])
-    print("Every spike time in seconds:        ", config["perf_spike"]["spike_time_in_seconds"])
-    print("Interval between spikes in seconds: ", config["perf_spike"]["rest_time_in_seconds"])
-    print("Overall time in minutes:            ", config["perf_spike"]["overall_time_in_seconds"] / 60)
-    if config["perf_spike"]["read_mode"] == 'permanent' and config["read_txns"]["step_time_in_seconds"] != 0:
-        subprocess_args = create_subprocess_args(config, "read_background", folder_count, root_log_folder_name)
-        subprocess.Popen(subprocess_args, close_fds=True)
-    elif config["perf_spike"]["read_mode"] == 'permanent' and config["read_txns"]["step_time_in_seconds"] == 0:
-        steps_number = int(config["perf_spike"]["spike_time_in_seconds"] / config["read_txns"]["step_time_in_seconds"])
-        for i in range(0, steps_number):
-            subprocess_args = create_subprocess_args(config, "read_background", folder_count, root_log_folder_name)
-            subprocess.Popen(subprocess_args, close_fds=True)
-    end_time = datetime.now() + timedelta(seconds=int(config["perf_spike"]["overall_time_in_seconds"]))
-    while datetime.now() < end_time:
+    mode = config["profile"]["mode"]
+    if mode == "stress":
+        stress_profile(config)
+    elif mode == "permanent":
+        permanent_profile(config)
+    elif mode == "spike":
+        spike_profile(config)
+
+
+def run_spikes(config, root_log_folder_name):
+    spike_time_in_seconds = config["profile"]["spike_time_in_seconds"]
+    folder_count = 0
+    key = None
+    for key in config["spikes"].keys():
         folder_count += 1
-        if config["perf_spike"]["read_mode"] == 'spike':
-            # start profile with reading transactions for x minutes
-            subprocess_args = create_subprocess_args(config, "read_spike", folder_count, root_log_folder_name)
-            subprocess.Popen(subprocess_args, close_fds=True)
+        if config["spikes"][key]["step_time_in_seconds"] != 0:
+            sub_process_type = "spike"
+            step_time_in_seconds = int(config[key]["stepwise"]["step_time_in_seconds"])
+            step_txns_per_second = int(config[key]["stepwise"]["step_txns_per_second"])
+            step_load_rate = int(config[key]["stepwise"]["step_initial_load_rate"])
+            steps_number = int(spike_time_in_seconds / step_time_in_seconds)
+
+            for i in range(0, steps_number):
+                load_time = spike_time_in_seconds - step_time_in_seconds * i
+                directory = [config["common"]["directory"], root_log_folder_name,
+                             "  {}_{}".format(sub_process_type, folder_count)]
+                directory_arg = create_output_directory(directory)
+                create_subprocess(config, sub_process_type, directory_arg, load_time, step_load_rate)
+                step_load_rate = step_txns_per_second
+                time.sleep(step_time_in_seconds)
+                folder_count += 1
+        else:
+            sub_process_type = key
+            directory_parts = [config["common"]["directory"], root_log_folder_name,
+                               "{}_{}".format(sub_process_type, folder_count)]
+            directory_arg = create_output_directory(directory_parts)
+            create_subprocess(config, sub_process_type, directory_arg, spike_time_in_seconds)
+            print("Spike {}".format(key))
+    time.sleep(int(config["profile"]["spike_time_in_seconds"]) +
+               int(config["profile"]["rest_time_in_seconds"]))
+
+
+def spike_profile(config):
+    root_log_folder_name = "Spike_log {}".format(time.strftime("%m-%d-%y %H-%M-%S"))
+
+    end_time = datetime.now() + timedelta(seconds=int(config["profile"]["overall_time_in_seconds"]))
+    while datetime.now() < end_time:
+        run_spikes(config, root_log_folder_name)
+
+
+def permanent_profile(config):
+    root_log_folder_name = "Spike_with_bg_log {}".format(time.strftime("%m-%d-%y %H-%M-%S"))
+    directory = [config["common"]["directory"], root_log_folder_name,
+                 "{}_{}".format("background", "0")]
+    directory_arg = create_output_directory(directory)
+    create_subprocess(config, "background", directory_arg, config["profile"]["overall_time_in_seconds"])
+
+    end_time = datetime.now() + timedelta(seconds=int(config["profile"]["overall_time_in_seconds"]))
+    while datetime.now() < end_time:
+        run_spikes(config, root_log_folder_name)
+
+
+def stress_profile(config):
+    folder_count = 1  # ordering number of the spike which goes to logs folder name
+    root_log_folder_name = "Stress_log_{}".format(time.strftime("%m-%d-%y_%H-%M-%S"))
+    overall_time_in_seconds = int(config["profile"]["overall_time_in_seconds"])
+    sub_process_type = "background"
+    if config["background"]["stepwise"]["step_time_in_seconds"] != 0:
+        step_time_in_seconds = int(config["background"]["stepwise"]["step_time_in_seconds"])
+        step_txns_per_second = int(config["background"]["stepwise"]["step_txns_per_second"])
+        step_load_rate = int(config["background"]["stepwise"]["step_initial_load_rate"])
+        steps_number = int(overall_time_in_seconds/step_time_in_seconds)
+
+        for i in range(0, steps_number):
+            load_time = overall_time_in_seconds - step_time_in_seconds * i
+            directory = [config["common"]["directory"], root_log_folder_name,
+                         "{}_{}".format(sub_process_type, folder_count)]
+            directory_arg = create_output_directory(directory)
+            create_subprocess(config, sub_process_type, directory_arg, load_time, step_load_rate)
+            step_load_rate = step_txns_per_second
+            time.sleep(step_time_in_seconds)
             folder_count += 1
-        # start profile with writing transactions for x minutes
-        subprocess_args = create_subprocess_args(config, "write_spike", folder_count, root_log_folder_name)
-        subprocess.Popen(subprocess_args, close_fds=True)
-        time.sleep(int(config["perf_spike"]["spike_time_in_seconds"]) +
-                   int(config["perf_spike"]["rest_time_in_seconds"]))
+    else:
+        directory_parts = [config["common"]["directory"], root_log_folder_name, "Stable_stress"]
+        directory_arg = create_output_directory(directory_parts)
+        create_subprocess(config, sub_process_type, directory_arg, overall_time_in_seconds)
+        print("Stress stable")
 
 
 if __name__ == '__main__':
