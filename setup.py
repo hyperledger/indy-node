58af8054ae5bea8a9e258915fe8f0bbaf5cb38a8
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2018-08-31 15:22:22 +0300

    [RC-1.6.72] bump plenum
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum==1.6.50',
+    install_requires=['indy-plenum==1.6.51',
                       'indy-anoncreds==1.0.11',
                       'python-dateutil',
                       'timeout-decorator==0.4.0'],
