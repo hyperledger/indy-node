bd6cb000b76c6feb52faf257e7d03a1274c30e2e
NataliaDracheva <natalia.dracheva@dsr-corporation.com>
2018-09-04 19:04:50 +0300

    [INDY-1666] Added a possibility to check load profile before running.
    Signed-off-by: NataliaDracheva <natalia.dracheva@dsr-corporation.com>

--- scripts/performance/perf_spike_load.py
+++ scripts/performance/perf_spike_load.py
@@ -1,14 +1,23 @@
 #! /usr/bin/env python3
 
-"""This script uses another load script (perf_processes.py) running it with different parameters which are
-provided in config_perf_spike_load.yml file"""
 
-
-from datetime import timedelta, datetime
 import subprocess
 import yaml
 import time
 import os
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description='This script uses another load script (perf_processes.py) running it '
+                                             'with different parameters which are provided in '
+                                             'config_perf_spike_load.yml file')
+
+parser.add_argument('-g', '--graph', default=False, type=bool, required=False, dest='graph',
+                    help='Build a graph to check if the configured profile is correct')
+
+args = parser.parse_args()
 
 
 def create_output_directory(folder_path):
@@ -19,120 +28,179 @@ def create_output_directory(folder_path):
         output_folder = os.path.expanduser(output_folder)
     except OSError:
         raise ValueError("Bad output log folder pathname!")
-    if not os.path.isdir(output_folder):
+    if not os.path.isdir(output_folder) and not args.graph:
         os.makedirs(output_folder)
     directory = "--directory={}".format(output_folder)
     return directory
 
 
-def create_subprocess(config, sub_process_type, directory_arg, load_time, load_rate=None):
-    args = ["python3", "perf_processes.py"]
+def get_args(config, process_type, directory_arg):
+    args_for_script = ["python3", "perf_processes.py"]
     common_args = config["common"].copy()
-    common_args.update(config[sub_process_type])
+    common_args.update(config["processes"][process_type])
     for dict_key in common_args:
         if dict_key == "directory":
-            args.append(directory_arg)
-        elif "stepwise" in dict_key:
+            args_for_script.append(directory_arg)
+        elif "step" in dict_key:
             continue
-        elif load_rate is not None and dict_key == "load_rate":
-            args.append("--{}={}".format(dict_key, load_rate))
         else:
-            args.append("--{}={}".format(dict_key, common_args[dict_key]))
-    args.append("--load_time={}".format(load_time))
-    subprocess.Popen(args, close_fds=True)
-    return
+            args_for_script.append("--{}={}".format(dict_key, common_args[dict_key]))
+    return args_for_script
+
+
+def order_processes(delays, args_for_script):
+    processed_delays = []
+    processes_dictionary = {}
+    for delay in delays:
+        if delay in processed_delays:
+            continue
+        else:
+            processed_delays.append(delay)
+        delays_indices = [i for i, e in enumerate(delays) if e == delay]
+        args_list = []
+        for index in delays_indices:
+            args_list.append(args_for_script[index])
+        processes_dictionary.update({delay: args_list})
+    processes_dictionary_sorted = collections.OrderedDict(sorted(processes_dictionary.items()))
+    return processes_dictionary_sorted
+
+
+def collect_delays(function_parameters, time_interval, spike_delay=0):
+    args_for_script = function_parameters[0]
+    step_time = function_parameters[1]
+    step_initial_load = function_parameters[2]
+    step_final_load = function_parameters[3]
+    args_copy = args_for_script.copy()
+    args_to_send = []
+    delay = []
+    if step_time != 0 and step_final_load != step_initial_load:
+        step_number = int(time_interval / step_time)
+        step_value = int((step_final_load - step_initial_load) / step_number)
+        if step_value == 0:
+            raise ValueError("There should be at least one transaction per step.")
+        for i in range(0, step_number):
+            load_time = time_interval - step_time * i
+            args_copy = args_for_script.copy()
+            args_copy.append("--load_time={}".format(load_time))
+            if i != 0:
+                args_copy.append("--load_rate={}".format(step_value))
+            else:
+                args_copy.append("--load_rate={}".format(step_initial_load))
+            delay.append(spike_delay + time_interval - load_time)
+            args_to_send.append(args_copy)
+            step_number += 1
+    else:
+        delay.append(spike_delay)
+        args_copy.append("--load_time={}".format(time_interval))
+        args_copy.append("--load_rate={}".format(step_initial_load))
+        args_to_send.append(args_copy)
+    return [delay, args_to_send]
+
+
+def collect_processes(config):
+    root_log_folder_name = "Stress_log_{}".format(time.strftime("%m-%d-%y_%H-%M-%S"))
+    processes = list(config["processes"].keys())
+    functions = {}
+    for process_name in processes:
+        step_time_in_seconds = config["processes"][process_name]["step_time_in_seconds"]
+        if step_time_in_seconds == 0:
+            continue
+        initial_rate = config["processes"][process_name]["step_initial_load_rate"]
+        final_rate = config["processes"][process_name]["step_final_load_rate"]
+        if initial_rate > final_rate:
+            raise ValueError("In {} block initial rate is bigger than final!".format(process_name))
+        directory = [config["common"]["directory"], root_log_folder_name, process_name]
+        directory_arg = create_output_directory(directory)
+        args_for_script = get_args(config, process_name, directory_arg)
+        step_parameters = [args_for_script, step_time_in_seconds, initial_rate, final_rate]
+        functions.update({process_name: step_parameters})
+    return functions
 
 
 def start_profile():
     with open("config_perf_spike_load.yml") as file:
         config = yaml.load(file)
-    mode = config["profile"]["mode"]
-    if mode == "stress":
-        stress_profile(config)
-    elif mode == "permanent":
-        permanent_profile(config)
-    elif mode == "spike":
-        spike_profile(config)
-
-
-def run_spikes(config, root_log_folder_name):
-    spike_time_in_seconds = config["profile"]["spike_time_in_seconds"]
-    folder_count = 0
-    key = None
-    for key in config["spikes"].keys():
-        folder_count += 1
-        if config["spikes"][key]["step_time_in_seconds"] != 0:
-            sub_process_type = "spike"
-            step_time_in_seconds = int(config[key]["stepwise"]["step_time_in_seconds"])
-            step_txns_per_second = int(config[key]["stepwise"]["step_txns_per_second"])
-            step_load_rate = int(config[key]["stepwise"]["step_initial_load_rate"])
-            steps_number = int(spike_time_in_seconds / step_time_in_seconds)
-
-            for i in range(0, steps_number):
-                load_time = spike_time_in_seconds - step_time_in_seconds * i
-                directory = [config["common"]["directory"], root_log_folder_name,
-                             "  {}_{}".format(sub_process_type, folder_count)]
-                directory_arg = create_output_directory(directory)
-                create_subprocess(config, sub_process_type, directory_arg, load_time, step_load_rate)
-                step_load_rate = step_txns_per_second
-                time.sleep(step_time_in_seconds)
-                folder_count += 1
-        else:
-            sub_process_type = key
-            directory_parts = [config["common"]["directory"], root_log_folder_name,
-                               "{}_{}".format(sub_process_type, folder_count)]
-            directory_arg = create_output_directory(directory_parts)
-            create_subprocess(config, sub_process_type, directory_arg, spike_time_in_seconds)
-            print("Spike {}".format(key))
-    time.sleep(int(config["profile"]["spike_time_in_seconds"]) +
-               int(config["profile"]["rest_time_in_seconds"]))
-
-
-def spike_profile(config):
-    root_log_folder_name = "Spike_log {}".format(time.strftime("%m-%d-%y %H-%M-%S"))
-
-    end_time = datetime.now() + timedelta(seconds=int(config["profile"]["overall_time_in_seconds"]))
-    while datetime.now() < end_time:
-        run_spikes(config, root_log_folder_name)
-
-
-def permanent_profile(config):
-    root_log_folder_name = "Spike_with_bg_log {}".format(time.strftime("%m-%d-%y %H-%M-%S"))
-    directory = [config["common"]["directory"], root_log_folder_name,
-                 "{}_{}".format("background", "0")]
-    directory_arg = create_output_directory(directory)
-    create_subprocess(config, "background", directory_arg, config["profile"]["overall_time_in_seconds"])
-
-    end_time = datetime.now() + timedelta(seconds=int(config["profile"]["overall_time_in_seconds"]))
-    while datetime.now() < end_time:
-        run_spikes(config, root_log_folder_name)
-
-
-def stress_profile(config):
-    folder_count = 1  # ordering number of the spike which goes to logs folder name
-    root_log_folder_name = "Stress_log_{}".format(time.strftime("%m-%d-%y_%H-%M-%S"))
+    spike_time = config["profile"]["spike_time_in_seconds"]
+    rest_time = config["profile"]["rest_time_in_seconds"]
     overall_time_in_seconds = int(config["profile"]["overall_time_in_seconds"])
-    sub_process_type = "background"
-    if config["background"]["stepwise"]["step_time_in_seconds"] != 0:
-        step_time_in_seconds = int(config["background"]["stepwise"]["step_time_in_seconds"])
-        step_txns_per_second = int(config["background"]["stepwise"]["step_txns_per_second"])
-        step_load_rate = int(config["background"]["stepwise"]["step_initial_load_rate"])
-        steps_number = int(overall_time_in_seconds/step_time_in_seconds)
+    delays_list = []
+    args_list = []
+    background_plot = []
+    processes_dict = collect_processes(config)
+    if "background" in list(processes_dict.keys()):
+        delays_args_list = collect_delays(processes_dict["background"], overall_time_in_seconds)
+        delays_list.extend(delays_args_list[0])
+        args_list.extend(delays_args_list[1])
+        background_plot = prepare_plot_values(delays_args_list)
 
-        for i in range(0, steps_number):
-            load_time = overall_time_in_seconds - step_time_in_seconds * i
-            directory = [config["common"]["directory"], root_log_folder_name,
-                         "{}_{}".format(sub_process_type, folder_count)]
-            directory_arg = create_output_directory(directory)
-            create_subprocess(config, sub_process_type, directory_arg, load_time, step_load_rate)
-            step_load_rate = step_txns_per_second
-            time.sleep(step_time_in_seconds)
-            folder_count += 1
+    spike_plots_list = []
+    time_count = 0
+    spikes_list = filter(lambda i: "background" not in i, list(processes_dict.keys()))
+    spike_number = 0
+    for spike in spikes_list:
+        while time_count < overall_time_in_seconds:
+            spike_delay = (spike_time + rest_time) * spike_number
+            delays_args_list = (collect_delays(processes_dict[spike], spike_time, spike_delay))
+            delays_list.extend(delays_args_list[0])
+            args_list.extend(delays_args_list[1])
+            spike_plots_list.append(prepare_plot_values(delays_args_list))
+            spike_number += 1
+            time_count += spike_time + rest_time
+
+        spike_number = 0
+        time_count = 0
+
+    if args.graph:
+        build_plot_on_config(background_plot, spike_plots_list)
     else:
-        directory_parts = [config["common"]["directory"], root_log_folder_name, "Stable_stress"]
-        directory_arg = create_output_directory(directory_parts)
-        create_subprocess(config, sub_process_type, directory_arg, overall_time_in_seconds)
-        print("Stress stable")
+        prepared = order_processes(delays_list, args_list)
+        time_count = 0
+        for item in prepared.keys():
+            time.sleep(item - time_count)
+            for process_args in prepared[item]:
+                subprocess.Popen(process_args, close_fds=True)
+            time_count = item
+
+
+def prepare_plot_values(delays_args_list):
+    delays = delays_args_list[0]
+    args_for_script = delays_args_list[1]
+    plot_dict = {}
+    for i in range(0, len(delays)):
+        plot_dict.update({delays[i]: int(args_for_script[i][-1].split("=")[-1])})
+    plot_dict_sorted = collections.OrderedDict(sorted(plot_dict.items()))
+    return plot_dict_sorted
+
+
+def add_plot(ax, args_dict, color):
+    step_count = 1
+    time_ax = []
+    load_rate = []
+    for delay in args_dict.keys():
+        step_load_rate = args_dict[delay]
+        time_ax.append(delay)
+        if step_count != 1:
+            load_rate.append(load_rate[0] + step_load_rate * (step_count - 1))
+        else:
+            load_rate.append(step_load_rate)
+        step_count += 1
+    time_ax.append((time_ax[-1] - time_ax[-2]) + time_ax[-1])
+    load_rate.append((load_rate[-1] - load_rate[-2]) + load_rate[-1])
+    ax.fill_between(time_ax, load_rate, facecolor=color, alpha=0.4)
+
+
+def build_plot_on_config(background, spikes):
+    figure, ax = plt.subplots(1, 1)
+    if len(background) != 0:
+        add_plot(ax, background, 'b')
+    if len(spikes) != 0:
+        for spike in spikes:
+            add_plot(ax, spike, 'g')
+    start, stop = ax.get_ylim()
+    ticks = np.arange(start, stop + (stop // 10), stop // 10)
+    ax.set_yticks(ticks)
+    ax.grid()
+    plt.show()
 
 
 if __name__ == '__main__':
