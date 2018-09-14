1c9355738a8cea74278c4e1e3a8da923df7a63d4
dsurnin <dmitry.surnin@dsr-corporation.com>
2018-09-14 15:55:37 +0300

    Remove pcakge specific files due to jenkins issues
    Signed-off-by: dsurnin <dmitry.surnin@dsr-corporation.com>

--- scripts/performance/__metadata__.py
+++
@@ -1,9 +0,0 @@
-"""
-indy-node performance testing package metadata
-"""
-__version_info__ = (0, 1)
-__version__ = '.'.join(map(str, __version_info__))
-__author__ = "Hyperledger"
-__license__ = "Apache 2.0"
-
-__all__ = ['__version_info__', '__version__', '__author__', '__license__']
