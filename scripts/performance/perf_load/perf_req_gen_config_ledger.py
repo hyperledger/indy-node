c13a68030a595c972fe6a7b5d57170963c650cbf
NataliaDracheva <natalia.dracheva@dsr-corporation.com>
2018-10-31 14:38:56 +0000

    [INDY-1432] Added a request type for config ledger.
    Signed-off-by: NataliaDracheva <natalia.dracheva@dsr-corporation.com>

--- scripts/performance/perf_load/perf_req_gen_config_ledger.py
+++ scripts/performance/perf_load/perf_req_gen_config_ledger.py
@@ -1,17 +1,14 @@
-import libnacl
+import random
 from indy import ledger
 
-from perf_load.perf_utils import rawToFriendly
 from perf_load.perf_req_gen import RequestGenerator
 
 
-class RGConfig(RequestGenerator):
+class RGConfigChangeState(RequestGenerator):
     _req_types = ["111"]
 
     def _rand_data(self):
-        raw = libnacl.randombytes(16)
-        req_did = rawToFriendly(raw)
-        return req_did
+        return str(random.randint(0, 99999999))
 
     async def _gen_req(self, submit_did, req_data):
         return await ledger.build_pool_config_request(submit_did, True, False)
