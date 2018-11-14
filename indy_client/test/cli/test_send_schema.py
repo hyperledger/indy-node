82152c9d5f00fddac53834b2655691700ca4675d
Cam Parra <caeparra@gmail.com>
2018-11-13 18:23:12 -0700

    fixing testsendmultipleattributes to not use indy_client deps
    Signed-off-by: Cam Parra <caeparra@gmail.com>

--- indy_client/test/cli/test_send_schema.py
+++ indy_client/test/cli/test_send_schema.py
@@ -1,10 +1,24 @@
+import json
+
+from indy.anoncreds import issuer_create_schema
+from indy.ledger import build_schema_request
 from indy_client.test.cli.constants import SCHEMA_ADDED, SCHEMA_NOT_ADDED_DUPLICATE
+from indy_node.test.api.helper import sdk_write_schema
+from plenum.test.helper import sdk_get_reply, sdk_sign_and_submit_req
 
 
-def test_send_schema_multiple_attribs(be, do, poolNodesStarted, trusteeCli):
-    be(trusteeCli)
-    do('send SCHEMA name=Degree version=1.0 keys=attrib1,attrib2,attrib3',
-       expect=SCHEMA_ADDED, within=5)
+def testSendSchemaMultipleAttribs(looper, sdk_pool_handle, sdk_wallet_trust_anchor):
+
+    wallet_handle, identifier = sdk_wallet_trust_anchor
+
+    schema_json, _ = sdk_write_schema(looper, sdk_pool_handle, sdk_wallet_trust_anchor)
+    _, schema_json = looper.loop.run_until_complete(
+        issuer_create_schema(identifier, "name=Degree", "1.0", json.dumps(["attrib1","attrib2","attrib3"])))
+
+    request = looper.loop.run_until_complete(build_schema_request(identifier, schema_json))
+
+    return schema_json, \
+           sdk_get_reply(looper, sdk_sign_and_submit_req(sdk_pool_handle, sdk_wallet_trust_anchor, request))[1]
 
 
 def test_send_schema_one_attrib(be, do, poolNodesStarted, trusteeCli):
