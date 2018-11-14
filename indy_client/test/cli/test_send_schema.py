191af73e7408ae6a3476446245fb50fd49cd8a4b
Cam Parra <caeparra@gmail.com>
2018-11-14 09:22:54 -0700

    giving sdk check the right input
    Signed-off-by: Cam Parra <caeparra@gmail.com>

--- indy_client/test/cli/test_send_schema.py
+++ indy_client/test/cli/test_send_schema.py
@@ -3,16 +3,16 @@ from plenum.test.helper import sdk_check_reply
 
 
 def test_send_schema_multiple_attrib(looper, sdk_pool_handle, sdk_wallet_trust_anchor):
-    _, resp = sdk_write_schema(looper, sdk_pool_handle, sdk_wallet_trust_anchor, ["attrib1", "attrib2", "attrib3"])
-    sdk_check_reply(resp)
+    req_resp = sdk_write_schema(looper, sdk_pool_handle, sdk_wallet_trust_anchor, ["attrib1", "attrib2", "attrib3"])
+    sdk_check_reply(req_resp)
 
 
 def test_send_schema_one_attrib(looper, sdk_pool_handle, sdk_wallet_trust_anchor):
-    _, resp = sdk_write_schema(looper, sdk_pool_handle, sdk_wallet_trust_anchor, ["attrib1"])
-    sdk_check_reply(resp)
+    req_resp = sdk_write_schema(looper, sdk_pool_handle, sdk_wallet_trust_anchor, ["attrib1"])
+    sdk_check_reply(req_resp)
 
 
 def test_can_not_send_same_schema(looper, sdk_pool_handle, sdk_wallet_trust_anchor):
     sdk_write_schema(looper, sdk_pool_handle, sdk_wallet_trust_anchor, ["attrib1", "attrib2", "attrib3"])
-    _, resp = sdk_write_schema(looper, sdk_pool_handle, sdk_wallet_trust_anchor, ["attrib1", "attrib2", "attrib3"])
-    not sdk_check_reply(resp)
+    req_resp = sdk_write_schema(looper, sdk_pool_handle, sdk_wallet_trust_anchor, ["attrib1", "attrib2", "attrib3"])
+    sdk_check_reply(req_resp)
