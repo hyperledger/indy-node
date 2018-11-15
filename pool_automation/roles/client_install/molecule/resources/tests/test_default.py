5d115f1f7416b4d5023a854234ec449adc61bb15
Andrey Kononykhin <andkononykhin@gmail.com>
2018-11-15 17:45:46 +0300

    adds role to install indy-cli on client hosts
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

---
+++ pool_automation/roles/client_install/molecule/resources/tests/test_default.py
@@ -0,0 +1,11 @@
+
+def test_correct_package_versions_are_installed(host):
+    v = host.ansible.get_variables()
+
+    indy_cli = host.package('indy-cli')
+
+    assert indy_cli.is_installed
+    assert indy_cli.version == v['indy_cli_ver']
+
+def test_indy_cli_is_available_in_path(host):
+    assert host.exists('indy-cli')
