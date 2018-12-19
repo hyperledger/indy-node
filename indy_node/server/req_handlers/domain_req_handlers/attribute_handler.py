64e69a27ebc0a20d9472d4d64386fe059b8d3645
ArtObr <artemobruchnikov@gmail.com>
2018-12-19 18:37:10 +0300

    INDY-1856: New req handlers implementation
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- indy_node/server/req_handlers/domain_req_handlers/attribute_handler.py
+++ indy_node/server/req_handlers/domain_req_handlers/attribute_handler.py
@@ -1,5 +1,78 @@
+from indy_common.state import domain
+
+from indy_common.constants import ATTRIB
+from plenum.common.constants import DOMAIN_LEDGER_ID, RAW, ENC, HASH, TARGET_NYM
+from plenum.common.exceptions import InvalidClientRequest, UnauthorizedClientRequest
+
+from plenum.common.request import Request
+from plenum.common.txn_util import get_type
+from plenum.server.database_manager import DatabaseManager
 from plenum.server.request_handlers.handler_interfaces.write_request_handler import WriteRequestHandler
 
 
 class AttributeHandler(WriteRequestHandler):
-    pass
+
+    def __init__(self, config, database_manager: DatabaseManager):
+        super().__init__(config, database_manager, ATTRIB, DOMAIN_LEDGER_ID)
+
+    def static_validation(self, request: Request):
+        identifier, req_id, operation = request.identifier, request.reqId, request.operation
+
+        if not self._validate_attrib_keys(operation):
+            raise InvalidClientRequest(identifier, req_id,
+                                       '{} should have one and only one of '
+                                       '{}, {}, {}'
+                                       .format(ATTRIB, RAW, ENC, HASH))
+
+    def dynamic_validation(self, request: Request):
+        identifier, req_id, operation = request.identifier, request.reqId, request.operation
+
+        if not (not operation.get(TARGET_NYM) or
+                self.hasNym(operation[TARGET_NYM], isCommitted=False)):
+            raise InvalidClientRequest(identifier, req_id,
+                                       '{} should be added before adding '
+                                       'attribute for it'.
+                                       format(TARGET_NYM))
+
+        if operation.get(TARGET_NYM) and operation[TARGET_NYM] != identifier and \
+                not self.idrCache.getOwnerFor(operation[TARGET_NYM],
+                                              isCommitted=False) == identifier:
+            raise UnauthorizedClientRequest(
+                identifier,
+                req_id,
+                "Only identity owner/guardian can add attribute "
+                "for that identity")
+
+    def gen_txn_path(self, txn):
+        path = domain.prepare_attr_for_state(txn, path_only=True)
+        return path.decode()
+
+    def _updateStateWithSingleTxn(self, txn, isCommitted=False) -> None:
+        """
+        The state trie stores the hash of the whole attribute data at:
+            the did+attribute name if the data is plaintext (RAW)
+            the did+hash(attribute) if the data is encrypted (ENC)
+        If the attribute is HASH, then nothing is stored in attribute store,
+        the trie stores a blank value for the key did+hash
+        """
+        assert get_type(txn) == ATTRIB
+        attr_type, path, value, hashed_value, value_bytes = domain.prepare_attr_for_state(txn)
+        self.state.set(path, value_bytes)
+        if attr_type != HASH:
+            self.attributeStore.set(hashed_value, value)
+
+    def hasNym(self, nym, isCommitted: bool = True):
+        return self.idrCache.hasNym(nym, isCommitted=isCommitted)
+
+    @staticmethod
+    def _validate_attrib_keys(operation):
+        dataKeys = {RAW, ENC, HASH}.intersection(set(operation.keys()))
+        return len(dataKeys) == 1
+
+    @property
+    def idrCache(self):
+        return self.database_manager.get_store('idr')
+
+    @property
+    def attributeStore(self):
+        return self.database_manager.get_store('attrib')
--- indy_node/server/req_handlers/domain_req_handlers/revoc_reg_def_handler.py
+++ indy_node/server/req_handlers/domain_req_handlers/revoc_reg_def_handler.py
@@ -1,5 +1,33 @@
+from indy_common.constants import REVOC_REG_DEF
+from plenum.common.constants import DOMAIN_LEDGER_ID
+from plenum.common.request import Request
+
+from plenum.server.database_manager import DatabaseManager
 from plenum.server.request_handlers.handler_interfaces.write_request_handler import WriteRequestHandler
 
 
 class RevocationRegDefHandler(WriteRequestHandler):
-    pass
+
+    def __init__(self, config, database_manager: DatabaseManager):
+        super().__init__(config, database_manager, REVOC_REG_DEF, DOMAIN_LEDGER_ID)
+
+    def static_validation(self, request: Request):
+        pass
+
+    def dynamic_validation(self, request: Request):
+        pass
+
+    def gen_txn_path(self, txn):
+        path = domain.prepare_revoc_reg_entry_for_state(txn, path_only=True)
+        return path.decode()
+
+    def _updateStateWithSingleTxn(self, txn, isCommitted=False):
+        current_entry, revoc_def = self._get_current_revoc_entry_and_revoc_def(
+            author_did=get_from(txn),
+            revoc_reg_def_id=get_payload_data(txn)[REVOC_REG_DEF_ID],
+            req_id=get_req_id(txn)
+        )
+        writer_cls = self.get_revocation_strategy(
+            revoc_def[VALUE][ISSUANCE_TYPE])
+        writer = writer_cls(self.state)
+        writer.write(current_entry, txn)
