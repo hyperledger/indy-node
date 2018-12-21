34bc25c7580dbc05b7815a1ad44d81c6352978a7
ArtObr <artemobruchnikov@gmail.com>
2018-12-21 10:02:31 +0300

    INDY-1856: Tests for nym_handler
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

---
+++ indy_node/test/request_handlers/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from indy_common.config_helper import NodeConfigHelper
+
+from indy_node.persistence.idr_cache import IdrCache
+from plenum.common.constants import KeyValueStorageType
+
+from plenum.server.database_manager import DatabaseManager
+from storage.helper import initKeyValueStorage
+
+
+@pytest.fixture(scope="module")
+def db_manager(tconf, tdir):
+
+    db_manager = DatabaseManager()
+    name = 'name'
+    idr_cache = IdrCache(name,
+                             initKeyValueStorage(KeyValueStorageType.Rocksdb,
+                                                 tdir,
+                                                 tconf.idrCacheDbName,
+                                                 db_config=tconf.db_idr_cache_db_config))
+    db_manager.register_new_store('idr', idr_cache)
+    return db_manager
+
---
+++ indy_node/test/request_handlers/test_nym_handler.py
@@ -0,0 +1,104 @@
+import random
+
+import pytest
+from indy_common.auth import Authoriser
+
+from indy_common.constants import NYM
+
+from indy_node.server.request_handlers.domain_req_handlers.nym_handler import NymHandler
+from plenum.common.constants import STEWARD, TRUSTEE
+from plenum.common.exceptions import InvalidClientRequest, UnauthorizedClientRequest
+from plenum.common.request import Request
+
+from plenum.common.util import randomString
+
+
+@pytest.fixture(scope="module")
+def nym_handler(db_manager):
+    return NymHandler(None, db_manager)
+
+
+@pytest.fixture(scope="module")
+def creator(db_manager):
+    identifier = randomString()
+    idr = db_manager.get_store('idr')
+    add_to_idr(idr, identifier, None)
+    return identifier
+
+
+@pytest.fixture(scope="function")
+def nym_request(creator):
+    return Request(identifier=creator,
+                   reqId=5,
+                   operation={'type': NYM,
+                              'dest': randomString(),
+                              'role': None,
+                              'verkey': randomString()})
+
+
+def add_to_idr(idr, identifier, role):
+    random_s = randomString()
+    idr.set(identifier,
+            seqNo=5,
+            txnTime=random.randint(10, 100000),
+            ta=random_s,
+            role=role,
+            verkey=random_s,
+            isCommitted=True)
+
+
+def test_nym_static_validation_passes(nym_request, nym_handler: NymHandler):
+    nym_handler.static_validation(nym_request)
+
+
+def test_nym_static_validation_failed_without_dest(nym_request, nym_handler: NymHandler):
+    del nym_request.operation['dest']
+    with pytest.raises(InvalidClientRequest):
+        nym_handler.static_validation(nym_request)
+
+
+def test_nym_static_validation_failed_with_none_dest(nym_request, nym_handler: NymHandler):
+    nym_request.operation['dest'] = None
+    with pytest.raises(InvalidClientRequest):
+        nym_handler.static_validation(nym_request)
+
+
+def test_nym_static_validation_failed_with_empty_dest(nym_request, nym_handler: NymHandler):
+    nym_request.operation['dest'] = ''
+    with pytest.raises(InvalidClientRequest):
+        nym_handler.static_validation(nym_request)
+
+
+def test_nym_static_validation_failed_with_spaced_dest(nym_request, nym_handler: NymHandler):
+    nym_request.operation['dest'] = ' ' * 5
+    with pytest.raises(InvalidClientRequest):
+        nym_handler.static_validation(nym_request)
+
+
+def test_nym_static_validation_authorized(nym_request, nym_handler: NymHandler):
+    for role in Authoriser.ValidRoles:
+        nym_request.operation['role'] = role
+        nym_handler.static_validation(nym_request)
+
+
+def test_nym_static_validation_not_authorized_random(nym_request, nym_handler: NymHandler):
+    nym_request.operation['role'] = randomString()
+    with pytest.raises(InvalidClientRequest):
+        nym_handler.static_validation(nym_request)
+
+
+def test_nym_dynamic_validation_for_new_nym(nym_request, nym_handler: NymHandler, creator):
+    for role in Authoriser.ValidRoles:
+        nym_request.operation['role'] = role
+        with pytest.raises(UnauthorizedClientRequest):
+            nym_handler.dynamic_validation(nym_request)
+    add_to_idr(nym_handler.idrCache, creator, STEWARD)
+    nym_handler.dynamic_validation(nym_request)
+
+
+def test_nym_dynamic_validation_for_existing_nym(nym_request: Request, nym_handler: NymHandler, creator):
+    add_to_idr(nym_handler.idrCache, nym_request.operation['dest'], None)
+    with pytest.raises(UnauthorizedClientRequest):
+        nym_handler.dynamic_validation(nym_request)
+    nym_request._identifier = nym_request.operation['dest']
+    nym_handler.dynamic_validation(nym_request)
