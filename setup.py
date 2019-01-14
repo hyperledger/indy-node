70c38acbb43185653d076977baea180322fb6c6b
ArtObr <artemobruchnikov@gmail.com>
2019-01-14 10:03:45 +0300

    INDY-1919: Review fix
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- setup.py
+++ setup.py
@@ -58,8 +58,7 @@ setup(
     )],
     install_requires=['indy-plenum-dev==1.6.646',
                       'python-dateutil',
-                      'timeout-decorator==0.4.0',
-                      'distro==1.3.0'],
+                      'timeout-decorator==0.4.0'],
     setup_requires=['pytest-runner'],
     extras_require={
         'tests': tests_require
