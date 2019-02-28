00ee9a7cb0b87533520f8d24228b04f15c687eb0
Andrey Kononykhin <andkononykhin@gmail.com>
2019-02-28 12:46:27 +0300

    adds tests for NodeControlUtil
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

---
+++ indy_node/test/node_control_utils/test_node_control_util.py
@@ -0,0 +1,69 @@
+import pytest
+import shutil
+
+
+from indy_node.utils.node_control_utils import NodeControlUtil
+
+generated_command = None
+
+
+@pytest.fixture
+def catch_generated_command(monkeypatch):
+    global generated_command
+    generated_command = None
+
+    def _f(command, *args, **kwargs):
+        global generated_command
+        generated_command = command
+        return ''
+
+    monkeypatch.setattr(NodeControlUtil, 'run_shell_script', _f)
+    monkeypatch.setattr(NodeControlUtil, 'run_shell_command', _f)
+
+
+def test_generated_cmd_get_curr_info(catch_generated_command):
+    pkg_name = 'some_package'
+    # TODO not an API for now
+    NodeControlUtil._get_curr_info(pkg_name)
+    assert generated_command == "dpkg -s {}".format(pkg_name)
+
+
+def test_generated_cmd_get_latest_pkg_version(catch_generated_command):
+    pkg_name = 'some_package'
+    NodeControlUtil.get_latest_pkg_version(pkg_name)
+    assert generated_command == (
+        "apt-cache show {} | grep -E '^Version: ([0-9]+:)?{}(-|$)'"
+        .format(pkg_name, '.*')
+    )
+
+    upstream_version = '1.2.3'
+    NodeControlUtil.get_latest_pkg_version(pkg_name, upstream=upstream_version)
+    assert generated_command == (
+        "apt-cache show {} | grep -E '^Version: ([0-9]+:)?{}(-|$)'"
+        .format(pkg_name, upstream_version)
+    )
+
+
+def test_generated_cmd_get_info_from_package_manager(catch_generated_command):
+    packages = ['package1', 'package2']
+    # TODO not an API for now
+    NodeControlUtil._get_info_from_package_manager(*packages)
+    assert generated_command == "apt-cache show {}".format(" ".join(packages))
+
+
+def test_generated_cmd_update_package_cache(catch_generated_command):
+    NodeControlUtil.update_package_cache()
+    assert generated_command == "apt update"
+
+
+def test_generated_cmd_get_sys_holds(monkeypatch, catch_generated_command):
+    monkeypatch.setattr(shutil, 'which', lambda *_: 'path')
+    NodeControlUtil.get_sys_holds()
+    assert generated_command == "apt-mark showhold"
+
+
+def test_generated_cmd_hold_packages(monkeypatch, catch_generated_command):
+    packages = ['package1', 'package2']
+    monkeypatch.setattr(shutil, 'which', lambda *_: 'path')
+    NodeControlUtil.hold_packages(packages)
+    assert generated_command == "apt-mark hold {}".format(' '.join(packages))
