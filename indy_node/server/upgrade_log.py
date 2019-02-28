3146004386f01737c3d2372640731efa259730e2
Andrey Kononykhin <andkononykhin@gmail.com>
2019-02-28 08:03:47 +0300

    updates UpgradeLog and its tests
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- indy_node/server/upgrade_log.py
+++ indy_node/server/upgrade_log.py
@@ -5,26 +5,18 @@ from indy_node.server.action_log import ActionLogData, ActionLog
 
 # TODO tests
 class UpgradeLogData(ActionLogData):
+    _items = ActionLogData._items + ['version', 'upgrade_id', 'pkg_name']
+
     def __init__(self, when: datetime,
                  version: str, upgrade_id: str, pkg_name: str):
-        super().__new__(when)
+        super().__init__(when)
         self.version = version
         self.upgrade_id = upgrade_id
         self.pkg_name = pkg_name
-
-    @property
-    def packed(self):
-        return (super().packed +
-                [self.version, self.upgrade_id, self.pkg_name])
-
-    @staticmethod
-    def parse(when, version, upgrade_id, pkg_name):
-        return ActionLogData.parse(when) + [version, upgrade_id, pkg_name]
 
 
 class UpgradeLog(ActionLog):
     """
     Append-only event log of upgrade event
     """
-    def __init__(self, *args, data_class=UpgradeLogData, **kwargs):
-        super().__init__(*args, data_class=data_class, **kwargs)
+    pass
--- indy_node/test/upgrade/test_upgrade_log.py
+++ indy_node/test/upgrade/test_upgrade_log.py
@@ -1,44 +1,10 @@
-import dateutil
-
-from indy_node.server.upgrade_log import UpgradeLog
+from indy_node.server.upgrade_log import UpgradeLogData
 from datetime import datetime
-import os
-from os import path
 
 
-tmpDir = "/tmp/indy/"
-tmpFileName = "upgrade_log_test_file"
-tmpPkgName = "tmpPkgName"
-
-
-def test_update_log():
-    """
-    Test for UpgradeLog class.
-    Adds record to it, gets it back and compares.
-    Then opens the same file file another instance and tries to
-    read added record
-    """
-    tmpFilePath = path.join(tmpDir, tmpFileName)
-    if not os.path.exists(tmpDir):
-        os.makedirs(tmpDir)
-    elif os.path.exists(tmpFilePath):
-        os.remove(tmpFilePath)
-    log = UpgradeLog(tmpFilePath)
-    assert log.lastEvent is None
-
-    now = datetime.utcnow().replace(tzinfo=dateutil.tz.tzutc())
-    version = "1.2.3"
-    upgrade_id = '1'
-
-    # Check that we can add and then get event
-    log.appendScheduled(now, version, upgrade_id, tmpPkgName)
-    last = log.lastEvent
-    assert last[1] is UpgradeLog.SCHEDULED
-    assert last[2] == now
-    assert last[3] == version
-    assert last[4] == upgrade_id
-    assert last[5] == tmpPkgName
-
-
-    # Check that the we can load and parse the line we appended before
-    assert UpgradeLog(tmpFilePath).lastEvent == last
+def test_upgrade_log_data_pack_unpack():
+    delimiter = '|'
+    data = UpgradeLogData(datetime.utcnow(), '1.2.3', 'some_id', 'some_pkg')
+    assert data == UpgradeLogData.unpack(
+        data.pack(delimiter=delimiter), delimiter=delimiter
+    )
