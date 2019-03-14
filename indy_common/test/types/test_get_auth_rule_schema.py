f80ba8038479604fe530f50743b09f937160956f
toktar <renata.toktar@dsr-corporation.com>
2019-03-14 11:25:15 +0300

    INDY-2010: fix ClientGetAuthRuleOperation schema tests
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- indy_common/test/types/test_get_auth_rule_schema.py
+++ indy_common/test/types/test_get_auth_rule_schema.py
@@ -1,9 +1,7 @@
 from collections import OrderedDict
 
-from indy_common.types import ClientSchemaOperation, SchemaField, ConstraintListField, ClientAuthRuleOperation, \
-    ConstraintEntityField, ConstraintField
-from plenum.common.messages.fields import ConstantField, VersionField, IterableField, LimitedLengthStringField, \
-    ChooseField, RoleField, NonNegativeNumberField, BooleanField, AnyMapField
+from indy_common.types import ClientGetAuthRuleOperation
+from plenum.common.messages.fields import ConstantField, LimitedLengthStringField, ChooseField
 
 EXPECTED_ORDERED_FIELDS = OrderedDict([
     ("type", ConstantField),
@@ -16,11 +14,11 @@ EXPECTED_ORDERED_FIELDS = OrderedDict([
 
 
 def test_has_expected_fields():
-    actual_field_names = OrderedDict(ClientAuthRuleOperation.schema).keys()
+    actual_field_names = OrderedDict(ClientGetAuthRuleOperation.schema).keys()
     assert actual_field_names == EXPECTED_ORDERED_FIELDS.keys()
 
 
 def test_has_expected_validators():
-    schema = dict(ClientAuthRuleOperation.schema)
+    schema = dict(ClientGetAuthRuleOperation.schema)
     for field, validator in EXPECTED_ORDERED_FIELDS.items():
         assert isinstance(schema[field], validator)
