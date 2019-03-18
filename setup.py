72ecf718db8dc5f5fb85c23d5e07e8a6f7f35310
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2019-03-18 15:53:02 +0300

    Bump plenum version
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum-dev==1.6.726',
+    install_requires=['indy-plenum-dev==1.6.729',
                       'python-dateutil',
                       'timeout-decorator==0.4.0',
                       'distro==1.3.0'],
