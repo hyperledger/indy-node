5037174b9f62f94e52280bc1ec981b7d01a11118
toktar <renata.toktar@dsr-corporation.com>
2019-04-12 19:36:55 +0300

    INDY-2043: Change test_txn_with_different_signature_and_idr.py
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- indy_node/test/auth_rule/test_txn_with_different_signature_and_idr.py
+++ indy_node/test/auth_rule/test_txn_with_different_signature_and_idr.py
@@ -1,18 +1,27 @@
+import json
 import pytest
 
-from indy_node.test.auth_rule.helper import add_new_nym
 from plenum.common.constants import STEWARD_STRING
-from plenum.common.exceptions import RequestRejectedException
+from plenum.common.exceptions import RequestNackedException
+from plenum.common.util import randomString
+from plenum.test.helper import sdk_multi_sign_request_objects, sdk_json_to_request_object, sdk_send_signed_requests, \
+    sdk_get_and_check_replies
+from plenum.test.pool_transactions.helper import prepare_nym_request
 
 
 def test_send_same_txn_with_different_signatures_in_separate_batches(
         looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_trustee, sdk_wallet_client):
+    # filling nym request and getting steward did
+    nym_request, new_did = looper.loop.run_until_complete(
+        prepare_nym_request(sdk_wallet_trustee, randomString(32),
+                            'newSteward1', STEWARD_STRING))
 
-    wh, did = sdk_wallet_trustee
-    with pytest.raises(RequestRejectedException, match="Not enough TRUSTEE signatures"):
-        add_new_nym(looper,
-                    sdk_pool_handle,
-                    [sdk_wallet_client],
-                    'newSteward1',
-                    STEWARD_STRING,
-                    dest=did)
+    # sending request using 'sdk_' functions
+    signed_reqs = sdk_multi_sign_request_objects(looper, [sdk_wallet_client],
+                                                 [sdk_json_to_request_object(
+                                                     json.loads(nym_request))])
+    request_couple = sdk_send_signed_requests(sdk_pool_handle, signed_reqs)[0]
+
+    with pytest.raises(RequestNackedException, match="IdentifierNotInSignatures"):
+        # waiting for replies
+        sdk_get_and_check_replies(looper, [request_couple])
