5302e0a6db60d7cc640a7f5e3f626ceda32a724e
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2019-05-13 19:40:48 +0300

    INDY-2066: Improve tests
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- indy_node/test/txn_author_agreement/test_node_taa.py
+++ indy_node/test/txn_author_agreement/test_node_taa.py
@@ -1,27 +1,30 @@
 import json
 
 import pytest
+from indy.ledger import build_txn_author_agreement_request
 
 from indy_common.authorize.auth_map import steward_or_trustee_constraint
 from indy_node.server.config_req_handler import ConfigReqHandler
 from indy_node.test.auth_rule.helper import sdk_send_and_check_auth_rule_request
 from plenum.common.constants import TXN_AUTHOR_AGREEMENT_VERSION, TXN_AUTHOR_AGREEMENT_TEXT, TXN_AUTHOR_AGREEMENT
 from plenum.common.exceptions import RequestRejectedException
-from plenum.common.types import OPERATION
-from plenum.test.helper import sdk_get_and_check_replies
+from plenum.common.util import randomString
+from plenum.test.helper import sdk_get_and_check_replies, sdk_sign_and_submit_req
 from plenum.test.node_catchup.helper import ensure_all_nodes_have_same_data
-from plenum.test.pool_transactions.helper import sdk_sign_and_send_prepared_request
-from plenum.test.txn_author_agreement.helper import prepare_txn_author_agreement, get_config_req_handler
+from plenum.test.txn_author_agreement.helper import get_config_req_handler
+
+
+def sdk_send_txn_author_agreement(looper, sdk_pool_handle, sdk_wallet, text: str, version: str):
+    req = looper.loop.run_until_complete(build_txn_author_agreement_request(sdk_wallet[1], text, version))
+    rep = sdk_sign_and_submit_req(sdk_pool_handle, sdk_wallet, req)
+    return sdk_get_and_check_replies(looper, [rep])
 
 
 def test_send_valid_txn_author_agreement_succeeds(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_trustee):
-    req = looper.loop.run_until_complete(prepare_txn_author_agreement(sdk_wallet_trustee[1]))
-    rep = sdk_sign_and_send_prepared_request(looper, sdk_wallet_trustee, sdk_pool_handle, req)
-    sdk_get_and_check_replies(looper, [rep])
+    text = randomString(1024)
+    version = randomString(16)
+    sdk_send_txn_author_agreement(looper, sdk_pool_handle, sdk_wallet_trustee, text, version)
 
-    req = json.loads(req)
-    version = req[OPERATION][TXN_AUTHOR_AGREEMENT_VERSION]
-    text = req[OPERATION][TXN_AUTHOR_AGREEMENT_TEXT]
     digest = ConfigReqHandler._taa_digest(version, text)
 
     # TODO: Replace this with get transaction
@@ -41,11 +44,10 @@ def test_send_valid_txn_author_agreement_succeeds(looper, txnPoolNodeSet, sdk_po
 
 def test_send_valid_txn_author_agreement_without_enough_privileges_fails(looper, txnPoolNodeSet, sdk_pool_handle,
                                                                          sdk_wallet_steward):
-    req = looper.loop.run_until_complete(prepare_txn_author_agreement(sdk_wallet_steward[1]))
-    rep = sdk_sign_and_send_prepared_request(looper, sdk_wallet_steward, sdk_pool_handle, req)
-
+    text = randomString(1024)
+    version = randomString(16)
     with pytest.raises(RequestRejectedException):
-        sdk_get_and_check_replies(looper, [rep])
+        sdk_send_txn_author_agreement(looper, sdk_pool_handle, sdk_wallet_steward, text, version)
 
 
 def test_txn_author_agreement_respects_current_auth_rules(looper, txnPoolNodeSet, sdk_pool_handle,
@@ -54,6 +56,6 @@ def test_txn_author_agreement_respects_current_auth_rules(looper, txnPoolNodeSet
                                          auth_type=TXN_AUTHOR_AGREEMENT, field='*', new_value='*',
                                          constraint=steward_or_trustee_constraint.as_dict)
 
-    req = looper.loop.run_until_complete(prepare_txn_author_agreement(sdk_wallet_steward[1]))
-    rep = sdk_sign_and_send_prepared_request(looper, sdk_wallet_steward, sdk_pool_handle, req)
-    sdk_get_and_check_replies(looper, [rep])
+    text = randomString(1024)
+    version = randomString(16)
+    sdk_send_txn_author_agreement(looper, sdk_pool_handle, sdk_wallet_steward, text, version)
