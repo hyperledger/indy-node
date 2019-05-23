9733ee98a911b5e391643cbbd18a92dd4ea2afe4
ArtObr <artemobruchnikov@gmail.com>
2019-05-23 11:28:03 +0300

    INDY-2091: Review fixes
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- scripts/performance/perf_load/perf_req_gen_get_auth_rules.py
+++ scripts/performance/perf_load/perf_req_gen_get_auth_rules.py
@@ -7,7 +7,8 @@ class RGGetAuthRules(RequestGenerator):
     _req_types = ["121"]
 
     def _rand_data(self):
-        return ('1', 'ADD', 'role')
+        return ('102', 'ADD', '*', '*')
 
     async def _gen_req(self, submitter_did, req_data):
-        return await ledger.build_get_auth_rule_request(None, req_data[0], req_data[1], req_data[2], None, None)
+        return await ledger.build_get_auth_rule_request(submitter_did, req_data[0], req_data[1],
+                                                        req_data[2], None, req_data[3])
