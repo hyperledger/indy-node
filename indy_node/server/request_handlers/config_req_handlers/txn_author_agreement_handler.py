1663aadd9b52e3935503eafe7b00c2d7f37de5c5
toktar <renata.toktar@dsr-corporation.com>
2019-05-31 12:19:37 +0300

    INDY-2097: add TAA handlers
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

---
+++ indy_node/server/request_handlers/config_req_handlers/txn_author_agreement_handler.py
@@ -0,0 +1,23 @@
+from indy_common.authorize.auth_actions import AuthActionAdd
+from indy_common.authorize.auth_request_validator import WriteRequestValidator
+from plenum.common.request import Request
+from plenum.server.database_manager import DatabaseManager
+from plenum.server.request_handlers.txn_author_agreement_handler import TxnAuthorAgreementHandler \
+    as PTxnAuthorAgreementHandler
+
+class TxnAuthorAgreementHandler(PTxnAuthorAgreementHandler):
+
+    def __init__(self, database_manager: DatabaseManager, bls_crypto_verifier,
+                 write_request_validator: WriteRequestValidator):
+        super().__init__(database_manager, bls_crypto_verifier)
+        self.write_request_validator = write_request_validator
+
+    def dynamic_validation(self, request: Request):
+        super().dynamic_validation(request)
+        self.write_request_validator.validate(request,
+                                              [AuthActionAdd(txn_type=self.txn_type,
+                                                             field='*',
+                                                             value='*')])
+
+    def authorize(self, request):
+        pass
