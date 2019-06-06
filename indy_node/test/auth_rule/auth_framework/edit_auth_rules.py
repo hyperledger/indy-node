eb362f9c3e9d7d9b1f54b7183e768373f2088725
toktar <renata.toktar@dsr-corporation.com>
2019-06-06 21:40:11 +0300

    INDY-2097: bump plenum version, use libindy auth_rules
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- indy_node/test/auth_rule/auth_framework/edit_auth_rules.py
+++ indy_node/test/auth_rule/auth_framework/edit_auth_rules.py
@@ -6,8 +6,8 @@ from indy_common.authorize.auth_actions import AuthActionEdit, EDIT_PREFIX
 from indy_common.authorize.auth_constraints import AuthConstraint
 from indy_common.constants import AUTH_RULES, RULES
 from indy_node.test.auth_rule.auth_framework.basic import AuthTest
-from indy_node.test.auth_rule.helper import generate_auth_rule_operation, \
-    sdk_send_and_check_auth_rules_request, generate_auth_rule
+from indy_node.test.helper import generate_auth_rule
+from indy_node.test.helper import sdk_send_and_check_auth_rules_request
 from plenum.common.constants import TXN_TYPE
 from plenum.common.exceptions import RequestRejectedException
 from plenum.test.helper import sdk_gen_request
@@ -26,27 +26,27 @@ class AuthRulesTest(AuthTest):
 
     def run(self):
         # Step 1. Check default auth rule
-        sdk_send_and_check_auth_rules_request(self.looper, self.trustee_wallet, self.sdk_pool_handle)
+        sdk_send_and_check_auth_rules_request(self.looper, self.sdk_pool_handle, self.trustee_wallet)
         with pytest.raises(RequestRejectedException):
-            sdk_send_and_check_auth_rules_request(self.looper, self.new_default_wallet, self.sdk_pool_handle)
+            sdk_send_and_check_auth_rules_request(self.looper, self.sdk_pool_handle, self.new_default_wallet)
 
         # Step 2. Change auth rule
         self.send_and_check(self.changed_auth_rule, self.trustee_wallet)
 
         # Step 3. Check, that we cannot send txn the old way
         with pytest.raises(RequestRejectedException):
-            sdk_send_and_check_auth_rules_request(self.looper, self.trustee_wallet, self.sdk_pool_handle)
+            sdk_send_and_check_auth_rules_request(self.looper, self.sdk_pool_handle, self.trustee_wallet)
 
         # Step 4. Check, that new auth rule is used
-        sdk_send_and_check_auth_rules_request(self.looper, self.new_default_wallet, self.sdk_pool_handle)
+        sdk_send_and_check_auth_rules_request(self.looper, self.sdk_pool_handle, self.new_default_wallet)
 
         # Step 5. Return default auth rule
         self.send_and_check(self.default_auth_rule, self.new_default_wallet)
 
         # Step 6. Check, that default auth rule works
-        sdk_send_and_check_auth_rules_request(self.looper, self.trustee_wallet, self.sdk_pool_handle)
+        sdk_send_and_check_auth_rules_request(self.looper, self.sdk_pool_handle, self.trustee_wallet)
         with pytest.raises(RequestRejectedException):
-            sdk_send_and_check_auth_rules_request(self.looper, self.new_default_wallet, self.sdk_pool_handle)
+            sdk_send_and_check_auth_rules_request(self.looper, self.sdk_pool_handle, self.new_default_wallet)
 
     def result(self):
         pass
