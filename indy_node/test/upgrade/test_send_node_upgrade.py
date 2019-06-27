1cc92727a58688d27c28ee22001c5221d587c031
ArtObr <artemobruchnikov@gmail.com>
2019-06-27 17:35:35 +0300

    Review fixes
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- indy_node/test/upgrade/test_send_node_upgrade.py
+++ indy_node/test/upgrade/test_send_node_upgrade.py
@@ -12,7 +12,7 @@ from plenum.common.constants import TXN_TYPE, CURRENT_PROTOCOL_VERSION
 from plenum.common.types import OPERATION, f
 
 
-def test_send_audit_txn1(looper, sdk_wallet_client, sdk_pool_handle):
+def test_send_node_upgrade_txn(looper, sdk_wallet_client, sdk_pool_handle):
     req = {
         OPERATION: {
             TXN_TYPE: NODE_UPGRADE,
