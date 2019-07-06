a2626f78b4367036e2c331dfc47099ea6bcad1a9
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2019-07-06 20:53:08 +0300

    [INDY-2154] bump plenum version
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum==1.9.0.dev829',
+    install_requires=['indy-plenum==1.9.0.dev832',
                       'python-dateutil',
                       'timeout-decorator==0.4.0',
                       'distro==1.3.0'],
