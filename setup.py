3be9f0efc75731cb4bfdaf2c8980879e7ca5fd63
toktar <renata.toktar@dsr-corporation.com>
2019-07-24 12:54:12 +0300

    Bump indy-node version to 1.9.1
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum==1.9.0.dev850',
+    install_requires=['indy-plenum==1.9.1.dev852',
                       'python-dateutil',
                       'timeout-decorator==0.4.0',
                       'distro==1.3.0'],
