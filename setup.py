63dbfc6228bb9e31969b5344cec95b5f31f466a4
toktar <renata.toktar@dsr-corporation.com>
2019-10-11 16:08:26 +0300

    INDY-2140: fix pytest-forked version
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ with open(metadata['__file__'], 'r') as f:
 
 BASE_DIR = os.path.join(os.path.expanduser("~"), ".indy")
 
-tests_require = ['attrs==19.1.0', 'pytest==3.3.1', 'pytest-xdist==1.22.1', 'python3-indy==1.11.1-dev-1343', 'pytest-asyncio==0.8.0']
+tests_require = ['attrs==19.1.0', 'pytest==3.3.1', 'pytest-xdist==1.22.1', 'pytest-forked==0.2',
+                 'python3-indy==1.11.1-dev-1343', 'pytest-asyncio==0.8.0']
 
 setup(
     name=metadata['__title__'],
