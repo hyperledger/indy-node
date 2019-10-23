ac7853542be78952fd802728ba68b9c63cef2f6b
toktar <renata.toktar@dsr-corporation.com>
2019-10-23 16:36:52 +0300

    INDY-2247: bump plenum version
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum==1.11.0.dev938',
+    install_requires=['indy-plenum==1.11.0.dev939',
                       'timeout-decorator==0.4.0',
                       'distro==1.3.0'],
     setup_requires=['pytest-runner'],
