cd3b2a09659bc2c375c947be370048b07d3c595a
toktar <renata.toktar@dsr-corporation.com>
2019-12-24 12:13:44 +0300

    INDY-2316: bump plenum
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- indy_node/test/txn_author_agreement/test_recover_taa_from_ledger.py
+++ indy_node/test/txn_author_agreement/test_recover_taa_from_ledger.py
@@ -1,3 +1,5 @@
+import pytest
+
 from indy_common.state.state_constants import LAST_UPDATE_TIME
 from plenum.common.constants import TXN_AUTHOR_AGREEMENT, TXN_AUTHOR_AGREEMENT_DIGEST, \
     TXN_AUTHOR_AGREEMENT_RATIFICATION_TS, TXN_AUTHOR_AGREEMENT_VERSION, TXN_AUTHOR_AGREEMENT_RETIREMENT_TS, \
@@ -12,6 +14,7 @@ from plenum.test.pool_transactions.helper import disconnect_node_and_ensure_disc
 from plenum.test.txn_author_agreement.helper import sdk_send_txn_author_agreement, sdk_get_txn_author_agreement
 
 
+@pytest.mark.skip()
 def test_recover_taa_from_ledger(txnPoolNodeSet,
                                  sdk_pool_handle,
                                  sdk_wallet_trustee,
