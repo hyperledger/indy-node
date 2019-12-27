25c1bd9db2b81c3fab1bdaf7f9370e8433ba3099
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2019-12-28 00:10:10 +0300

    bump plenum
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-corporation.com>

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum==1.12.1.dev993',
+    install_requires=['indy-plenum==1.12.1.dev995',
                       'timeout-decorator==0.4.0',
                       'distro==1.3.0'],
     setup_requires=['pytest-runner'],
