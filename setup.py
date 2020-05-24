b6f3b13c7aff75d0403c8a2dde0407c3ba8a4a32
toktar <renata.toktar@dsr-corporation.com>
2020-05-24 09:38:06 +0300

    bump plenum
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/nssm_original.exe'])
     )],
-    install_requires=['indy-plenum==1.13.0.dev1021',
+    install_requires=['indy-plenum==1.13.0.dev1024',
                       'timeout-decorator==0.4.0',
                       'distro==1.3.0'],
     setup_requires=['pytest-runner'],
