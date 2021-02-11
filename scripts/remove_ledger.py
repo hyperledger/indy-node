ac5a12bae2b2d9b3133f81cfbcd77c85afa3abfb
anton.denishchenko <anton.denishchenko@evernym.com>
2021-02-11 11:46:36 +0300

    Fix after review
    Signed-off-by: anton.denishchenko <anton.denishchenko@evernym.com>

--- scripts/remove_ledger.py
+++ scripts/remove_ledger.py
@@ -3,21 +3,21 @@
 from pathlib import Path
 import shutil
 from sys import argv
-script, file_path = argv
+script, ledger_name = argv
 from indy_common.config_util import getConfig
 from indy_common.config_helper import ConfigHelper
 
 config = getConfig()
 config_helper = ConfigHelper(config)
 
-def main(file_path):
+def remove(ledger_name):
     exceptions = ["domen", "config", "pool", "audit"]
-    if file_path not in exceptions:
-        for path in Path(config_helper.ledger_data_dir).rglob(file_path + "_*"):
+    if ledger_name not in exceptions:
+        for path in Path(config_helper.ledger_data_dir).rglob(ledger_name + "_*"):
             shutil.rmtree(str(path))
-            print('follow directory was deleted: ' + path.name)
+            print('The follow directory was deleted: ' + path.name)
     else:
-        print('Can`t delete ledger: ' + file_path)
+        print('Can`t delete built in ledger: ' + ledger_name)
 
 if __name__ == '__main__':
-    main(file_path)
+    remove(ledger_name)
