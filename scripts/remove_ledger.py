95d721bd33842a38f15eade889ea88eff77b6920
anton.denishchenko <anton.denishchenko@evernym.com>
2021-02-15 01:10:35 +0300

    Remove extra white spaces
    Signed-off-by: anton.denishchenko <anton.denishchenko@evernym.com>
    
    Signed-off-by: anton.denishchenko <anton.denishchenko@evernym.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: anton.denishchenko <anton.denishchenko@evernym.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: anton.denishchenko <anton.denishchenko@evernym.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: anton.denishchenko <anton.denishchenko@evernym.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: anton.denishchenko <anton.denishchenko@evernym.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: anton.denishchenko <anton.denishchenko@evernym.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: anton.denishchenko <anton.denishchenko@evernym.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable
    
    Signed-off-by: anton.denishchenko <anton.denishchenko@evernym.com>
    Sign-off-executed-by: toktar <renata.toktar@evernym.com>
    Approved-at: master-stable

--- scripts/remove_ledger.py
+++ scripts/remove_ledger.py
@@ -9,16 +9,16 @@ from indy_common.config_helper import ConfigHelper
 
 def warn(ledger_name, directories_path):
     print('The following directories will be deleted:')
-    
+
     for path in directories_path:
         print(str(path))
-    
+
     print('Deleting a ledger is an irrevocable operation.\nProceed only if you know the consequences.')
     answer = input('Do you want to delete ledger ' + ledger_name + '?\n Press [y/N]')
-    
+
     if answer.lower() == 'yes' or answer.lower() == 'y':
         return True
-    
+
     return False
 
 
@@ -29,15 +29,15 @@ def remove(ledger_name):
 
         for path in Path(config_helper.ledger_data_dir).rglob(ledger_name + "_*"):
             directories_path.append(path)
-        
+
         if not len(directories_path):
             print('Ledger doesn`t exist: ' + ledger_name)
-        
+
         elif warn(ledger_name, directories_path):
             for path in directories_path:
                 shutil.rmtree(str(path))
             print('Ledger removed successfully!')
-        
+
     else:
         print('Can`t delete built in ledger: ' + ledger_name)
 
