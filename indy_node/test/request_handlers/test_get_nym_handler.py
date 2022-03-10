77cb0882a0006253bde654b5db6078cf0dc4fe51
Char Howland <char@indicio.tech>
2022-03-10 19:38:07 +0000

    test: fail when both seqNo and timestamp are found
    Signed-off-by: Char Howland <char@indicio.tech>

--- indy_node/test/request_handlers/test_get_nym_handler.py
+++ indy_node/test/request_handlers/test_get_nym_handler.py
@@ -6,10 +6,13 @@ from random import randint
 import pytest
 from indy.did import create_and_store_my_did
 from indy_common.constants import ENDORSER
+from indy_node.test.helper import sdk_send_and_check_req_json
 from indy_node.test.mock import build_get_nym_request, build_nym_request
+from plenum.common.exceptions import RequestNackedException
 from plenum.common.util import randomString
 from plenum.test.helper import sdk_get_and_check_replies
 from plenum.test.pool_transactions.helper import sdk_sign_and_send_prepared_request
+
 
 diddoc_content = {
     "@context": [
@@ -189,3 +192,59 @@ def test_get_previous_nym_data_by_seq_no(
     assert (
         json.loads(replies[0][1]["result"]["data"])["diddocContent"] == diddoc_content_json
     )
+
+
+def test_nym_txn_rejected_with_both_seqNo_and_timestamp(
+    looper, sdk_pool_handle, sdk_wallet_endorser, add_diddoc_content
+):
+
+    _, did = sdk_wallet_endorser
+
+    # Get current nym data
+    get_nym_request = build_get_nym_request(did, did)
+    request_couple = sdk_sign_and_send_prepared_request(
+        looper, sdk_wallet_endorser, sdk_pool_handle, get_nym_request
+    )
+    replies = sdk_get_and_check_replies(looper, [request_couple])
+
+    # Get timestamp from data
+    timestamp = replies[0][1]["result"]["txnTime"]
+    seq_no = replies[0][1]["result"]["seqNo"]
+
+    # Write new nym data
+    new_diddoc_content = copy.deepcopy(diddoc_content)
+    new_diddoc_content["serviceEndpoint"][0][
+        "serviceEndpoint"
+    ] = "https://new.example.com"
+    new_diddoc_content = json.dumps(new_diddoc_content)
+
+    time.sleep(3)
+
+    nym_request = build_nym_request(
+        identifier=did, dest=did, diddoc_content=new_diddoc_content
+    )
+    request_couple = sdk_sign_and_send_prepared_request(
+        looper, sdk_wallet_endorser, sdk_pool_handle, nym_request
+    )
+    sdk_get_and_check_replies(looper, [request_couple])
+
+    get_nym_request = build_get_nym_request(did, did)
+    request_couple = sdk_sign_and_send_prepared_request(
+        looper, sdk_wallet_endorser, sdk_pool_handle, get_nym_request
+    )
+    replies = sdk_get_and_check_replies(looper, [request_couple])
+
+    assert (
+        json.loads(replies[0][1]["result"]["data"])["diddocContent"] == new_diddoc_content
+    )
+
+    # Attempt to get previous nym data by exact timestamp and seqNo
+    get_nym_request = build_get_nym_request(did, did, timestamp, seq_no)
+
+    with pytest.raises(RequestNackedException) as e:
+        sdk_send_and_check_req_json(
+            looper, sdk_pool_handle, sdk_wallet_endorser, get_nym_request
+        )
+    e.match("InvalidClientRequest")
+    e.match("client request invalid")
+    e.match("Cannot resolve nym with both seqNo and timestamp present.")
