36bf7b5ba3bdfcfbb4d6b01fb6ee4af3364d6838
Char Howland <char@indicio.tech>
2022-03-10 19:31:09 +0000

    feat: move seqNo and timestamp resolution tests
    Signed-off-by: Char Howland <char@indicio.tech>

--- indy_node/test/nym_txn/test_nym_did_indy.py
+++ indy_node/test/nym_txn/test_nym_did_indy.py
@@ -1,12 +1,11 @@
 import copy
 import json
-import time
 from random import randint
 
 import pytest
 from indy.did import create_and_store_my_did
 from indy_common.constants import ENDORSER, NYM
-from indy_node.test.mock import build_get_nym_request, build_nym_request
+from indy_node.test.mock import build_nym_request
 from plenum.common.exceptions import RequestNackedException
 from plenum.common.util import randomString
 from plenum.test.helper import sdk_get_and_check_replies
@@ -59,141 +58,6 @@ def add_diddoc_content(looper, sdk_pool_handle, sdk_wallet_endorser, prepare_end
 
 def test_diddoc_content_added(add_diddoc_content):
     pass
-
-
-def test_get_nym_data_with_diddoc_content(
-    looper, sdk_pool_handle, sdk_wallet_endorser, add_diddoc_content
-):
-    _, did = sdk_wallet_endorser
-    get_nym_request = build_get_nym_request(did, did)
-    request_couple = sdk_sign_and_send_prepared_request(
-        looper, sdk_wallet_endorser, sdk_pool_handle, get_nym_request
-    )
-    replies = sdk_get_and_check_replies(looper, [request_couple])
-
-    assert (
-        json.loads(replies[0][1]["result"]["data"])["diddocContent"] == diddoc_content_json
-    )
-
-
-def test_get_previous_nym_data_by_timestamp(
-    looper, sdk_pool_handle, sdk_wallet_endorser, add_diddoc_content
-):
-    _, did = sdk_wallet_endorser
-
-    # Get current nym data
-    get_nym_request = build_get_nym_request(did, did)
-    request_couple = sdk_sign_and_send_prepared_request(
-        looper, sdk_wallet_endorser, sdk_pool_handle, get_nym_request
-    )
-    replies = sdk_get_and_check_replies(looper, [request_couple])
-
-    # Get timestamp from data
-    timestamp = replies[0][1]["result"]["txnTime"]
-
-    # Write new nym data
-    new_diddoc_content = copy.deepcopy(diddoc_content)
-    new_diddoc_content["serviceEndpoint"][0][
-        "serviceEndpoint"
-    ] = "https://new.example.com"
-    new_diddoc_content = json.dumps(new_diddoc_content)
-
-    time.sleep(3)
-
-    nym_request = build_nym_request(
-        identifier=did, dest=did, diddoc_content=new_diddoc_content
-    )
-    request_couple = sdk_sign_and_send_prepared_request(
-        looper, sdk_wallet_endorser, sdk_pool_handle, nym_request
-    )
-    sdk_get_and_check_replies(looper, [request_couple])
-
-    get_nym_request = build_get_nym_request(did, did)
-    request_couple = sdk_sign_and_send_prepared_request(
-        looper, sdk_wallet_endorser, sdk_pool_handle, get_nym_request
-    )
-    replies = sdk_get_and_check_replies(looper, [request_couple])
-
-    assert (
-        json.loads(replies[0][1]["result"]["data"])["diddocContent"] == new_diddoc_content
-    )
-
-    update_ts = replies[0][1]["result"]["txnTime"]
-
-    # Get previous nym data by exact timestamp
-    get_nym_request = build_get_nym_request(did, did, timestamp)
-    request_couple = sdk_sign_and_send_prepared_request(
-        looper, sdk_wallet_endorser, sdk_pool_handle, get_nym_request
-    )
-    replies = sdk_get_and_check_replies(looper, [request_couple])
-
-    assert (
-        json.loads(replies[0][1]["result"]["data"])["diddocContent"] == diddoc_content_json
-    )
-
-    # Get previous nym data by timestamp but not exact
-    ts = randint(timestamp + 1, update_ts - 1)
-    get_nym_request = build_get_nym_request(did, did, ts)
-    request_couple = sdk_sign_and_send_prepared_request(
-        looper, sdk_wallet_endorser, sdk_pool_handle, get_nym_request
-    )
-    replies = sdk_get_and_check_replies(looper, [request_couple])
-
-    assert (
-        json.loads(replies[0][1]["result"]["data"])["diddocContent"] == diddoc_content_json
-    )
-
-
-def test_get_previous_nym_data_by_seq_no(
-    looper, sdk_pool_handle, sdk_wallet_endorser, add_diddoc_content
-):
-    _, did = sdk_wallet_endorser
-
-    # Get current nym data
-    get_nym_request = build_get_nym_request(did, did)
-    request_couple = sdk_sign_and_send_prepared_request(
-        looper, sdk_wallet_endorser, sdk_pool_handle, get_nym_request
-    )
-    replies = sdk_get_and_check_replies(looper, [request_couple])
-
-    # Get seq_no from data
-    seq_no = replies[0][1]["result"]["seqNo"]
-
-    # Write new nym data
-    new_diddoc_content = copy.deepcopy(diddoc_content)
-    new_diddoc_content["serviceEndpoint"][0][
-        "serviceEndpoint"
-    ] = "https://new.example.com"
-    new_diddoc_content = json.dumps(new_diddoc_content)
-
-    time.sleep(3)
-
-    nym_request = build_nym_request(did, did, None, new_diddoc_content, None)
-    request_couple = sdk_sign_and_send_prepared_request(
-        looper, sdk_wallet_endorser, sdk_pool_handle, nym_request
-    )
-    sdk_get_and_check_replies(looper, [request_couple])
-
-    get_nym_request = build_get_nym_request(did, did)
-    request_couple = sdk_sign_and_send_prepared_request(
-        looper, sdk_wallet_endorser, sdk_pool_handle, get_nym_request
-    )
-    replies = sdk_get_and_check_replies(looper, [request_couple])
-
-    assert (
-        json.loads(replies[0][1]["result"]["data"])["diddocContent"] == new_diddoc_content
-    )
-
-    # Get previous nym data by seq_no
-    get_nym_request = build_get_nym_request(did, did, seq_no=seq_no)
-    request_couple = sdk_sign_and_send_prepared_request(
-        looper, sdk_wallet_endorser, sdk_pool_handle, get_nym_request
-    )
-    replies = sdk_get_and_check_replies(looper, [request_couple])
-
-    assert (
-        json.loads(replies[0][1]["result"]["data"])["diddocContent"] == diddoc_content_json
-    )
 
 
 # Schema of NYM Tx in plenum is not enforced! Arbitrary properties can be injected, but won't be included in state
