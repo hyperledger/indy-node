88f2c5781eafcad3321292a9f222c3d08689394e
Daniel Bluhm <dbluhm@pm.me>
2022-03-30 20:44:10 -0400

    test: simplify doc and nym version tests
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- indy_node/test/nym_txn/test_nym_diddoc_content.py
+++ indy_node/test/nym_txn/test_nym_diddoc_content.py
@@ -3,51 +3,16 @@ import json
 from random import randint
 
 import pytest
-from indy.did import create_and_store_my_did
-from indy_common.constants import ENDORSER, NYM
+from indy_common.constants import NYM
 from indy_node.test.mock import build_nym_request
 from plenum.common.exceptions import RequestNackedException
-from plenum.common.util import randomString
 from plenum.test.helper import sdk_get_and_check_replies
 from plenum.test.pool_transactions.helper import sdk_sign_and_send_prepared_request
 
-diddoc_content = {
-    "@context": [
-        "https://www.w3.org/ns/did/v1",
-        "https://identity.foundation/didcomm-messaging/service-endpoint/v1",
-    ],
-    "serviceEndpoint": [
-        {
-            "id": "did:indy:sovrin:123456#didcomm",
-            "type": "didcomm-messaging",
-            "serviceEndpoint": "https://example.com",
-            "recipientKeys": ["#verkey"],
-            "routingKeys": [],
-        }
-    ],
-}
-diddoc_content_json = json.dumps(diddoc_content)
 
-
-# Prepare nym with role endorser and no diddoc content
-@pytest.fixture(scope="module")
-def prepare_endorser(looper, sdk_pool_handle, sdk_wallet_steward, sdk_wallet_endorser):
-    _, did_steward = sdk_wallet_steward
-    wh, _ = sdk_wallet_endorser
-    seed = randomString(32)
-    dest, verkey = looper.loop.run_until_complete(
-        create_and_store_my_did(wh, json.dumps({"seed": seed}))
-    )
-    nym_request = build_nym_request(did_steward, dest, verkey, None, ENDORSER)
-    request_couple = sdk_sign_and_send_prepared_request(
-        looper, sdk_wallet_steward, sdk_pool_handle, nym_request
-    )
-    sdk_get_and_check_replies(looper, [request_couple])
-
-
-# Add diddoc content to nym
-@pytest.fixture(scope="module")
-def add_diddoc_content(looper, sdk_pool_handle, sdk_wallet_endorser, prepare_endorser):
+def test_diddoc_content_added(
+    looper, sdk_pool_handle, sdk_wallet_endorser, diddoc_content
+):
     _, did = sdk_wallet_endorser
     nym_request = build_nym_request(did, did, None, diddoc_content, None)
     request_couple = sdk_sign_and_send_prepared_request(
@@ -56,13 +21,9 @@ def add_diddoc_content(looper, sdk_pool_handle, sdk_wallet_endorser, prepare_end
     sdk_get_and_check_replies(looper, [request_couple])
 
 
-def test_diddoc_content_added(add_diddoc_content):
-    pass
-
-
 # Schema of NYM Tx in plenum is not enforced! Arbitrary properties can be injected, but won't be included in state
 def test_add_arbitrary_property(
-    looper, sdk_pool_handle, sdk_wallet_endorser, prepare_endorser
+    looper, sdk_pool_handle, sdk_wallet_endorser
 ):
     _, did = sdk_wallet_endorser
 
@@ -84,7 +45,7 @@ def test_add_arbitrary_property(
 
 
 def test_add_didoc_with_id_fails(
-    looper, sdk_pool_handle, sdk_wallet_endorser, prepare_endorser
+    looper, sdk_pool_handle, sdk_wallet_endorser, diddoc_content
 ):
     _, did = sdk_wallet_endorser
     diddoc_content_with_id = copy.deepcopy(diddoc_content)
