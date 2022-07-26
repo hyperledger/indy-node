9f98d3fbb5498211f51295a985ef6df29a838f6d
Wade Barnes <wade@neoterictech.ca>
2022-07-26 10:24:12 -0700

    Fix indy-test-automation pipeline dependencies.
    - libindy deb packages were renamed starting with `1.14.3~126`, and a `-xenial` suffix was added.
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- ci/pipeline.groovy
+++ ci/pipeline.groovy
@@ -81,7 +81,7 @@ def systemTests(Closure body) {
                     "PYTHON3_LIBINDY_CRYPTO_VERSION=${indyCryptoVersions.debian}",
                     "INDY_PLENUM_VERSION=${indyPlenumVersions.debian}",
                     "INDY_NODE_VERSION=${config.pkgVersion}",
-                    "LIBINDY_REPO_COMPONENT=${indySDKVersions.debian == indySDKVersions.pypi ? 'stable' : 'master'}",
+                    "LIBINDY_REPO_COMPONENT=${indySDKVersions.debian.replaceAll("-xenial", "") == indySDKVersions.pypi ? 'stable' : 'master'}",
                     "LIBINDY_VERSION=${indySDKVersions.debian}",
                 ]) {
                     sh "./system/docker/prepare.sh $systemTestsNetwork"
@@ -165,7 +165,7 @@ def systemTests(Closure body) {
                 indySDKVersions.pypi = sh(returnStdout: true, script: """
                     grep "^Collecting python3-indy==" $pipLogName | awk '{print \$2}' | awk -F'==' '{print \$2}'
                 """).trim()
-                indySDKVersions.debian = indySDKVersions.pypi.replaceAll(/-(dev|rc)-(.*)/, "~\$2")
+                indySDKVersions.debian = indySDKVersions.pypi.replaceAll(/-(dev|rc)-(.*)/, "~\$2") + '-xenial'
                 echo "indy-sdk version: ${indySDKVersions}"
 
                 indyCryptoVersions.pypi = sh(returnStdout: true, script: """
