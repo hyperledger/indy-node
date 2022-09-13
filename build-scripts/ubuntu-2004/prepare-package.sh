cc8235a209bdf0056cf19eab827b8d23a86151cd
Philipp Schlarb <p.schlarb@esatus.com>
2022-09-13 15:36:44 +0000

    Fixed prepare_packages
    Signed-off-by: Philipp Schlarb <p.schlarb@esatus.com>

--- build-scripts/ubuntu-2004/prepare-package.sh
+++ build-scripts/ubuntu-2004/prepare-package.sh
@@ -24,21 +24,31 @@ echo -e "\nGenerating manifest"
 bash -ex $GENERATE_MANIFEST_SCRIPT
 cat $module_name/__manifest__.json
 
-echo -e "\n\nPrepares indy-node debian package version"
-sed -i -r "s~indy-node==([0-9\.]+[0-9])(\.)?([a-z]+)~indy-node==\1\~\3~" setup.py
-
 if [ "$distro_packages" = "debian-packages" ]; then
+  echo -e "\n\nPrepares indy-node debian package version"
+  sed -i -r "s~indy-node==([0-9\.]+[0-9])(\.)?([a-z]+)~indy-node==\1\~\3~" setup.py
+
   # Update the package names to match the names that are available on the os.
   echo -e "\nAdapt the dependencies for the Canonical archive"
   sed -i "s~timeout-decorator~python3-timeout-decorator~" setup.py
   sed -i "s~distro~python3-distro~" setup.py
   sed -i "s~importlib-metadata=~python3-importlib-metadata=~" setup.py
 
-
   # Only used for the deb package builds, NOT for the PyPi package builds.
   echo -e "\n\nPrepares indy-plenum debian package version"
   sed -i -r "s~indy-plenum==([0-9\.]+[0-9])(\.)?([a-z]+)~indy-plenum==\1\~\3~" setup.py
-
+  
+  echo "Preparing config files"
+  GENERAL_CONFIG_DIR="\/etc\/indy"
+  REPO_GENERAL_CONFIG_DIR="indy_node/general_config"
+  # Define user config directory
+  sed -i "s/^\(GENERAL_CONFIG_DIR\s*=\s*\).*\$/\1\"$GENERAL_CONFIG_DIR\"/" indy_common/config.py
+  # Create user config
+  cp $REPO_GENERAL_CONFIG_DIR/general_config.py $REPO_GENERAL_CONFIG_DIR/indy_config.py
+  cat $REPO_GENERAL_CONFIG_DIR/ubuntu_platform_config.py >> $REPO_GENERAL_CONFIG_DIR/indy_config.py
+  rm -f $REPO_GENERAL_CONFIG_DIR/general_config.py
+  rm -f $REPO_GENERAL_CONFIG_DIR/ubuntu_platform_config.py
+  rm -f $REPO_GENERAL_CONFIG_DIR/windows_platform_config.py
 elif [ "$distro_packages" = "python-packages" ]; then
   echo -e "\nNo adaption of dependencies for python packages"
 else
@@ -46,19 +56,6 @@ else
   exit 1
 fi
 
-echo "Preparing config files"
-GENERAL_CONFIG_DIR="\/etc\/indy"
-REPO_GENERAL_CONFIG_DIR="indy_node/general_config"
-# Define user config directory
-sed -i "s/^\(GENERAL_CONFIG_DIR\s*=\s*\).*\$/\1\"$GENERAL_CONFIG_DIR\"/" indy_common/config.py
-# Create user config
-cp $REPO_GENERAL_CONFIG_DIR/general_config.py $REPO_GENERAL_CONFIG_DIR/indy_config.py
-cat $REPO_GENERAL_CONFIG_DIR/ubuntu_platform_config.py >> $REPO_GENERAL_CONFIG_DIR/indy_config.py
-rm -f $REPO_GENERAL_CONFIG_DIR/general_config.py
-rm -f $REPO_GENERAL_CONFIG_DIR/ubuntu_platform_config.py
-rm -f $REPO_GENERAL_CONFIG_DIR/windows_platform_config.py
-
 popd
 
 echo -e "\nFinished preparing $repo for publishing\n"
-
