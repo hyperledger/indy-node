9ae0ccf2926b15cd4c223b84287f1bf2e2006787
Christian Bormann <ChristianCarl.Bormann@de.bosch.com>
2022-09-21 15:55:27 +0200

    Add test for state updates
    Signed-off-by: Christian Bormann <ChristianCarl.Bormann@de.bosch.com>

--- indy_node/test/request_handlers/test_flag_handler.py
+++ indy_node/test/request_handlers/test_flag_handler.py
@@ -211,6 +211,53 @@ def test_config_flag_update_state_correct_value(
     assert(state_time == txn_time)
 
 
+def test_config_flag_update_state_edit(
+    flag_handler: FlagHandler,
+    flag_request: Request,
+):
+    add_to_idr(
+        flag_handler.database_manager.idr_cache,
+        flag_request.identifier,
+        TRUSTEE,
+    )
+
+    val_old = "old_value"
+    flag_request.operation[FLAG_VALUE] = val_old
+    key = flag_request.operation.get(FLAG_NAME)
+    seq_no = 2
+    txn_time_old = 1560241033
+    txn = reqToTxn(flag_request)
+    append_txn_metadata(txn, seq_no, txn_time_old)
+    flag_handler.update_state(txn, None, flag_request)
+    old_head_hash = flag_handler.state.headHash
+    assert(old_head_hash)
+    (value, timestamp) = flag_handler.get_state(key)
+    assert(value == val_old)
+    assert(timestamp == txn_time_old)
+
+    val_new = "new_value"
+    flag_request.operation[FLAG_VALUE] = val_new
+    key = flag_request.operation.get(FLAG_NAME)
+    seq_no = 3
+    txn_time = 1560251034
+    txn = reqToTxn(flag_request)
+    append_txn_metadata(txn, seq_no, txn_time)
+    flag_handler.update_state(txn, None, flag_request)
+
+    (value, timestamp) = flag_handler.get_state(key)
+    assert(value == val_new)
+    assert(timestamp == txn_time)
+
+    # check old state
+    path = FlagHandler.make_state_path_for_flag(key)
+    state_encoded = flag_handler.state.get_for_root_hash(old_head_hash, path)
+    state_raw = flag_handler._decode_state_value(state_encoded)
+    value = FlagHandler.get_state_value(state_raw)
+    timestamp = FlagHandler.get_state_timestamp(state_raw)
+    assert(value == val_old)
+    assert(timestamp == txn_time_old)
+
+
 def test_config_flag_get_state_empty_key(
     flag_handler: FlagHandler,
     flag_request: Request,
