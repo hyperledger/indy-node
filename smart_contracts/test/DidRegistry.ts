cdf940c08e93940754d5e6e6b6feba9d6635d9af
aziz.karabashov <aziz.karabashov@dsr-corporation.com>
2023-09-13 20:06:52 +0500

    Add unit tests for create DID
    Signed-off-by: aziz.karabashov <aziz.karabashov@dsr-corporation.com>

---
+++ smart_contracts/test/DidRegistry.ts
@@ -0,0 +1,132 @@
+import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { DidRegistry } from '../typechain-types';
+
+describe('DIDContract', function () {
+	// We define a fixture to reuse the same setup in every test.
+	// We use loadFixture to run this setup once, snapshot that state,
+	// and reset Hardhat Network to that snapshot in every test.
+	async function deployDIDContractixture() {
+		// Contracts are deployed using the first signer/account by default
+		const [owner, otherAccount] = await ethers.getSigners();
+
+		const DidRegistry = await ethers.getContractFactory('DidRegistry');
+		const didRegistry = await DidRegistry.deploy();
+
+		ethers.deployContract('DidRegistry')
+
+		return { didRegistry, owner, otherAccount };
+	}
+
+	describe('Create DID', function () {
+		it('Should create DID document', async function () {
+			const { didRegistry } = await loadFixture(deployDIDContractixture);
+
+			const did: string = 'did:indy2:testnet:SEp33q43PsdP7nDATyySSH'
+
+			const verificationMethod: DidRegistry.VerificationMethodStruct = {
+				id: did,
+				verificationMethodType: 'Ed25519VerificationKey2018',
+				controller: 'did:indy2:testnet:N22SEp33q43PsdP7nDATyySSH',
+				publicKeyMultibase: 'zAKJP3f7BD6W4iWEQ9jwndVTCBq8ua2Utt8EEjJ6Vxsf',
+				publicKeyJwk: '',
+			}
+
+			const authentication: DidRegistry.VerificationRelationshipStruct = {
+				id: did,
+				verificationMethod: {
+					id: '',
+					verificationMethodType: '',
+					controller: '',
+					publicKeyMultibase: '',
+					publicKeyJwk: '',
+				}
+			}
+
+			const didDocument: DidRegistry.DidDocumentStruct = {
+				context: [],
+				id: did,
+				controller: [],
+				verificationMethod: [verificationMethod],
+				authentication: [authentication],
+				assertionMethod: [],
+				capabilityInvocation: [],
+				capabilityDelegation: [],
+				keyAgreement: [],
+				service: [],
+				alsoKnownAs: [],
+			}
+
+			const signature: DidRegistry.SignatureStruct = { 
+				id: did, 
+				value: '4X3skpoEK2DRgZxQ9PwuEvCJpL8JHdQ8X4HDDFyztgqE15DM2ZnkvrAh9bQY16egVinZTzwHqznmnkaFM4jjyDgd' 
+			}
+
+			const tx = await didRegistry.createDid(didDocument, [signature])
+			await tx.wait()
+
+			const savedDid = await didRegistry.dids(did)
+
+			expect(savedDid.document.id).to.equals(didDocument.id);
+			expect(savedDid.document.authentication[0].id).to.equals(didDocument.authentication[0].id);
+			expect(savedDid.document.verificationMethod[0].id)
+				.to.equals(didDocument.verificationMethod[0].id);
+			expect(savedDid.document.verificationMethod[0].verificationMethodType)
+				.to.equals(didDocument.verificationMethod[0].verificationMethodType);
+			expect(savedDid.document.verificationMethod[0].controller)
+				.to.equals(didDocument.verificationMethod[0].controller);
+			expect(savedDid.document.verificationMethod[0].publicKeyMultibase)
+				.to.equals(didDocument.verificationMethod[0].publicKeyMultibase);
+		});
+
+		it('Should fail if the DID being created already exists', async function () {
+			const { didRegistry } = await loadFixture(deployDIDContractixture);
+
+			const did: string = 'did:indy2:testnet:SEp33q43PsdP7nDATyySSH'
+
+			const verificationMethod: DidRegistry.VerificationMethodStruct = {
+				id: did,
+				verificationMethodType: 'Ed25519VerificationKey2018',
+				controller: 'did:indy2:testnet:N22SEp33q43PsdP7nDATyySSH',
+				publicKeyMultibase: 'zAKJP3f7BD6W4iWEQ9jwndVTCBq8ua2Utt8EEjJ6Vxsf',
+				publicKeyJwk: '',
+			}
+
+			const authentication: DidRegistry.VerificationRelationshipStruct = {
+				id: did,
+				verificationMethod: {
+					id: '',
+					verificationMethodType: '',
+					controller: '',
+					publicKeyMultibase: '',
+					publicKeyJwk: '',
+				}
+			}
+
+			const didDocument: DidRegistry.DidDocumentStruct = {
+				context: [],
+				id: did,
+				controller: [],
+				verificationMethod: [verificationMethod],
+				authentication: [authentication],
+				assertionMethod: [],
+				capabilityInvocation: [],
+				capabilityDelegation: [],
+				keyAgreement: [],
+				service: [],
+				alsoKnownAs: [],
+			}
+
+			const signature: DidRegistry.SignatureStruct = { 
+				id: did, 
+				value: '4X3skpoEK2DRgZxQ9PwuEvCJpL8JHdQ8X4HDDFyztgqE15DM2ZnkvrAh9bQY16egVinZTzwHqznmnkaFM4jjyDgd' 
+			}
+
+			var tx = await didRegistry.createDid(didDocument, [signature])
+			await tx.wait()
+
+			await expect(didRegistry.createDid(didDocument, [signature])).to.be.revertedWith('DID has already exist')
+		});
+	});
+});
