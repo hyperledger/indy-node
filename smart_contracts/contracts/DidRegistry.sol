afaf1b2f128b364d9b4f05edbe2ad5ed9c28dc46
aziz.karabashov <aziz.karabashov@dsr-corporation.com>
2023-09-15 13:02:12 +0500

    Add DID Syntaxt validation
    Signed-off-by: aziz.karabashov <aziz.karabashov@dsr-corporation.com>

--- smart_contracts/contracts/DidRegistry.sol
+++ smart_contracts/contracts/DidRegistry.sol
@@ -1,18 +1,31 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
+import { strings } from "./utils/string.sol";
+
 contract DidRegistry {
+    using strings for *;
+
+    /**
+     * @dev DidDocumentStorage holds the DID Document and its associated metadata
+     */
     struct DidDocumentStorage {
         DidDocument document;
         DidMetadata metadata;
     }
 
+    /**
+     * @dev VerificationRelationship links a DID to a verification method
+     */
     struct DidMetadata {
         uint256 created;
         uint256 updated;
         bool deactivated;
     }
 
+    /**
+     * @dev DidDocument represent the main DID Document structure.
+     */
     struct DidDocument {
         string[] context;
         string id;
@@ -27,6 +40,9 @@ contract DidRegistry {
         string[] alsoKnownAs;
     }
 
+    /**
+     * @dev VerificationMethod are used to define how to authenticate/authorise interactions with a DID subject or delegates.
+     */
     struct VerificationMethod {
         string id;
         string verificationMethodType;
@@ -35,11 +51,17 @@ contract DidRegistry {
         string publicKeyMultibase;
     }
 
+    /**
+     * @dev VerificationRelationship links a DID to a verification method.
+     */
     struct VerificationRelationship {
         string id;
         VerificationMethod verificationMethod;
     }
 
+    /**
+     * @dev Service describes a service endpoint related to the DID.
+     */
     struct Service {
         string id;
         string serviceType;
@@ -48,36 +70,83 @@ contract DidRegistry {
         string[] routingKeys;
     }
 
+    /**
+     * @dev Signature describes DID Document signature
+     */
     struct Signature {
         string id;
         string value;
     }
 
-    mapping(string => DidDocumentStorage) public dids;
-
+    /**
+     * @dev Event that is sent when a DID Document is created
+     */
     event DIDCreated(string did);
+
+    /**
+     * @dev Event that is sent when a DID Document is updated
+     */
     event DIDUpdated(string did);
+
+    /**
+     * @dev Event that is sent when a DID Document is deactivated
+     */
     event DIDDeactivated(string did);
 
+    /**
+     * @dev Mapping DID to its corresponfing DID Document.
+     */
+    mapping(string => DidDocumentStorage) public dids;
+
+    /**
+     * Checks that DID already exists
+     */
     modifier didExist(string memory did) {
         require(dids[did].metadata.created != 0, "DID not found");
         _;
     }
 
+    /**
+     * Сhecks that the DID has not yet been added
+     */
     modifier didNotExist(string memory did) {
         require(dids[did].metadata.created == 0, "DID has already exist");
         _;
     }
 
+     /**
+     * Validated the DID syntax
+     */
+    modifier validDid(string memory did) {
+        strings.slice memory didSlice = did.toSlice();
+        strings.slice memory delimiter = ':'.toSlice();
+        strings.slice[] memory parts = new strings.slice[](didSlice.count(delimiter));
+        for (uint i = 0; i < parts.length; i++) {                              
+           parts[i] = didSlice.split(delimiter);                               
+        }
+        
+        require(parts[0].equals("did".toSlice()), "Incorrect DID schema");
+        require(parts[1].equals("indy2".toSlice()) || parts[1].equals("indy".toSlice()) || parts[1].equals("sov".toSlice()), "Unsupported DID method");
+        _;
+    }
+
+    /**
+     * Сhecks that the DID has not been deactivated
+     */
     modifier didIsActive(string memory did) {
         require(!dids[did].metadata.deactivated, "DID has been deactivated");
         _;
     }
 
+    /**
+     * Creates a new DID
+     * @param document The new DID Document
+     * @param signatures An array of DID Document signatures
+     */
     function createDid(
         DidDocument calldata document,
         Signature[] calldata signatures
-    ) public didNotExist(document.id) {
+    ) public didNotExist(document.id) validDid(document.id) {
         dids[document.id].document = document;
         dids[document.id].metadata.created = block.timestamp;
         dids[document.id].metadata.updated = block.timestamp;
@@ -85,6 +154,11 @@ contract DidRegistry {
         emit DIDCreated(document.id);
     }
 
+    /**
+     * Updates an existing DID
+     * @param document The updated DID Document
+     * @param signatures An array of DID Document signatures
+     */
     function updateDidDocument(
         DidDocument calldata document,
         Signature[] calldata signatures
@@ -95,6 +169,11 @@ contract DidRegistry {
         emit DIDUpdated(document.id);
     }
 
+    /**
+     * Deactivates a DID
+     * @param id The DID to be deactivated
+     * @param signatures An array of DID Document signatures
+     */
     function deactivateDid(
         string calldata id,
         Signature[] calldata signatures
@@ -102,16 +181,5 @@ contract DidRegistry {
         dids[id].metadata.deactivated = true;
 
         emit DIDDeactivated(id);
-    }
-
-    function isEquals(
-        string memory string1,
-        string memory string2
-    ) private pure returns (bool) {
-        return keccak256(bytes(string1)) == keccak256(bytes(string2));
-    }
-
-    function isEmpty(string memory _string) private pure returns (bool) {
-        return bytes(_string).length == 0;
     }
 }
--- smart_contracts/test/DidRegistry.ts
+++ smart_contracts/test/DidRegistry.ts
@@ -7,7 +7,7 @@ describe('DIDContract', function () {
 	// We define a fixture to reuse the same setup in every test.
 	// We use loadFixture to run this setup once, snapshot that state,
 	// and reset Hardhat Network to that snapshot in every test.
-	async function deployDIDContractixture() {
+	async function deployDidContractixture() {
 		// Contracts are deployed using the first signer/account by default
 		const [owner, otherAccount] = await ethers.getSigners();
 
@@ -21,7 +21,7 @@ describe('DIDContract', function () {
 
 	describe('Create DID', function () {
 		it('Should create DID document', async function () {
-			const { didRegistry } = await loadFixture(deployDIDContractixture);
+			const { didRegistry } = await loadFixture(deployDidContractixture);
 
 			const did: string = 'did:indy2:testnet:SEp33q43PsdP7nDATyySSH'
 
@@ -63,8 +63,7 @@ describe('DIDContract', function () {
 				value: '4X3skpoEK2DRgZxQ9PwuEvCJpL8JHdQ8X4HDDFyztgqE15DM2ZnkvrAh9bQY16egVinZTzwHqznmnkaFM4jjyDgd' 
 			}
 
-			const tx = await didRegistry.createDid(didDocument, [signature])
-			await tx.wait()
+			await didRegistry.createDid(didDocument, [signature])
 
 			const savedDid = await didRegistry.dids(did)
 
@@ -81,7 +80,7 @@ describe('DIDContract', function () {
 		});
 
 		it('Should fail if the DID being created already exists', async function () {
-			const { didRegistry } = await loadFixture(deployDIDContractixture);
+			const { didRegistry } = await loadFixture(deployDidContractixture);
 
 			const did: string = 'did:indy2:testnet:SEp33q43PsdP7nDATyySSH'
 
@@ -128,5 +127,104 @@ describe('DIDContract', function () {
 
 			await expect(didRegistry.createDid(didDocument, [signature])).to.be.revertedWith('DID has already exist')
 		});
+
+		it('Should fail if provided did with incorrect schema', async function () {
+			const { didRegistry } = await loadFixture(deployDidContractixture);
+
+			const did: string = 'indy:indy2:testnet:SEp33q43PsdP7nDATyySSH'
+
+			const verificationMethod: DidRegistry.VerificationMethodStruct = {
+				id: did,
+				verificationMethodType: 'Ed25519VerificationKey2018',
+				controller: 'did:indy2:testnet:N22SEp33q43PsdP7nDATyySSH',
+				publicKeyMultibase: 'zAKJP3f7BD6W4iWEQ9jwndVTCBq8ua2Utt8EEjJ6Vxsf',
+				publicKeyJwk: '',
+			}
+
+			const authentication: DidRegistry.VerificationRelationshipStruct = {
+				id: did,
+				verificationMethod: {
+					id: '',
+					verificationMethodType: '',
+					controller: '',
+					publicKeyMultibase: '',
+					publicKeyJwk: '',
+				}
+			}
+
+			const didDocument: DidRegistry.DidDocumentStruct = {
+				context: [],
+				id: did,
+				controller: [],
+				verificationMethod: [verificationMethod],
+				authentication: [authentication],
+				assertionMethod: [],
+				capabilityInvocation: [],
+				capabilityDelegation: [],
+				keyAgreement: [],
+				service: [],
+				alsoKnownAs: [],
+			}
+
+			const signature: DidRegistry.SignatureStruct = { 
+				id: did, 
+				value: '4X3skpoEK2DRgZxQ9PwuEvCJpL8JHdQ8X4HDDFyztgqE15DM2ZnkvrAh9bQY16egVinZTzwHqznmnkaFM4jjyDgd' 
+			}
+
+			await expect(didRegistry.createDid(didDocument, [signature])).to.be.revertedWith('Incorrect DID schema')
+		});
+
+		it('Should fail if provided did with unsupported DID method', async function () {
+			const { didRegistry } = await loadFixture(deployDidContractixture);
+
+			const did: string = 'did:indy3:testnet:SEp33q43PsdP7nDATyySSH'
+
+			const verificationMethod: DidRegistry.VerificationMethodStruct = {
+				id: did,
+				verificationMethodType: 'Ed25519VerificationKey2018',
+				controller: 'did:indy2:testnet:N22SEp33q43PsdP7nDATyySSH',
+				publicKeyMultibase: 'zAKJP3f7BD6W4iWEQ9jwndVTCBq8ua2Utt8EEjJ6Vxsf',
+				publicKeyJwk: '',
+			}
+
+			const authentication: DidRegistry.VerificationRelationshipStruct = {
+				id: did,
+				verificationMethod: {
+					id: '',
+					verificationMethodType: '',
+					controller: '',
+					publicKeyMultibase: '',
+					publicKeyJwk: '',
+				}
+			}
+
+			const didDocument: DidRegistry.DidDocumentStruct = {
+				context: [],
+				id: did,
+				controller: [],
+				verificationMethod: [verificationMethod],
+				authentication: [authentication],
+				assertionMethod: [],
+				capabilityInvocation: [],
+				capabilityDelegation: [],
+				keyAgreement: [],
+				service: [],
+				alsoKnownAs: [],
+			}
+
+			const signature: DidRegistry.SignatureStruct = { 
+				id: did, 
+				value: '4X3skpoEK2DRgZxQ9PwuEvCJpL8JHdQ8X4HDDFyztgqE15DM2ZnkvrAh9bQY16egVinZTzwHqznmnkaFM4jjyDgd' 
+			}
+
+			await expect(didRegistry.createDid(didDocument, [signature])).to.be.revertedWith('Unsupported DID method')
+		});
+	});
+
+	describe('Update DID', function () {
+		
+	});
+
+	describe('Deactivate DID', function () {
 	});
 });
