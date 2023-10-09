f148a33c900eba2fc1287439adb0617c9a134a7a
aziz.karabashov <aziz.karabashov@dsr-corporation.com>
2023-10-09 15:19:12 +0500

    Remove redundant typechain classes
    Signed-off-by: aziz.karabashov <aziz.karabashov@dsr-corporation.com>

--- smart_contracts/demos/account-control.ts
+++ smart_contracts/demos/account-control.ts
@@ -1,10 +1,9 @@
 import { Actor } from './utils/actor'
 import assert from 'assert'
 import environment from '../environment'
-import { ethers } from 'ethers'
 import { ROLES } from '../contracts-ts'
-import simpleContractJson from './utils/contracts/SimpleContract.json'
 import { SimpleContract } from './utils/contracts/SimpleContract'
+import { delay } from '../utils'
 
 async function demo() {
   const trustee = await new Actor(environment.accounts.account1).init()
@@ -21,72 +20,55 @@ async function demo() {
   console.log(`Role ${ROLES.STEWARD} assigned to account ${steward.address} -- ${JSON.stringify(receipt)}`)
 
   console.log('3. Try deploying a contract by an unauthorized account')
-  let simpleContractFactory = new ethers.ContractFactory(
-    simpleContractJson.abi,
-    simpleContractJson.bytecode,
-    unauthorized.account.signer,
-  )
-  await assert.rejects(simpleContractFactory.deploy(), (err) => {
+  let simpleContract = new SimpleContract(unauthorized.account)
+  await assert.rejects(simpleContract.deploy(), (err) => {
     console.log(JSON.stringify(err))
     return true
   })
 
   console.log('4. Try deploying a contract by an endorser')
-  simpleContractFactory = new ethers.ContractFactory(
-    simpleContractJson.abi,
-    simpleContractJson.bytecode,
-    endorser.account.signer,
-  )
-  await assert.rejects(simpleContractFactory.deploy(), (err) => {
+  simpleContract = new SimpleContract(endorser.account)
+  await assert.rejects(simpleContract.deploy(), (err) => {
     console.log(JSON.stringify(err))
     return true
   })
 
   console.log('5. Try deploying a contract by a steward')
-  simpleContractFactory = new ethers.ContractFactory(
-    simpleContractJson.abi,
-    simpleContractJson.bytecode,
-    steward.account.signer,
-  )
-  await assert.rejects(simpleContractFactory.deploy(), (err) => {
+  simpleContract = new SimpleContract(steward.account)
+  await assert.rejects(simpleContract.deploy(), (err) => {
     console.log(JSON.stringify(err))
     return true
   })
 
   console.log('6. Deploy a contract by a trustee')
-  simpleContractFactory = new ethers.ContractFactory(
-    simpleContractJson.abi,
-    simpleContractJson.bytecode,
-    trustee.account.signer,
-  )
-  let simpleContract = (await simpleContractFactory.deploy()) as SimpleContract
-  const address = await simpleContract.getAddress()
-  console.log(`Contract ${simpleContractJson.sourceName} deployed to address ${address} by trustee`)
-
+  simpleContract = new SimpleContract(trustee.account)
+  await simpleContract.deploy()
+  console.log(`Contract deployed to address ${simpleContract.address} by trustee`)
+  await delay(4000)
+  
   console.log('7. Try calling the update contact method by an unauthorized account')
   simpleContract = simpleContract.connect(unauthorized.account.signer)
-  await assert.rejects(simpleContract.update('test'), (err) => {
+  await assert.rejects(simpleContract.update('unauthorized'), (err) => {
     console.log(JSON.stringify(err))
     return true
   })
 
   console.log('8. Call the update contact method by an endorser')
   simpleContract = simpleContract.connect(endorser.account.signer)
-  let tx = await simpleContract.update('endorser')
-  receipt = await tx.wait()
-  console.log(`Message updated by endorser -- ${JSON.stringify(receipt)}`)
+  receipt = await simpleContract.update('endorser')
+  let message = await simpleContract.message()
+  console.log(`Message updated to '${message}' by endorser -- ${JSON.stringify(receipt)}`)
 
   console.log('9. Call the update contact method by a steward')
   simpleContract = simpleContract.connect(steward.account.signer)
-  tx = await simpleContract.update('steward')
-  receipt = await tx.wait()
-  console.log(`Message updated by steward -- ${JSON.stringify(receipt)}`)
+  receipt = await simpleContract.update('steward')
+  message = await simpleContract.message()
+  console.log(`Message updated to '${message}' by steward -- ${JSON.stringify(receipt)}`)
 
   console.log('10. Call the update contact method by a trustee')
-  simpleContract = simpleContract.connect(trustee.account.signer)
-  tx = await simpleContract.update('trustee')
-  receipt = await tx.wait()
-  console.log(`Message updated by trustee -- ${JSON.stringify(receipt)}`)
+  receipt = await simpleContract.update('trustee')
+  message = await simpleContract.message()
+  console.log(`Message updated to '${message}' by trustee -- ${JSON.stringify(receipt)}`)
 }
 
 if (require.main === module) {
